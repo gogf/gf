8002e63d24e5622bf81ff297be9c5c391536964e
John <john@johng.cn>
2017-07-06 21:07:43 +0800

    dev

--- src/g/core.config.go
+++ src/g/core.config.go
@@ -2,10 +2,19 @@ package g
 
 
 type gConfig struct {
-    Debug bool
+    Debug    bool
+    Database struct {
+        master struct{
+
+        }
+        slave struct{
+
+        }
+    }
 }
 
 // 框架设置项
 var Config = gConfig {
     Debug : true,
+
 }
--- src/gexample/other/test.go
+++ src/gexample/other/test.go
@@ -2,20 +2,105 @@ package main
 
 import (
     "fmt"
+    //"strconv"
 )
 
-func test(data interface{}) {
-    switch data.(type) {
-    case string:
-        fmt.Println("string")
-    case map[string]string:
-        fmt.Println("map[string]string")
-    case []interface{}:
-        fmt.Println("[]interface{}")
-    default:
-        fmt.Println("default")
-    }
+
+const (
+    GJSON_CHAR_BRACE_LEFT        = rune('{')
+    GJSON_CHAR_BRACE_RIGHT       = rune('}')
+    GJSON_CHAR_BRACKET_LEFT      = rune('[')
+    GJSON_CHAR_BRACKET_RIGHT     = rune(']')
+    GJSON_CHAR_QUOTATION         = rune('\\')
+    GJSON_CHAR_COMMA             = rune(',')
+    GJSON_CHAR_COLON             = rune(':')
+    GJSON_CHAR_DOUBLE_QUOTE_MARK = rune('"')
+)
+
+
+type jsonParser struct {
+    content []rune
+    tokens  [][]rune
+    size    int
 }
+
+func (p *jsonParser)getNextToken(token rune, from int) int {
+    for i := from + 1; i < p.size; i++ {
+        if p.content[i] == token {
+            if i > 0 && p.content[i - 1] != GJSON_CHAR_QUOTATION {
+                return i
+            }
+        } else {
+            switch p.content[i] {
+                case GJSON_CHAR_DOUBLE_QUOTE_MARK:
+                    r := p.getNextToken(GJSON_CHAR_DOUBLE_QUOTE_MARK, i)
+                    if r > 0 {
+                        i = r
+                    }
+            }
+        }
+    }
+    return 0
+}
+
+func (p *jsonParser)parseTokens()  {
+    for i := 1; i < p.size; i++ {
+        switch p.content[i] {
+            case GJSON_CHAR_DOUBLE_QUOTE_MARK:
+                r := p.getNextToken(GJSON_CHAR_DOUBLE_QUOTE_MARK, i)
+                if r > 0 {
+                    fmt.Println(string(p.content[i:r + 1]))
+                    p.tokens = append(p.tokens, p.content[i:r + 1])
+                    i = r + 1
+                }
+
+            case GJSON_CHAR_COLON:
+                fallthrough
+            case GJSON_CHAR_COMMA:
+            case GJSON_CHAR_BRACE_LEFT:
+            case GJSON_CHAR_BRACE_RIGHT:
+            case GJSON_CHAR_BRACKET_LEFT:
+            case GJSON_CHAR_BRACKET_RIGHT:
+                fmt.Println(string(p.content[i:i]))
+                p.tokens = append(p.tokens, p.content[i:i])
+        }
+    }
+    // fmt.Println(p.tokens)
+}
+
+func (p *jsonParser)parse()  {
+    for i := 1; i < p.size; i++ {
+        switch p.content[i] {
+        case GJSON_CHAR_DOUBLE_QUOTE_MARK:
+            r := p.getNextToken(GJSON_CHAR_DOUBLE_QUOTE_MARK, i)
+            if r > 0 {
+                fmt.Println(string(p.content[i:r + 1]))
+                i = r + 1
+            }
+
+        case GJSON_CHAR_BRACE_LEFT:
+            r := p.getNextToken(GJSON_CHAR_BRACE_RIGHT, i)
+            if r > 0 {
+                fmt.Println(string(p.content[i:r + 1]))
+                i = r + 1
+            }
+        }
+    }
+
+}
+
+func jsonDecode(j *string)  {
+    json   := []rune(*j)
+    size   := len(json)
+    parser := &jsonParser{
+        content     : json,
+        size        : size,
+    }
+    parser.parseTokens()
+
+}
+
 func main() {
-    test(map[string]string {"k": "v"})
+    json := `{"name":"中国","age":31,"list":[["a","b","c"],["d","e","f"]],"item":{"title":"make\"he moon","name":"make'he moon","content":"'[}]{[}he moon"}}`
+    jsonDecode(&json)
 }
