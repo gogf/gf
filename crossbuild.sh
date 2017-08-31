1e752fc92d41495b422f6ed26763a4a53f0d6aaa
John <john@johng.cn>
2017-08-31 15:06:48 +0800

    gluster开发

--- crossbuild.sh
+++ crossbuild.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-./cbuild gluster_0.6 src/gapp/gluster/gluster.go
+# 生成交叉编译的可执行文件cbuild
+go build src/gapp/cbuild/cbuild.go
+# 开始交叉编译gluster
+#./cbuild src/gapp/gluster/gluster.go --name=gluster --version=0.6 --arch=386,amd64 --os=linux,windows,darwin,freebsd,openbsd
+./cbuild src/gapp/gluster/gluster.go --name=gluster --version=0.6 --arch=386,amd64 --os=linux,windows
+# 删除临时可文件
+rm cbuild
