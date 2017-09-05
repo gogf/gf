788b76b3574a4ed6c97cea672bdcb314f32c5b48
John <john@johng.cn>
2017-09-05 17:38:52 +0800

    gluster细节处理，性能优化

--- buildandcp.sh
+++ buildandcp.sh
@@ -2,12 +2,12 @@
 # 编译
 go build src/gapp/gluster/gluster.go
 ## 同步执行文件到测试节点
-#sshpass -p 123456 scp -P 22 ./gluster john@192.168.2.147:/home/john/gluster
-#sshpass -p 123456 scp -P 22 ./gluster john@192.168.2.186:/home/john/gluster
-#sshpass -p 123456 scp -P 22 ./gluster john@192.168.2.196:/home/john/gluster
-## 同步配置文件到测试节点
-#sshpass -p 123456 scp -P 22 ./src/gapp/gluster/gluster_server.json john@192.168.2.147:/home/john/gluster.json
-#sshpass -p 123456 scp -P 22 ./src/gapp/gluster/gluster_server.json john@192.168.2.186:/home/john/gluster.json
-#sshpass -p 123456 scp -P 22 ./src/gapp/gluster/gluster_server.json john@192.168.2.196:/home/john/gluster.json
+sshpass -p 123456 scp -P 22 ./gluster john@192.168.2.147:/home/john/gluster
+sshpass -p 123456 scp -P 22 ./gluster john@192.168.2.186:/home/john/gluster
+sshpass -p 123456 scp -P 22 ./gluster john@192.168.2.196:/home/john/gluster
+# 同步配置文件到测试节点
+sshpass -p 123456 scp -P 22 ./src/gapp/gluster/gluster_server.json john@192.168.2.147:/home/john/gluster.json
+sshpass -p 123456 scp -P 22 ./src/gapp/gluster/gluster_server.json john@192.168.2.186:/home/john/gluster.json
+sshpass -p 123456 scp -P 22 ./src/gapp/gluster/gluster_server.json john@192.168.2.196:/home/john/gluster.json
 ##删除本地执行文件
 #rm gluster
