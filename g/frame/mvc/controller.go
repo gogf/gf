95f5097c8a40610f7b8ce9409ca19200ac819338
John <john@johng.cn>
2017-12-08 09:50:11 +0800

    mvc框架结构完善

---
+++ g/frame/mvc/controller.go
@@ -0,0 +1,8 @@
+package mvc
+
+import "gitee.com/johng/gf/g/net/ghttp"
+
+// 控制器基类
+type Controller struct {
+    ghttp.Controller
+}
---
+++ g/frame/mvc/controller_rest.go
@@ -0,0 +1,8 @@
+package mvc
+
+import "gitee.com/johng/gf/g/net/ghttp"
+
+// 基于RESTful格式的控制器
+type Controller_Rest struct {
+    ghttp.ControllerRest
+}
