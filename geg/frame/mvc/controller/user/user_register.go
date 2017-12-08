2a7c4e8029934e70acd217d1bd6871ccae43d9e3
John <john@johng.cn>
2017-12-08 09:50:11 +0800

    mvc框架结构完善

---
+++ geg/frame/mvc/controller/user/user_register.go
@@ -0,0 +1,25 @@
+package user
+
+import (
+    "gitee.com/johng/gf/g/net/ghttp"
+    "gitee.com/johng/gf/g/frame/mvc"
+)
+
+// 定义业务相关的控制器对象
+type Controller_User_Register struct {
+    mvc.Controller
+}
+
+// 初始化控制器对象，并绑定操作到Web Server
+func init() {
+    ur := &Controller_User_Register{}
+    ghttp.GetServer("johng.cn").BindHandle("/user/register", ur.Show)
+}
+
+// 定义操作逻辑
+func (cu *Controller_User_Register) Show(r *ghttp.ClientRequest, w *ghttp.ServerResponse) {
+    w.Write([]byte("register page"))
+}
+
+
+
