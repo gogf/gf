aed07b824680152fe5f47a8be40624ca39e17c9b
John <john@johng.cn>
2017-12-11 18:21:30 +0800

    gsession开发

--- g/net/ghttp/http_controller.go
+++ g/net/ghttp/http_controller.go
@@ -1,5 +1,7 @@
 package ghttp
 
+import "gitee.com/johng/gf/g/net/gsession"
+
 // 控制器基类
 type Controller struct {
 
@@ -14,3 +16,24 @@ func (c *Controller) Patch(*ClientRequest, *ServerResponse)   {}
 func (c *Controller) Connect(*ClientRequest, *ServerResponse) {}
 func (c *Controller) Options(*ClientRequest, *ServerResponse) {}
 func (c *Controller) Trace(*ClientRequest, *ServerResponse)   {}
+
+// 获取当前请求的session对象
+func (c *Controller) Session(r *ClientRequest, w *ServerResponse) *gsession.Session {
+    sessionid := ""
+    if r, err := r.Cookie("gfsessionid"); err == nil {
+        sessionid = r.Value
+    } else {
+        sessionid = gsession.Id()
+    }
+    return gsession.Get(sessionid)
+}
+
+// 请求初始化时的回调函数
+func (c *Controller) __init(r *ClientRequest, w *ServerResponse) {
+
+}
+
+// 请求结束时的回调函数
+func (c *Controller) __shut(r *ClientRequest, w *ServerResponse) {
+    
+}
