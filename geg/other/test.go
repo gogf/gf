a8faa2d3e81c19c7ae2f8aef6f952a4b7fb32fb8
John <john@johng.cn>
2017-12-26 11:46:48 +0800

    ghttp支持绑定实例化对象

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,11 +1,24 @@
 package main
+
 import (
-    "sort"
     "fmt"
+    "reflect"
 )
 
+type Test struct {
+    name string
+}
+
+func (t *Test) Show(s string) {
+    fmt.Println(t.name)
+    fmt.Println(s)
+}
+
+type F func(string)
+
 func main() {
-    fnums := []uint64{0,12,2,4,5,5,2,uint64(10)}
-    sort.Slice(fnums, func(i, j int) bool { return fnums[i] < fnums[j] })
-    fmt.Println(fnums)
+    t := &Test{}
+    t.name = "john"
+    reflect.ValueOf(t).Method(0).Interface().(F)("123")
+
 }
