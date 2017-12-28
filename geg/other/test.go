e35aea0dae19208ef56108947570758b8d899f05
John <john@johng.cn>
2017-12-28 10:54:36 +0800

    未完成的模块新建分支管理，从master分支中剔除掉

--- geg/other/test.go
+++ geg/other/test.go
@@ -2,23 +2,10 @@ package main
 
 import (
     "fmt"
-    "reflect"
+    "math"
 )
 
-type Test struct {
-    name string
-}
-
-func (t *Test) Show(s string) {
-    fmt.Println(t.name)
-    fmt.Println(s)
-}
-
-type F func(string)
 
 func main() {
-    t := &Test{}
-    t.name = "john"
-    reflect.ValueOf(t).Method(0).Interface().(F)("123")
-
+    fmt.Printf("given id not match current maxid [%d]", int(math.MaxInt64))
 }
