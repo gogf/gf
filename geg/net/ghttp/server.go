c8d76d940038a56463cd56a426687e3ad154663b
John <john@johng.cn>
2017-12-31 20:00:09 +0800

    增加glog示例代码，完善glog backtrace

--- geg/net/ghttp/server.go
+++ geg/net/ghttp/server.go
@@ -1,12 +1,9 @@
 package main
 
-import (
-    "gitee.com/johng/gf/g/net/ghttp"
-)
+import "gitee.com/johng/gf/g/net/ghttp"
 
 func main() {
     s := ghttp.GetServer()
-    s.SetAddr(":8199")
     s.SetIndexFolder(true)
     s.SetServerRoot("/tmp")
     s.Run()
