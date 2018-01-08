5b4c3868dde72029dfbcdc2e9152a4dba48cc849
John <john@johng.cn>
2018-01-08 11:38:31 +0800

    完善ghttp服务注册

---
+++ geg/frame/mvc/controller/demo/object_method.go
@@ -0,0 +1,30 @@
+package demo
+
+import (
+    "gitee.com/johng/gf/g/net/ghttp"
+)
+
+type ObjectMethod struct {}
+
+func init() {
+    ghttp.GetServer().BindObjectMethod("/object-method", &ObjectMethod{}, "Show1, Show2, Show3")
+    ghttp.GetServer().Domain("localhost").BindObjectMethod("/object-method", &ObjectMethod{}, "Show4")
+}
+
+func (o *ObjectMethod) Show1(r *ghttp.Request) {
+    r.Response.WriteString("show 1")
+}
+
+func (o *ObjectMethod) Show2(r *ghttp.Request) {
+    r.Response.WriteString("show 2")
+}
+
+func (o *ObjectMethod) Show3(r *ghttp.Request) {
+    r.Response.WriteString("show 3")
+}
+
+func (o *ObjectMethod) Show4(r *ghttp.Request) {
+    r.Response.WriteString("show 4")
+}
+
+
