afd9c4a5a52869fc21c71c26f449f9fc8ca9ed8e
John <john@johng.cn>
2018-01-11 17:05:10 +0800

    完善ghttp客户端示例

--- geg/net/ghttp/upload_client.go
+++ geg/net/ghttp/upload_client.go
@@ -2,13 +2,13 @@ package main
 
 import (
     "fmt"
-    "gitee.com/johng/gf/g/net/ghttp"
     "gitee.com/johng/gf/g/os/glog"
+    "gitee.com/johng/gf/g/net/ghttp"
 )
 
 func main() {
-    path := "/home/john/Workspace/Go/GOPATH/src/gitee.com/johng/gf/version.go"
-    r, e := ghttp.Post("http://127.0.0.1:8199/upload?page=1", "name=john&upload-file=@file:" + path)
+    path := "/home/john/Workspace/Go/gitee.com/johng/gf/version.go"
+    r, e := ghttp.Post("http://127.0.0.1:8199/upload", "name=john&age=18&upload-file=@file:" + path)
     if e != nil {
         glog.Error(e)
     } else {
