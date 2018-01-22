aacc75594f846a23b32a0aaeb5609d35c0ecdb1f
John <john@johng.cn>
2018-01-22 17:55:12 +0800

    gjson模块支持内容修改特性，开发中

--- geg/other/test.go
+++ geg/other/test.go
@@ -2,16 +2,28 @@ package main
 
 import (
     "fmt"
+    "gitee.com/johng/gf/g/encoding/gjson"
+    "gitee.com/johng/gf/g/os/glog"
 )
 
 func main() {
-    c1 := make(chan int, 2)
-    c2 := make(chan int, 5)
-    c1 <- 1
-    c1 <- 2
-    c2  = c1
-    c2 <- 3
-    fmt.Println(<-c2)
-    fmt.Println(<-c2)
-    fmt.Println(<-c2)
+    data :=
+        `{
+            "users" : {
+                "count" : 100
+            }
+        }`
+    j, err := gjson.DecodeToJson([]byte(data))
+    if err != nil {
+        glog.Error(err)
+    } else {
+        j.Set("users.count",  1)
+        j.Set("users.list",  []string{"John", "小明"})
+        j.Set("users.0",     "a")
+        fmt.Println(j.Get("users.count"))
+        fmt.Println(j.Get("users.count"))
+        //fmt.Println(j.Get("users.list"))
+        c, _ := j.ToJson()
+        fmt.Println(string(c))
+    }
 }
