a66542e8fdf9a70b3c77a2d05c17b83d57fe80c6
John <john@johng.cn>
2018-01-23 16:40:48 +0800

    完善gjson和gparser

--- geg/other/test.go
+++ geg/other/test.go
@@ -2,35 +2,14 @@ package main
 
 import (
     "fmt"
-    "gitee.com/johng/gf/g/encoding/gjson"
-    "gitee.com/johng/gf/g/os/glog"
+    "gitee.com/johng/gf/g/encoding/gparser"
 )
 
 func main() {
-    j, _ := gjson.Load("/home/john/Workspace/Go/GOPATH/src/gitee.com/johng/gf/geg/frame/config.yml")
-    //c, _ := j.ToToml()
-    //fmt.Println(j.Get("database.default").([]interface{})[0])
-    fmt.Println(j.Get("database.default.0"))
-    return
-    data :=
-        `{
-            "users" : {
-                "count" : 100
-            }
-        }`
-    j, err := gjson.DecodeToJson([]byte(data))
-    if err != nil {
-        glog.Error(err)
-    } else {
-        //j.Set("users.count",  1)
-        j.Set("users.list",  []string{"John", "小明"})
-        fmt.Println(j.Set("users.list.10",  []string{"John", "小明10"}))
-        fmt.Println(j.Set("users.list.9",  []string{"John", "小明9"}))
-        j.Set("users",     "a")
-        //fmt.Println(j.Get("users.count"))
-        //fmt.Println(j.Get("users.count"))
-        fmt.Println(j.Get("users.list.10"))
-        c, _ := j.ToJson()
-        fmt.Println(string(c))
-    }
+    f := gparser.New()
+    f.Set("name", "john")
+    f.Set("name", "john2")
+    c, e := f.ToJson()
+    fmt.Println(e)
+    fmt.Println(string(c))
 }
