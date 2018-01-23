851fb91d2ba1a971ef4c71c1907ee2f8f6a6e8a9
John <john@johng.cn>
2018-01-23 15:02:42 +0800

    初步完善gjson功能，新增gparser包，用于编码/解析json,xml,yaml,toml数据格式内容

--- geg/other/test.go
+++ geg/other/test.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+    j, _ := gjson.Load("/home/john/Workspace/Go/GOPATH/src/gitee.com/johng/gf/geg/frame/config.json")
+    c, _ := j.ToToml()
+    fmt.Println(string(c))
+    return
     data :=
         `{
             "users" : {
@@ -17,12 +21,14 @@ func main() {
     if err != nil {
         glog.Error(err)
     } else {
-        j.Set("users.count",  1)
+        //j.Set("users.count",  1)
         j.Set("users.list",  []string{"John", "小明"})
-        j.Set("users.0",     "a")
-        fmt.Println(j.Get("users.count"))
-        fmt.Println(j.Get("users.count"))
-        //fmt.Println(j.Get("users.list"))
+        fmt.Println(j.Set("users.list.10",  []string{"John", "小明10"}))
+        fmt.Println(j.Set("users.list.9",  []string{"John", "小明9"}))
+        j.Set("users",     "a")
+        //fmt.Println(j.Get("users.count"))
+        //fmt.Println(j.Get("users.count"))
+        fmt.Println(j.Get("users.list.10"))
         c, _ := j.ToJson()
         fmt.Println(string(c))
     }
