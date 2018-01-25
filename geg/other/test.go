5b8d54f15e4e67ac712e531e0cef56d74a05ee16
John <john@johng.cn>
2018-01-25 17:43:07 +0800

    gparser功能修复及测试

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-    "fmt"
-    "gitee.com/johng/gf/g/encoding/gparser"
-)
 
 func main() {
-    f := gparser.New()
-    f.Set("name", "john")
-    f.Set("name", "john2")
-    c, e := f.ToJson()
-    fmt.Println(e)
-    fmt.Println(string(c))
+    a := make([]int, 0, 10)
+    a[0] = 1
 }
