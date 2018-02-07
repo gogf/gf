1e61951e68de4d062988b6220f08bed11937aad2
John <john@johng.cn>
2018-02-07 09:42:18 +0800

    完善gchan和gqueue注释

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,12 +1,15 @@
 package main
 
-import (
-    "time"
-)
 
 func main() {
+    defer func() {
+        recover()
+    }()
     events1 := make(chan int, 100)
-    events2 := make(chan int, 100)
+    events1 <- 1
+    close(events1)
+    events1 <- 2
+    //events2 := make(chan int, 100)
     //go func() {
     //    for{
     //        v := <- events1
@@ -15,18 +18,18 @@ func main() {
     //
     //}()
 
-    go func() {
-        time.Sleep(2*time.Second)
-        events1 <- 1
-        events2 <- 2
-        time.Sleep(2*time.Second)
-        close(events1)
-        close(events2)
-        events1 <- 1
-        events2 <- 2
-    }()
-
-    select {
-
-    }
+    //go func() {
+    //    time.Sleep(2*time.Second)
+    //    events1 <- 1
+    //    events2 <- 2
+    //    time.Sleep(2*time.Second)
+    //    close(events1)
+    //    close(events2)
+    //    events1 <- 1
+    //    events2 <- 2
+    //}()
+    //
+    //select {
+    //
+    //}
 }
