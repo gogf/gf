f369df4092ce7bc53ac00d03eeebf3c600cffad1
John <john@johng.cn>
2018-02-12 15:33:19 +0800

    改进gcache设计

---
+++ geg/os/gcache.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "gitee.com/johng/gf/g/os/gcache"
+    "time"
+    "fmt"
+)
+
+func main() {
+    gcache.Set("k1", "v1", 1000)
+    gcache.Set("k2", "v2", 2000)
+    time.Sleep(500*time.Millisecond)
+    fmt.Println(gcache.Get("k1"))
+    fmt.Println(gcache.Get("k2"))
+    time.Sleep(400*time.Millisecond)
+    fmt.Println(gcache.Get("k1"))
+    fmt.Println(gcache.Get("k2"))
+    time.Sleep(3000*time.Millisecond)
+    fmt.Println(gcache.Get("k1"))
+    fmt.Println(gcache.Get("k2"))
+    time.Sleep(3000*time.Millisecond)
+}
