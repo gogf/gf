bc47c68e9fc8374ac94f381d51b73956a71ab451
John <john@johng.cn>
2018-02-28 14:16:24 +0800

    完善gqueue基准测试

--- g/container/gqueue/gqueue_test.go
+++ g/container/gqueue/gqueue_test.go
@@ -14,11 +14,18 @@ import (
 )
 
 var length = 10000000
-var q = gqueue.New(length)
+var q      = gqueue.New(length)
+var c      = make(chan int, length)
 
 func BenchmarkGqueueNew1000W(b *testing.B) {
     for i := 0; i < b.N; i++ {
         gqueue.New(length)
+    }
+}
+
+func BenchmarkChannelNew1000W(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        c = make(chan int, length)
     }
 }
 
@@ -32,6 +39,13 @@ func BenchmarkGqueuePushAndPop(b *testing.B) {
     for i := 0; i < b.N; i++ {
         q.PushBack(i)
         q.PopFront()
+    }
+}
+
+func BenchmarkChannelPushAndPop(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        c <- i
+        <- c
     }
 }
 
