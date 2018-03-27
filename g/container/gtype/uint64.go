aaccbcf969a298d047eae210110beee87a39874b
John <john@johng.cn>
2018-03-27 15:09:17 +0800

    新增gtype包，并发安全的基本类型封装

---
+++ g/container/gtype/uint64.go
@@ -0,0 +1,34 @@
+// Copyright 2018 gf Author(https://gitee.com/johng/gf). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://gitee.com/johng/gf.
+
+package gtype
+
+import (
+    "sync/atomic"
+)
+
+type Uint64 struct {
+    val uint64
+}
+
+func NewUint64(value...uint64) *Uint64 {
+    if len(value) > 0 {
+        return &Uint64{val:value[0]}
+    }
+    return &Uint64{}
+}
+
+func (t *Uint64)Set(value uint64) {
+    atomic.StoreUint64(&t.val, value)
+}
+
+func (t *Uint64)Get() uint64 {
+    return atomic.LoadUint64(&t.val)
+}
+
+func (t *Uint64)Add(delta uint64) uint64 {
+    return atomic.AddUint64(&t.val, delta)
+}
