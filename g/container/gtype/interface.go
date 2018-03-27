dfbedf791251ef6840836f37ef80dbb4e9dea278
John <john@johng.cn>
2018-03-27 15:09:17 +0800

    新增gtype包，并发安全的基本类型封装

---
+++ g/container/gtype/interface.go
@@ -0,0 +1,36 @@
+// Copyright 2018 gf Author(https://gitee.com/johng/gf). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://gitee.com/johng/gf.
+
+package gtype
+
+import (
+    "sync"
+)
+
+type Interface struct {
+    mu  sync.RWMutex
+    val interface{}
+}
+
+func NewInterface(value...interface{}) *Interface {
+    if len(value) > 0 {
+        return &Interface{val:value[0]}
+    }
+    return &Interface{}
+}
+
+func (t *Interface)Set(value interface{}) {
+    t.mu.Lock()
+    t.val = value
+    t.mu.Unlock()
+}
+
+func (t *Interface)Get() interface{} {
+    t.mu.RLock()
+    b := t.val
+    t.mu.RUnlock()
+    return b
+}
