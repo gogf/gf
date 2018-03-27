38ad2d9bc65acade01f865f67208a9a62b787b36
John <john@johng.cn>
2018-03-27 15:09:17 +0800

    新增gtype包，并发安全的基本类型封装

---
+++ g/container/gtype/bool.go
@@ -0,0 +1,39 @@
+// Copyright 2018 gf Author(https://gitee.com/johng/gf). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://gitee.com/johng/gf.
+
+package gtype
+
+import (
+    "sync/atomic"
+)
+
+type Bool struct {
+    val int32
+}
+
+func NewBool(value...bool) *Bool {
+    t := &Bool{}
+    if len(value) > 0 {
+        if value[0] {
+            t.val = 1
+        } else {
+            t.val = 0
+        }
+    }
+    return t
+}
+
+func (t *Bool)Set(value bool) {
+    if value {
+        atomic.StoreInt32(&t.val, 1)
+    } else {
+        atomic.StoreInt32(&t.val, 0)
+    }
+}
+
+func (t *Bool)Get() bool {
+    return atomic.LoadInt32(&t.val) > 0
+}
---
+++ g/container/gtype/uint32.go
@@ -0,0 +1,34 @@
+// Copyright 2018 gf Author(https://gitee.com/johng/gf). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://gitee.com/johng/gf.
+
+package gtype
+
+import (
+    "sync/atomic"
+)
+
+type Uint32 struct {
+    val uint32
+}
+
+func NewUint32(value...uint32) *Uint32 {
+    if len(value) > 0 {
+        return &Uint32{val:value[0]}
+    }
+    return &Uint32{}
+}
+
+func (t *Uint32)Set(value uint32) {
+    atomic.StoreUint32(&t.val, value)
+}
+
+func (t *Uint32)Get() uint32 {
+    return atomic.LoadUint32(&t.val)
+}
+
+func (t *Uint32)Add(delta uint32) uint32 {
+    return atomic.AddUint32(&t.val, delta)
+}
