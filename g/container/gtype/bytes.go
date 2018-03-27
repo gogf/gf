73339085ece1b5a789975bb0cb9f3b4ebed9f096
John <john@johng.cn>
2018-03-27 15:09:17 +0800

    新增gtype包，并发安全的基本类型封装

---
+++ g/container/gtype/bytes.go
@@ -0,0 +1,36 @@
+// Copyright 2018 gf Author(https://gitee.com/johng/gf). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://gitee.com/johng/gf.
+
+package gtype
+
+import (
+    "sync"
+)
+
+type Bytes struct {
+    mu  sync.RWMutex
+    val []byte
+}
+
+func NewBytes(value...[]byte) *Bytes {
+    if len(value) > 0 {
+        return &Bytes{val:value[0]}
+    }
+    return &Bytes{}
+}
+
+func (t *Bytes)Set(value []byte) {
+    t.mu.Lock()
+    t.val = value
+    t.mu.Unlock()
+}
+
+func (t *Bytes)Get() []byte {
+    t.mu.RLock()
+    b := t.val
+    t.mu.RUnlock()
+    return b
+}
---
+++ g/container/gtype/int64.go
@@ -0,0 +1,34 @@
+// Copyright 2018 gf Author(https://gitee.com/johng/gf). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://gitee.com/johng/gf.
+
+package gtype
+
+import (
+    "sync/atomic"
+)
+
+type Int64 struct {
+    val int64
+}
+
+func NewInt64(value...int64) *Int64 {
+    if len(value) > 0 {
+        return &Int64{val:value[0]}
+    }
+    return &Int64{}
+}
+
+func (t *Int64)Set(value int64) {
+    atomic.StoreInt64(&t.val, value)
+}
+
+func (t *Int64)Get() int64 {
+    return atomic.LoadInt64(&t.val)
+}
+
+func (t *Int64)Add(delta int64) int64 {
+    return atomic.AddInt64(&t.val, delta)
+}
