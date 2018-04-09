3c9cf22b37ef5cc9362c70198a2c7d347ff5b332
John <john@johng.cn>
2018-04-09 17:55:46 +0800

    ghttp.Server增加事件回调特性，去掉Request ID生成器，采用性能更高的gtype包实现请求ID生成

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,56 +1,10 @@
 package main
 
 import (
-    "gitee.com/johng/gf/g/database/gdb"
     "fmt"
+    "math"
 )
 
-type Model struct {
-    TableName string
-}
-
-var Db *gdb.Db
-
-func init() {
-    gdb.AddDefaultConfigNode(gdb.ConfigNode {
-        Host    : "127.0.0.1",
-        Port    : "3306",
-        User    : "root",
-        Pass    : "123456",
-        Name    : "test",
-        Type    : "mysql",
-        Role    : "master",
-        Charset : "utf8",
-    })
-    var err error
-    Db, err = gdb.Instance()
-    checkErr(err)
-}
-
-func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
-}
-
-type UserModel struct {
-    Model
-}
-
-func (u *UserModel) Get() (user gdb.Map){
-    user, _ = Db.Table("user").Fields("uid, nickname, email").Where("uid = ?", 15).One()
-    return
-}
-
-func (u *UserModel) Insert(data gdb.Map) (id int64) {
-    ret, _ := Db.Table("user").Data(data).Insert()
-    id, _   = ret.LastInsertId()
-    return
-}
-
 func main() {
-    u    := &UserModel{}
-    user := u.Get()
-    fmt.Println(user)
-    u.Insert(gdb.Map{"uid": 100, "name": "jack"})
+    fmt.Println(math.MaxInt64)
 }
