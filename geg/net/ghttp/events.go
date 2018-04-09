56d7513e79dd25ea5a62d976eda0aadc13a91f84
John <john@johng.cn>
2018-04-09 18:51:46 +0800

    改进ghttp事件回调名称

--- geg/net/ghttp/events.go
+++ geg/net/ghttp/events.go
@@ -8,16 +8,14 @@ import (
 func main() {
     pattern := "/"
     ghttp.GetServer().BindHookHandlerByMap(pattern, map[string]ghttp.HandlerFunc{
-        "BeforeServe"        : func(r *ghttp.Request){ fmt.Println("BeforeServe") },
-        "AfterServe"         : func(r *ghttp.Request){ fmt.Println("AfterServe") },
-        "BeforeRouterPatch"  : func(r *ghttp.Request){ fmt.Println("BeforeRouterPatch") },
-        "AfterRouterPatch"   : func(r *ghttp.Request){ fmt.Println("AfterRouterPatch") },
-        "BeforeCookieOutput" : func(r *ghttp.Request){ fmt.Println("BeforeCookieOutput") },
-        "AfterCookieOutput"  : func(r *ghttp.Request){ fmt.Println("AfterCookieOutput") },
-        "BeforeBufferOutput" : func(r *ghttp.Request){ fmt.Println("BeforeBufferOutput") },
-        "AfterBufferOutput"  : func(r *ghttp.Request){ fmt.Println("AfterBufferOutput") },
-        "BeforeRequestClose" : func(r *ghttp.Request){ fmt.Println("BeforeRequestClose") },
-        "AfterRequestClose"  : func(r *ghttp.Request){ fmt.Println("AfterRequestClose") },
+        "BeforeServe"  : func(r *ghttp.Request){ fmt.Println("BeforeServe") },
+        "AfterServe"   : func(r *ghttp.Request){ fmt.Println("AfterServe") },
+        "BeforePatch"  : func(r *ghttp.Request){ fmt.Println("BeforePatch") },
+        "AfterPatch"   : func(r *ghttp.Request){ fmt.Println("AfterPatch") },
+        "BeforeOutput" : func(r *ghttp.Request){ fmt.Println("BeforeOutput") },
+        "AfterOutput"  : func(r *ghttp.Request){ fmt.Println("AfterOutput") },
+        "BeforeClose"  : func(r *ghttp.Request){ fmt.Println("BeforeClose") },
+        "AfterClose"   : func(r *ghttp.Request){ fmt.Println("AfterClose") },
     })
     ghttp.GetServer().BindHandler(pattern, func(r *ghttp.Request) {
         r.Response.WriteString("Hello World!")
