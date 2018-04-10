f0650504c635c532e64b37d89150efa88d6bd29a
John <john@johng.cn>
2018-04-10 15:52:58 +0800

    glog日志内容增加毫秒显示

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "github.com/fatih/structs"
+    "gitee.com/johng/gf/g/os/glog"
 )
 
-type T struct {
-    Name string
-    Age  int
-    S    struct {
-        N string
-    }
-}
-
-func (t *T) Test2Test() {}
-
 func main() {
-    v := &T{
-        Name : "john",
-        Age  : 18,
-    }
-    v.S.N = "ttt"
-    fmt.Println(structs.Map(v))
+    glog.Println(1)
 
 }
