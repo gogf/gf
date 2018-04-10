6b974fba10740f40b89a9d3fda6a4e46be102e05
John <john@johng.cn>
2018-04-10 15:36:35 +0800

    完善gcfg包使用示例

---
+++ geg/os/gcfg/gcfg1.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "gitee.com/johng/gf/g/os/gcfg"
+)
+
+func main() {
+    c              := gcfg.New("/home/john/Workspace/Go/GOPATH/src/gitee.com/johng/gf/geg/os/gcfg")
+    redisConfig    := c.GetArray("redis-cache", "redis.yml")
+    memcacheConfig := c.GetArray("", "memcache.yml")
+    fmt.Println(redisConfig)
+    fmt.Println(memcacheConfig)
+}
+
