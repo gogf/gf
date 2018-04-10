58095ac006210ccc50fde9df5a42fa620022867f
John <john@johng.cn>
2018-04-10 15:52:58 +0800

    glog日志内容增加毫秒显示

--- g/os/glog/glog.go
+++ g/os/glog/glog.go
@@ -302,7 +302,7 @@ func (l *Logger) backtrace() string {
 }
 
 func (l *Logger) format(s string) string {
-    return time.Now().Format("2006-01-02 15:04:05 ") + s
+    return time.Now().Format("2006-01-02 15:04:05.000 ") + s
 }
 
 func (l *Logger) Print(v ...interface{}) {
