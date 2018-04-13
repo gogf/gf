5f267c10e024a5f0fb7b323932fbb5122b43d304
John <john@johng.cn>
2018-04-13 16:16:12 +0800

    完成ghttp.Server动态路由规则特性，修复带HTTPMethod的服务注册规则问题

--- geg/net/ghttp/hello.go
+++ geg/net/ghttp/hello.go
@@ -3,19 +3,23 @@ package main
 import "gitee.com/johng/gf/g/net/ghttp"
 
 func main() {
-    ghttp.GetServer().BindHandler("/:name/*any", func(r *ghttp.Request) {
-        r.Response.WriteString("any")
-        r.Response.WriteString(r.GetQueryString("name"))
-        r.Response.WriteString(r.GetQueryString("any"))
+    ghttp.GetServer().BindHandler("get:/h", func(r *ghttp.Request) {
+        r.Response.WriteString("hello world")
     })
-    //ghttp.GetServer().BindHandler("/:name/action", func(r *ghttp.Request) {
+
+    //ghttp.GetServer().BindHandler("/:name/*any", func(r *ghttp.Request) {
+    //    r.Response.WriteString("any")
     //    r.Response.WriteString(r.GetQueryString("name"))
+    //    r.Response.WriteString(r.GetQueryString("any"))
     //})
-    ghttp.GetServer().BindHandler("/:name/:action/:aaa", func(r *ghttp.Request) {
-        r.Response.WriteString("name")
-        r.Response.WriteString(r.GetQueryString("name"))
-        r.Response.WriteString(r.GetQueryString("action"))
-    })
+    ////ghttp.GetServer().BindHandler("/:name/action", func(r *ghttp.Request) {
+    ////    r.Response.WriteString(r.GetQueryString("name"))
+    ////})
+    //ghttp.GetServer().BindHandler("/:name/:action/:aaa", func(r *ghttp.Request) {
+    //    r.Response.WriteString("name")
+    //    r.Response.WriteString(r.GetQueryString("name"))
+    //    r.Response.WriteString(r.GetQueryString("action"))
+    //})
     ghttp.GetServer().SetPort(10000)
     ghttp.GetServer().Run()
 }
