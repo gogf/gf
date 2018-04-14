200a85421ab995f1ca73532faaed9bf7cd6c535f
John <john@johng.cn>
2018-04-14 21:24:42 +0800

    README updates

--- geg/net/ghttp/hello.go
+++ geg/net/ghttp/hello.go
@@ -5,25 +5,25 @@ import (
 )
 
 func main() {
-    ghttp.GetServer().BindHandler("get:/h", func(r *ghttp.Request) {
+    ghttp.GetServer().BindHandler("/", func(r *ghttp.Request) {
         //r.Response.RedirectTo("http://www.baidu.com/")
-        r.Response.Write("hello world")
+        r.Response.Write("哈喽世界！")
         //r.Response.WriteStatus(302)
     })
 
-    ghttp.GetServer().BindHandler("/:name/*any", func(r *ghttp.Request) {
-        r.Response.Write("any")
-        r.Response.Write(r.Get("name"))
-        r.Response.Write(r.Get("any"))
-    })
-    //ghttp.GetServer().BindHandler("/:name/action", func(r *ghttp.Request) {
+    //ghttp.GetServer().BindHandler("/:name/*any", func(r *ghttp.Request) {
+    //    r.Response.Write("any")
     //    r.Response.Write(r.Get("name"))
+    //    r.Response.Write(r.Get("any"))
     //})
-    ghttp.GetServer().BindHandler("/:name/:action/:aaa", func(r *ghttp.Request) {
-        r.Response.Write("name")
-        r.Response.Write(r.Get("name"))
-        r.Response.Write(r.Get("action"))
-    })
+    ////ghttp.GetServer().BindHandler("/:name/action", func(r *ghttp.Request) {
+    ////    r.Response.Write(r.Get("name"))
+    ////})
+    //ghttp.GetServer().BindHandler("/:name/:action/:aaa", func(r *ghttp.Request) {
+    //    r.Response.Write("name")
+    //    r.Response.Write(r.Get("name"))
+    //    r.Response.Write(r.Get("action"))
+    //})
     ghttp.GetServer().SetPort(10000)
     ghttp.GetServer().Run()
 }
