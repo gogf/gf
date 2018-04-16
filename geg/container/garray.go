ab512f7cb25b664d49916b547e0cdd140b74b1be
John <john@johng.cn>
2018-04-16 17:59:23 +0800

    garray开发

---
+++ geg/container/garray.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "gitee.com/johng/gf/g/container/garray"
+)
+
+
+func main () {
+    a := garray.NewSortedArray(0, 0, func(v1, v2 interface{}) int {
+        if v1.(int) < v2.(int) {
+            return -1
+        }
+        if v1.(int) > v2.(int) {
+            return 1
+        }
+        return 0
+    })
+    a.Add(10)
+    a.Add(20)
+    a.Add(30)
+    a.Add(1)
+    a.Add(1)
+    a.Add(1)
+    a.Add(1)
+    a.DoUnique()
+    fmt.Println(a.Slice())
+}
