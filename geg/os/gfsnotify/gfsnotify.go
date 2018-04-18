157d15302c9f9a890b5c41c78d5ddc6e24f06a5f
John <john@johng.cn>
2018-04-18 15:17:07 +0800

    完成gfsnotify开发

--- geg/os/gfsnotify/gfsnotify.go
+++ geg/os/gfsnotify/gfsnotify.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-    "fmt"
+    "log"
     "gitee.com/johng/gf/g/os/gfsnotify"
 )
 
 func main() {
     err := gfsnotify.Add("/home/john/Documents/temp", func(event *gfsnotify.Event) {
-        fmt.Println(event)
+        if event.IsCreate() {
+            log.Println("创建文件 : ", event.Path)
+        }
+        if event.IsWrite() {
+            log.Println("写入文件 : ", event.Path)
+        }
+        if event.IsRemove() {
+            log.Println("删除文件 : ", event.Path)
+        }
+        if event.IsRename() {
+            log.Println("重命名文件 : ", event.Path)
+        }
+        if event.IsChmod() {
+            log.Println("修改权限 : ", event.Path)
+        }
     })
-    fmt.Println(err)
-    select {
-
+    if err != nil {
+        log.Fatalln(err)
+    } else {
+        select {}
     }
 }
