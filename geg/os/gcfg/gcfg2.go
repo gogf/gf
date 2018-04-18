31c72feba8b34d2dc3acebb3ecb9d11d84a24dec
John <john@johng.cn>
2018-04-18 16:00:47 +0800

    gcfg包增加配置文件自动更新检测特性

--- geg/os/gcfg/gcfg2.go
+++ geg/os/gcfg/gcfg2.go
@@ -3,14 +3,10 @@ package main
 import (
     "fmt"
     "gitee.com/johng/gf/g/os/gcfg"
-    "time"
 )
 
 func main() {
     c := gcfg.New("/home/john/Workspace/Go/GOPATH/src/gitee.com/johng/gf/geg/os/gcfg")
-    fmt.Println(c.GetArray("memcache"))
-    time.Sleep(10*time.Second)
-    // 给你10秒钟的时间修改配置，下一次读取会自动更新
     fmt.Println(c.GetArray("memcache"))
 }
 
