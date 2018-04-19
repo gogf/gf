ff610892ad1633b91adc183287b6cacf5b387afd
John <john@johng.cn>
2018-04-19 16:24:48 +0800

    改进glog & gfilepool

--- geg/other/test.go
+++ geg/other/test.go
@@ -2,12 +2,19 @@ package main
 
 import (
     "gitee.com/johng/gf/g/net/ghttp"
+    "gitee.com/johng/gf/g/frame/gins"
 )
 
 func main() {
     s := ghttp.GetServer()
-    s.SetServerRoot("/home/john/Documents")
-    s.SetIndexFolder(true)
-    s.SetPort(8199)
+    s.BindHandler("/template2", func(r *ghttp.Request){
+        tplcontent := `id:{{.id}}, name:{{.name}}`
+        content, _ := gins.View().ParseContent(tplcontent, map[string]interface{}{
+            "id"   : 123,
+            "name" : "john",
+        })
+        r.Response.Write(content)
+    })
+    //s.SetPort(8199)
     s.Run()
 }
