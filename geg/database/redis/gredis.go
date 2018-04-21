b0865bdb24ebd8412f37c01903d8eb95928b8f85
John <john@johng.cn>
2018-04-21 10:50:04 +0800

    完善redis示例

--- geg/database/redis/gredis.go
+++ geg/database/redis/gredis.go
@@ -2,14 +2,17 @@ package main
 
 import (
     "fmt"
+    "gitee.com/johng/gf/g/util/gconv"
     "gitee.com/johng/gf/g/database/gredis"
 )
 
 func main() {
     redis := gredis.New("127.0.0.1:6379", 1)
-    fmt.Println(redis.Do("SET", "k1", "v1"))
-    fmt.Println(redis.Do("SET", "k2", "v3"))
-    fmt.Println(redis.Do("GET", "k2"))
-    fmt.Println(redis.Stats())
+    redis.Do("SET", "k1", "v1")
+    redis.Do("SET", "k2", "v2")
+    v1, _ := redis.Do("GET", "k1")
+    v2, _ := redis.Do("GET", "k1")
+    fmt.Println(gconv.String(v1))
+    fmt.Println(gconv.String(v2))
 }
 
