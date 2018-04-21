fe967f11bfdd26b8d29a47ebb3240b50e047125a
John <john@johng.cn>
2018-04-21 10:50:04 +0800

    完善redis示例

---
+++ geg/database/redis/gredis_gins.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "gitee.com/johng/gf/g/frame/gins"
+    "gitee.com/johng/gf/g/util/gconv"
+)
+
+func main() {
+    gins.Config().SetPath("/home/john/Workspace/Go/GOPATH/src/gitee.com/johng/gf/geg/frame")
+    redis := gins.Redis("cache")
+    redis.Do("SET", "k", "v")
+    v, _ := redis.Do("GET", "k")
+    fmt.Println(gconv.String(v))
+}
+
