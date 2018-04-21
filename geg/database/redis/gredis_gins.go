01b51ed27021a68b656f3f2c6ca17ad93568441a
John <john@johng.cn>
2018-04-21 15:36:09 +0800

    README updates

--- geg/database/redis/gredis_gins.go
+++ geg/database/redis/gredis_gins.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-    "fmt"
+    "time"
+    "gitee.com/johng/gf/g/os/gtime"
     "gitee.com/johng/gf/g/frame/gins"
-    "gitee.com/johng/gf/g/util/gconv"
 )
 
 func main() {
     gins.Config().SetPath("/home/john/Workspace/Go/GOPATH/src/gitee.com/johng/gf/geg/frame")
-    
-    redis := gins.Redis("cache")
-    redis.Do("SET", "k", "v")
-    v, _ := redis.Do("GET", "k")
-    fmt.Println(gconv.String(v))
+    gtime.SetInterval(2*time.Second, func() bool {
+        redis := gins.Redis("cache")
+        redis.Do("GET", "k")
+        return true
+    })
+    select{}
 }
 
