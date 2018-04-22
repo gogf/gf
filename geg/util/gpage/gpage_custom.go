6513a5a708d8dbc5e91edf3ffcd44fb48bfaa5e7
John <john@johng.cn>
2018-04-22 23:23:31 +0800

    gpage示例完善

--- geg/util/gpage/gpage_custom.go
+++ geg/util/gpage/gpage_custom.go
@@ -38,7 +38,7 @@ func main() {
             </body>
         </html>
         `, g.Map{
-            "page" : gview.HTML(pageContent(page)),
+            "page" : g.HTML(pageContent(page)),
         })
         r.Response.Write(buffer)
     })
