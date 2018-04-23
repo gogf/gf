28deec1273f117775dd18c3f8aa82276ace98a39
John <john@johng.cn>
2018-04-23 14:20:41 +0800

    改进静态文件目录安全访问控制

--- geg/net/ghttp/server1.go
+++ geg/net/ghttp/server1.go
@@ -1,10 +1,13 @@
 package main
 
-import "gitee.com/johng/gf/g/net/ghttp"
+import (
+    "gitee.com/johng/gf/g/net/ghttp"
+)
 
 func main() {
     s := ghttp.GetServer()
     s.SetIndexFolder(true)
-    s.SetServerRoot("/home/www/")
+    s.SetServerRoot("C:\\Documents and Settings\\Claymore\\桌面\\gf.test")
+    s.SetPort(8199)
     s.Run()
 }
