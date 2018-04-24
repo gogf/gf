0821c8cacdc5d09d53009747ab806290af216153
John <john@johng.cn>
2018-04-24 23:09:59 +0800

    改进gf orm以及redis客户端的封装

--- g/database/gdb/gdb_config.go
+++ g/database/gdb/gdb_config.go
@@ -37,7 +37,7 @@ type ConfigNode  struct {
     Name     string // 数据库名称
     Type     string // 数据库类型：mysql, sqlite, mssql, pgsql, oracle(目前仅支持mysql)
     Role     string // (可选，默认为master)数据库的角色，用于主从操作分离，至少需要有一个master，参数值：master, slave
-    Charset  string // (可选，默认为 utf-8)编码，默认为 utf-8
+    Charset  string // (可选，默认为 utf8)编码，默认为 utf8
     Priority    int // (可选)用于负载均衡的权重计算，当集群中只有一个节点时，权重没有任何意义
     Linkinfo string // (可选)自定义链接信息，当该字段被设置值时，以上链接字段(Host,Port,User,Pass,Name)将失效(该字段是一个扩展功能)
 }
@@ -55,7 +55,7 @@ var DatabaseConfiguration = Config {
             Name     : "test",
             Type     : "mysql",
             Role     : "master",
-            Charset  : "utf-8",
+            Charset  : "utf8",
             Priority : 100,
         },
         {
@@ -66,7 +66,7 @@ var DatabaseConfiguration = Config {
             Name     : "test",
             Type     : "mysql",
             Role     : "slave",
-            Charset  : "utf-8",
+            Charset  : "utf8",
             Priority : 100,
         },
     },
@@ -99,45 +99,6 @@ func AddConfigNode (group string, node ConfigNode) {
     config.c[group] = append(config.c[group], node)
     config.Unlock()
 }
-
-// 添加一台数据库服务器配置，通过解析规范的字符串配置实现
-// 配置格式：账号@地址:端口,密码,数据库名称,数据库类型[,集群角色(master|slave),字符编码,负载均衡优先级,自定义链接]
-//func AddConfigNodeByString (group string, nodestr string) {
-//    reg, _ := regexp.Compile(`(.+)@(.+):([^,]+),([^,]+),([^,]+),([^,]+)`)
-//    match  := reg.FindStringSubmatch(nodestr)
-//    if match != nil {
-//        node := ConfigNode{
-//            User : strings.TrimSpace(match[1]),
-//            Host : strings.TrimSpace(match[2]),
-//            Port : strings.TrimSpace(match[3]),
-//            Pass : strings.TrimSpace(match[4]),
-//            Name : strings.TrimSpace(match[5]),
-//            Type : strings.TrimSpace(match[6]),
-//        }
-//        if len(match[0]) + 1 < len(nodestr) {
-//            extra := strings.Split(nodestr[len(match[0]) + 1:], ",")
-//            if len(extra) > 0 {
-//                node.Role = strings.TrimSpace(extra[0])
-//            }
-//            if len(extra) > 1 {
-//                node.Charset = strings.TrimSpace(extra[1])
-//            }
-//            if len(extra) > 2 {
-//                node.Priority, _ = strconv.Atoi(strings.TrimSpace(extra[2]))
-//            }
-//            if len(extra) > 3 {
-//                index        := len(extra[0]) + len(extra[1]) + len(extra[2]) + 3
-//                node.Linkinfo = strings.TrimSpace(nodestr[len(match[0]) + 1 + index:])
-//            }
-//        }
-//        AddConfigNode(group, node)
-//    }
-//}
-
-// 添加默认链接的一台数据库服务器配置,通过解析规范的字符串配置实现
-//func AddDefaultConfigNodeByString (nodestr string) {
-//    AddConfigNodeByString(gDEFAULT_CONFIG_GROUP_NAME, nodestr)
-//}
 
 // 添加默认链接的一台数据库服务器配置
 func AddDefaultConfigNode (node ConfigNode) {
