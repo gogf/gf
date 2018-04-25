31e71dbd72828120787f3522b6a4230b1bfdbe5d
John <john@johng.cn>
2018-04-25 17:11:34 +0800

    修复ghttp路由缓存问题

--- geg/net/ghttp/domain.go
+++ geg/net/ghttp/domain.go
@@ -3,16 +3,17 @@ package main
 import "gitee.com/johng/gf/g/net/ghttp"
 
 func Hello1(r *ghttp.Request) {
-    r.Response.Write("Hello World1!")
+    r.Response.Write("127.0.0.1: Hello World1!")
 }
 
 func Hello2(r *ghttp.Request) {
-    r.Response.Write("Hello World2!")
+    r.Response.Write("localhost: Hello World2!")
 }
 
 func main() {
     s := ghttp.GetServer()
     s.Domain("127.0.0.1").BindHandler("/", Hello1)
     s.Domain("localhost").BindHandler("/", Hello2)
+    s.SetPort(8199)
     s.Run()
 }
