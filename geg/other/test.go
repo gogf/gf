11a2012315fc3251bec44feee3df834e030334dc
John <john@johng.cn>
2018-04-27 13:59:02 +0800

    gdb增加二级数据库连接池开发中

--- geg/other/test.go
+++ geg/other/test.go
@@ -3,9 +3,12 @@ package main
 import (
     "fmt"
     "math/rand"
+    "sync"
 )
 
 func main() {
+    p := &sync.Pool{}
+    p.Put()
     for i := 0; i < 100; i++ {
         fmt.Println(rand.Intn(200))
     }
