92800a1241d9b2f5b350ab54560e7ca04aa5535f
John <john@johng.cn>
2018-04-29 23:19:02 +0800

    ghttp.Request增加请求进入和完成时间记录，并增加到默认日志内容中

--- geg/net/ghttp/log.go
+++ geg/net/ghttp/log.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "fmt"
     "net/http"
     "gitee.com/johng/gf/g/net/ghttp"
 )
@@ -13,14 +12,14 @@ func main() {
     })
     s.SetAccessLogEnabled(true)
     s.SetErrorLogEnabled(true)
-    s.SetLogHandler(func(r *ghttp.Request, error ...interface{}) {
-        if len(error) > 0 {
-            // 如果是错误日志
-            fmt.Println("错误产生了：", error[0])
-        }
-        // 这里是请求日志
-        fmt.Println("请求处理完成，请求地址:", r.URL.String(), "请求结果:", r.Response.Status)
-    })
+    //s.SetLogHandler(func(r *ghttp.Request, error ...interface{}) {
+    //    if len(error) > 0 {
+    //        // 如果是错误日志
+    //        fmt.Println("错误产生了：", error[0])
+    //    }
+    //    // 这里是请求日志
+    //    fmt.Println("请求处理完成，请求地址:", r.URL.String(), "请求结果:", r.Response.Status)
+    //})
     s.SetPort(8199)
     s.Run()
 }
