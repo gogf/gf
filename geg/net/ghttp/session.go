ad9280ea7b55fde7c6f2e5157ec95200aba0ec3c
John <john@johng.cn>
2018-04-30 22:28:01 +0800

    改进ghttp.Server Session示例

---
+++ geg/net/ghttp/session.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "gitee.com/johng/gf/g"
+    "gitee.com/johng/gf/g/net/ghttp"
+)
+
+
+func main() {
+    s := g.Server()
+    s.BindHandler("/session", func(r *ghttp.Request) {
+        id := r.Session.GetInt("id")
+        r.Session.Set("id", id + 1)
+        r.Response.Write("id:", id)
+    })
+    s.SetPort(8199)
+    s.Run()
+}
