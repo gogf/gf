adb38565552dd5447e3cc590c5f5c18d302663b7
John <john@johng.cn>
2018-04-30 22:28:01 +0800

    改进ghttp.Server Session示例

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-    "gitee.com/johng/gf/g"
-    "gitee.com/johng/gf/g/net/ghttp"
-)
 
 func main() {
-    s := g.Server()
-    s.BindHandler("/", func(r *ghttp.Request){
-        r.Response.Write("123")
-    })
-    s.SetPort(8199)
-    s.Run()
+
 }
