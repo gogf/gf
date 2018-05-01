449318ba338c3e23d080b846c4ff4f8e0c78a6db
John <john@johng.cn>
2018-05-01 11:03:53 +0800

    README updates

--- geg/other/test.go
+++ geg/other/test.go
@@ -2,15 +2,18 @@ package main
 
 import (
     "fmt"
-    "gitee.com/johng/gf/g/os/gtime"
-    "gitee.com/johng/gf/g/util/gconv"
-    "time"
-    "reflect"
+    "gitee.com/johng/gf/g"
+    "gitee.com/johng/gf/g/os/glog"
 )
 
 func main() {
-
-    fmt.Println(reflect.TypeOf(gconv.Time(gtime.Second())))
-    fmt.Println(time.Unix(gtime.Second(), 0).String())
-
+    g.Config().SetPath("/home/john/Workspace/Go/GOPATH/src/gitee.com/johng/gf/geg/frame")
+    db := g.Database()
+    if r, err := db.Table("goods").Where("id=?", 1).One(); err == nil {
+        fmt.Printf("goods    id: %d\n",   r["id"].Int())
+        fmt.Printf("goods title: %s\n",   r["title"].String())
+        fmt.Printf("goods proce: %.2f\n", r["price"].Float32())
+    } else {
+        glog.Error(err)
+    }
 }
