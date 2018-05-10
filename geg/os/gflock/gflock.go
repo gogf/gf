44b9cb77ec1cffdb8da42ced29b2a4b96fbf00e6
John <john@johng.cn>
2018-05-10 12:04:41 +0800

    gflock开发中

--- geg/os/gflock/gflock.go
+++ geg/os/gflock/gflock.go
@@ -3,14 +3,14 @@ package main
 import (
     "github.com/theckman/go-flock"
     "fmt"
-    "time"
 )
 
 func main() {
     fileLock := flock.NewFlock("/var/lock/go-lock.lock")
-    fmt.Println(fileLock.Lock())
-    fmt.Println(fileLock.Lock())
-    time.Sleep(1000*time.Second)
+
+    fmt.Println(fileLock.TryLock())
+    fmt.Println(fileLock.TryRLock())
+    //time.Sleep(1000*time.Second)
 //fmt.Println(locked)
 //    fmt.Println(fileLock.Locked())
 //fmt.Println(err)
