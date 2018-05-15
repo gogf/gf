9ce65c195a05a4f3696312ae01a3cde0c8b5df62
John <John>
2018-05-15 13:53:13 +0800

    完善模板引擎热更新机制测试

---
+++ geg/os/gview/web_hot_update.go
@@ -0,0 +1,17 @@
+package main
+
+
+import (
+    "gitee.com/johng/gf/g"
+    "gitee.com/johng/gf/g/net/ghttp"
+)
+
+func main() {
+    s := g.Server()
+    s.BindHandler("/", func(r *ghttp.Request) {
+        g.View().SetPath(`D:\Workspace\Go\GOPATH\src\gitee.com\johng\gf\geg\os\gview`)
+        b, _ := g.View().Parse("test.tpl", nil)
+        r.Response.Write(b)
+    })
+    s.Run()
+}
