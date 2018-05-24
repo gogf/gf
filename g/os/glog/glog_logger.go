ad2fe7f4995a2351342baf19e42b9fc67d90ab61
zseeker <675211118@qq.com>
2018-05-24 12:37:55 +0800

    增加glog控制台打印开关

--- g/os/glog/glog_logger.go
+++ g/os/glog/glog_logger.go
@@ -27,6 +27,9 @@ const (
 // 默认的日志换行符
 var ln = "\n"
 
+// 控制台打印开关
+var stdprint = false
+
 // 初始化日志换行符
 // @author zseeker
 // @date   2018-05-23
@@ -95,12 +98,21 @@ func (l *Logger) SetPath(path string) error {
     return nil
 }
 
+// 设置写日志时开启or关闭控制台打印，默认是关闭的
+// @author zseeker
+func (l *Logger) SetStdPrint(open bool) {
+    stdprint = open
+}
+
 // 这里的写锁保证统一时刻只会写入一行日志，防止串日志的情况
 func (l *Logger) print(defaultIO io.Writer, s string) {
     w := l.GetIO()
     if w == nil {
         if v := l.getFileByPool(); v != nil {
-            w = v.File()
+            // 同时输出到文件和终端 @author zseeker
+            if stdprint {
+                w = io.MultiWriter(v.File(), os.Stdout)
+            }
             defer v.Close()
         } else {
             w = defaultIO
