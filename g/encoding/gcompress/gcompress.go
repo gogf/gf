8f340684d4b990f0b7685be059b7c245a2e4eb63
wenzi1 <liyz23@qq.com>
2018-06-01 21:10:28 +0800

    增加gzip方法的封装，修改gxml能够支持GBK/GB18030自动转UTF-8

--- g/encoding/gcompress/gcompress.go
+++ g/encoding/gcompress/gcompress.go
@@ -10,6 +10,7 @@ package gcompress
 import (
     "bytes"
     "compress/zlib"
+    "compress/gzip"
     "io"
 )
 
@@ -38,4 +39,30 @@ func UnZlib(data []byte) []byte {
     }
     io.Copy(&out, r)
     return out.Bytes()
+}
+
+//做gzip解压缩
+func UnGzip(data []byte) []byte {
+    var buf bytes.Buffer
+	content := bytes.NewReader(data)
+	zipdata, err := gzip.NewReader(content)
+	if err != nil {
+		return nil
+	}
+	io.Copy(&buf, zipdata)
+	zipdata.Close()
+	return buf.Bytes()
+}
+
+//做gzip压缩
+func Gzip(data []byte) []byte {
+    var buf bytes.Buffer
+	zip := gzip.NewWriter(&buf)
+	_, err := zip.Write(data)
+	if err != nil {
+		return nil
+	}
+	zip.Close()
+
+	return buf.Bytes()
 }
