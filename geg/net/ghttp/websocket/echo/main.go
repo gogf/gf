5c22115c652095353767e408c3b83aecdbb5202d
John <john@johng.cn>
2018-06-02 23:47:16 +0800

    完成ghttp.Server对WebSocket的支持，并完成测试以及对应示例

---
+++ geg/net/ghttp/websocket/echo/main.go
@@ -0,0 +1,25 @@
+package echo
+
+import (
+    "gitee.com/johng/gf/g"
+    "gitee.com/johng/gf/g/net/ghttp"
+)
+
+func main() {
+    s := g.Server()
+    s.BindHandler("/ws", func(r *ghttp.Request) {
+        conn, _ := r.WebSocket()
+        for {
+            msgType, msg, err := conn.ReadMessage()
+            if err != nil {
+                return
+            }
+            if err = conn.WriteMessage(msgType, msg); err != nil {
+                return
+            }
+        }
+    })
+    s.SetPort(8199)
+    s.Run()
+}
+
