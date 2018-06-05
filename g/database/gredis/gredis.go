7ba6ccd2fbf40db1e5430c586b3e55aa053bc63b
John <john@johng.cn>
2018-06-05 21:18:41 +0800

    gredis增加密码支持

--- g/database/gredis/gredis.go
+++ g/database/gredis/gredis.go
@@ -12,7 +12,7 @@ import (
     "time"
     "github.com/gomodule/redigo/redis"
     "gitee.com/johng/gf/g/container/gmap"
-    "gitee.com/johng/gf/g/util/gconv"
+    "fmt"
 )
 
 const (
@@ -28,6 +28,14 @@ type Redis struct {
     pool *redis.Pool
 }
 
+// Redis服务端但节点连接配置信息
+type Config struct {
+    Host string // IP/域名
+    Port int    // 端口
+    Db   int    // db
+    Pass string // 密码
+}
+
 // Redis链接池统计信息
 type PoolStats struct {
     redis.PoolStats
@@ -38,13 +46,9 @@ var pools = gmap.NewStringInterfaceMap()
 
 // 创建redis操作对象
 // address参数格式 host:port
-func New(address string, db ... interface{}) *Redis {
-    r := &Redis{}
-    dialDb := 0
-    if len(db) > 0 {
-        dialDb = gconv.Int(db[0])
-    }
-    poolKey := address + "," + gconv.String(dialDb)
+func New(config Config) *Redis {
+    r       := &Redis{}
+    poolKey := fmt.Sprintf("%s:%d,%d", config.Host, config.Port, config.Db)
     if v := pools.Get(poolKey); v == nil {
         pool := &redis.Pool {
             MaxIdle         : gDEFAULT_POOL_MAX_IDLE,
@@ -52,11 +56,14 @@ func New(address string, db ... interface{}) *Redis {
             IdleTimeout     : gDEFAULT_POOL_IDLE_TIMEOUT,
             MaxConnLifetime : gDEFAULT_POOL_MAX_LIFE_TIME,
             Dial            : func() (redis.Conn, error) {
-                c, err := redis.Dial("tcp", address)
+                c, err := redis.Dial("tcp", fmt.Sprintf("%s:%d", config.Host, config.Port))
                 if err != nil {
                     return nil, err
                 }
-                c.Do("SELECT", dialDb)
+                c.Do("SELECT", config.Db)
+                if len(config.Pass) > 0 {
+                    c.Do("AUTH", config.Pass)
+                }
                 return c, nil
             },
         }
@@ -69,7 +76,7 @@ func New(address string, db ... interface{}) *Redis {
     return r
 }
 
-// 关闭链接
+// 关闭链接，将底层的redis对象放回池中
 func (r *Redis) Close() error {
     return r.conn.Close()
 }
@@ -99,12 +106,12 @@ func (r *Redis) Stats() *PoolStats {
     return &PoolStats{r.pool.Stats()}
 }
 
-// 执行命令 - Do
+// 执行同步命令 - Do
 func (r *Redis) Do(command string, args ...interface{}) (interface{}, error) {
     return r.conn.Do(command, args...)
 }
 
-// 执行命令 - Send
+// 执行异步命令 - Send
 func (r *Redis) Send(command string, args ...interface{}) error {
     return r.conn.Send(command, args...)
 }
--- geg/other/test.go
+++ geg/other/test.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-    "gitee.com/johng/gf/geg/other/sleep"
+    "fmt"
+    "gitee.com/johng/gf/g/util/gregx"
 )
 
 func main() {
-    sleep.Test()
+    a , e := gregx.MatchString(`(.+):(\d+),{0,1}(\d*),{0,1}(.*)`, "127.0.0.1:12333")
+    fmt.Println(e)
+    for k, v := range a {
+        fmt.Printf("%d:%v\n", k, v)
+    }
 }
