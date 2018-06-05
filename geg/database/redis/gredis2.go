be218230935eb0a4f7be619ea80a4c471f40e5e3
John <john@johng.cn>
2018-06-05 21:25:30 +0800

    完善gredis示例

--- geg/database/redis/gredis2.go
+++ geg/database/redis/gredis2.go
@@ -9,11 +9,8 @@ import (
 func main() {
     redis := g.Redis()
     defer redis.Close()
-    redis.Do("SET", "k1", "v1")
-    redis.Do("SET", "k2", "v2")
-    v1, _ := redis.Do("GET", "k1")
-    v2, _ := redis.Do("GET", "k1")
-    fmt.Println(gconv.String(v1))
-    fmt.Println(gconv.String(v2))
+    redis.Do("SET", "k", "v")
+    v, _ := redis.Do("GET", "k")
+    fmt.Println(gconv.String(v))
 }
 
