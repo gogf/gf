c5e61fc6fdd1ce1501565296a95098d80aa34d58
John <john@johng.cn>
2018-06-12 19:24:40 +0800

    增加docker测试脚本

---
+++ geg/other/docker/test/http.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "gitee.com/johng/gf/g"
+    "gitee.com/johng/gf/g/net/ghttp"
+)
+
+func main() {
+    s := g.Server()
+    s.BindHandler("/", func(r *ghttp.Request){
+        r.Response.Writeln("哈喽世界！")
+    })
+    s.SetPort(8199)
+    s.Run()
+}
