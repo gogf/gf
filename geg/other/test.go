261ce9fcc9b50dfb4e73fc3aa2b96bad55d0a29d
john <john@johng.cn>
2018-07-11 13:51:03 +0800

    gtime增加自定义格式支持

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-    "gitee.com/johng/gf/g/util/gregx"
     "fmt"
+    "time"
 )
 
 func main() {
-    fmt.Println(gregx.MatchString(`[-/]`, "-"))
+    //t, err := gtime.StrToTime("2018-01-02 13:08:01")
+    //fmt.Println(err)
+    //fmt.Println(t.Format("3:4"))
+    //fmt.Println(time.Now().Format(".000"))
+    fmt.Println(time.Now().Format(".000"))
 }
