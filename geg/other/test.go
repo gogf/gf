9631ff5ea0b588136e2e96d49d2cef6126aa6ef7
John <john@johng.cn>
2018-08-06 22:38:55 +0800

    完善gpage静态分页示例程序

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"gitee.com/johng/gf/g/os/gfile"
+	"fmt"
+	"gitee.com/johng/gf/g/util/gregex"
 )
 
 func main() {
-	path := "/home/john/Documents/temp"
-	flags1 := gfile.IsFile(path)
-	if flags1 == true {
-		println("有")
-	} else {
-		println("无")
-	}
+	name := "page"
+	path := "/page/template/{page}.html"
+	rule := fmt.Sprintf(`{%s}`, name, name)
+	tpl, err := gregex.ReplaceString(rule, `{.page}`, path)
+	fmt.Println(err)
+	fmt.Println(tpl)
 }
