9bb481ea961d2c540b16e0927c0b1246817f0f62
john <john@johng.cn>
2018-08-09 09:49:57 +0800

    调整orm示例代码目录，sqlite增加todo提示

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"gitee.com/johng/gf/g/os/gtime"
+    "fmt"
 )
 
 func main() {
-    fmt.Println(gtime.Second())
-    fmt.Println(gtime.Nanosecond())
-	t := gtime.Millisecond()
-	for t < 1e18 {
-        t *= 10
-    }
-	fmt.Println(t)
-	fmt.Println(int64(t/1e9))
-	fmt.Println(t%1e9)
-
-	fmt.Println(gtime.NewFromTimeStamp(t).Format("Y-m-d H:i:s.u"))
+    fmt.Println(1)
 }
