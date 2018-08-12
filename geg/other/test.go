9d4eb36810cf302ce42f91218d9b3bf48af75bad
John <john@johng.cn>
2018-08-12 17:17:17 +0800

    gvalid增加字段校验别名，用于自定义返回结果字段，并更新WebServer中相关使用的模块

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+    "gitee.com/johng/gf/g/util/gregex"
     "fmt"
+    "gitee.com/johng/gf/g/util/gutil"
 )
 
+
+
 func main() {
-    fmt.Println(1)
+    s := `username    @   required|length:6,30  #  请输入用户名称|用户名称长度非法`
+    match, err := gregex.MatchString(`\s*((\w+)\s*@){0,1}\s*([^#]+)\s*(#\s*(.*)){0,1}\s*`, s)
+    fmt.Println(err)
+    gutil.Dump(match)
 }
