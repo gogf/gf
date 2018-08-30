1c7e1b6c02a08868e4213a9832e634a96babd42e
John <john@johng.cn>
2018-08-30 23:21:53 +0800

    解决内存锁手动解锁与计时解锁的冲突

--- g/os/gmlock/gmlock_mutex.go
+++ g/os/gmlock/gmlock_mutex.go
@@ -9,11 +9,14 @@ package gmlock
 import (
     "sync"
     "gitee.com/johng/gf/g/container/gtype"
+    "gitee.com/johng/gf/g/os/gtime"
 )
 
 // 互斥锁对象
 type Mutex struct {
     mu     sync.RWMutex
+    rid    *gtype.Int64        // 当前RLock产生的唯一id(主要用于计时RUnlock的校验)
+    wid    *gtype.Int64        // 当前Lock产生的唯一id(主要用于计时Unlock的校验)
     rcount *gtype.Int          // RLock次数
     wcount *gtype.Int          // Lock次数
 }
@@ -21,6 +24,8 @@ type Mutex struct {
 // 创建一把内存锁使用的底层RWMutex
 func NewMutex() *Mutex {
     return &Mutex{
+        rid    : gtype.NewInt64(),
+        wid    : gtype.NewInt64(),
         rcount : gtype.NewInt(),
         wcount : gtype.NewInt(),
     }
@@ -38,6 +43,7 @@ func (l *Mutex) TryLock() bool {
 func (l *Mutex) Lock() {
     l.wcount.Add(1)
     l.mu.Lock()
+    l.wid.Set(gtime.Nanosecond())
 }
 
 // 安全的Unlock
@@ -60,6 +66,7 @@ func (l *Mutex) TryRLock() bool {
 func (l *Mutex) RLock() {
     l.rcount.Add(1)
     l.mu.RLock()
+    l.rid.Set(gtime.Nanosecond())
 }
 
 // 安全的RUnlock
