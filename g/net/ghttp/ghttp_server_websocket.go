ca266c1d5e0eee0ced08afdbc3083d2599d4f8c0
John <john@johng.cn>
2018-08-31 23:53:57 +0800

    websocket增加消息类型常量

--- g/net/ghttp/ghttp_server_websocket.go
+++ g/net/ghttp/ghttp_server_websocket.go
@@ -12,3 +12,25 @@ import "github.com/gorilla/websocket"
 type WebSocket struct {
     *websocket.Conn
 }
+
+const (
+    // TextMessage denotes a text data message. The text message payload is
+    // interpreted as UTF-8 encoded text data.
+    WS_MSG_TEXT   = websocket.TextMessage
+
+    // BinaryMessage denotes a binary data message.
+    WS_MSG_BINARY = websocket.BinaryMessage
+
+    // CloseMessage denotes a close control message. The optional message
+    // payload contains a numeric code and text. Use the FormatCloseMessage
+    // function to format a close message payload.
+    WS_MSG_CLOSE  = websocket.CloseMessage
+
+    // PingMessage denotes a ping control message. The optional message payload
+    // is UTF-8 encoded text.
+    WS_MSG_PING   = websocket.PingMessage
+
+    // PongMessage denotes a pong control message. The optional message payload
+    // is UTF-8 encoded text.
+    WS_MSG_PONG   = websocket.PongMessage
+)
