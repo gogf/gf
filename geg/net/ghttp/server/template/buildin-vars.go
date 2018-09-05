2219d00f6cbb2db98eea31f9dc620fe6fb3606c6
john <john@johng.cn>
2018-09-05 13:33:46 +0800

    增加模板引擎内置变量使用示例

--- geg/net/ghttp/server/template/buildin-vars.go
+++ geg/net/ghttp/server/template/buildin-vars.go
@@ -8,7 +8,9 @@ import (
 func main() {
     s := g.Server()
     s.BindHandler("/", func(r *ghttp.Request){
-        content :=`{{if (get "name")}} {{get "name"}} {{else}} NoName {{end}}`
+        r.Cookie.Set("theme", "default")
+        r.Session.Set("name", "john")
+        content :=`Config:{{.Config.redis.cache}}, Cookie:{{.Cookie.theme}}, Session:{{.Session.name}}`
         r.Response.WriteTplContent(content, nil)
     })
     s.SetPort(8199)
