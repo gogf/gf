c936f44a61219bff23537f78be9afc59f1fefc8a
john <john@johng.cn>
2018-09-07 19:08:42 +0800

    gcache增加GetOrSet/GetOrSetFunc方法

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+    "gitee.com/johng/gf/g/os/gfile"
+    "strings"
+    "gitee.com/johng/gf/g/os/gfcache"
+    "fmt"
+)
 
 func main() {
-	fmt.Println(1)
+	files := gfile.ScanDir("/home/john/Workspace/med3-svr", true)
+	for _, file := range files {
+	    if strings.Index(gfcache.GetContents(file), "ENV") != -1 {
+            fmt.Println(file)
+        }
+    }
 }
