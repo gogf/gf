8500091d70c733037464e46bf2b4d8dd0481ce12
John <john@johng.cn>
2018-10-13 22:43:35 +0800

    改进SESSION方法

--- g/net/ghttp/ghttp_server_session.go
+++ g/net/ghttp/ghttp_server_session.go
@@ -15,6 +15,7 @@ import (
     "gitee.com/johng/gf/g/util/grand"
     "gitee.com/johng/gf/g/util/gconv"
     "gitee.com/johng/gf/g/container/gmap"
+    "time"
 )
 
 // 单个session对象
@@ -57,8 +58,8 @@ func (s *Session) Data () map[string]interface{} {
 }
 
 // 设置session
-func (s *Session) Set (k string, v interface{}) {
-    s.data.Set(k, v)
+func (s *Session) Set (key string, value interface{}) {
+    s.data.Set(key, value)
 }
 
 // 批量设置(BatchSet别名)
@@ -72,46 +73,52 @@ func (s *Session) BatchSet (m map[string]interface{}) {
 }
 
 // 判断键名是否存在
-func (s *Session) Contains (k string) bool {
-    return s.data.Get(k) != nil
+func (s *Session) Contains (key string) bool {
+    return s.data.Contains(key)
 }
 
 // 获取session
-func (s *Session) Get (k string) interface{} {
-    return s.data.Get(k)
-}
+func (s *Session) Get (key string) interface{}  { return s.data.Get(key)          }
+func (s *Session) GetString (key string) string { return gconv.String(s.Get(key)) }
+func (s *Session) GetBool(key string) bool      { return gconv.Bool(s.Get(key))   }
 
-func (s *Session) GetString (k string) string {
-    return gconv.String(s.Get(k))
-}
+func (s *Session) GetInt(key string)            int             { return gconv.Int(s.Get(key))   }
+func (s *Session) GetInt8(key string)           int8            { return gconv.Int8(s.Get(key))  }
+func (s *Session) GetInt16(key string)          int16           { return gconv.Int16(s.Get(key)) }
+func (s *Session) GetInt32(key string)          int32           { return gconv.Int32(s.Get(key)) }
+func (s *Session) GetInt64(key string)          int64           { return gconv.Int64(s.Get(key)) }
 
-func (s *Session) GetBool (k string) bool {
-    return gconv.Bool(s.Get(k))
-}
+func (s *Session) GetUint(key string)           uint            { return gconv.Uint(s.Get(key))   }
+func (s *Session) GetUint8(key string)          uint8           { return gconv.Uint8(s.Get(key))  }
+func (s *Session) GetUint16(key string)         uint16          { return gconv.Uint16(s.Get(key)) }
+func (s *Session) GetUint32(key string)         uint32          { return gconv.Uint32(s.Get(key)) }
+func (s *Session) GetUint64(key string)         uint64          { return gconv.Uint64(s.Get(key)) }
 
-func (s *Session) GetInt (k string) int {
-    return gconv.Int(s.Get(k))
-}
+func (s *Session) GetFloat32 (key string) float32 { return gconv.Float32(s.Get(key)) }
+func (s *Session) GetFloat64 (key string) float64 { return gconv.Float64(s.Get(key)) }
 
-func (s *Session) GetUint (k string) uint {
-    return gconv.Uint(s.Get(k))
-}
+func (s *Session) GetBytes(key string)          []byte          { return gconv.Bytes(s.Get(key))      }
+func (s *Session) GetInts(key string)           []int           { return gconv.Ints(s.Get(key))       }
+func (s *Session) GetFloats(key string)         []float64       { return gconv.Floats(s.Get(key))     }
+func (s *Session) GetStrings(key string)        []string        { return gconv.Strings(s.Get(key))    }
+func (s *Session) GetInterfaces(key string)     []interface{}   { return gconv.Interfaces(s.Get(key)) }
 
-func (s *Session) GetUint8 (k string) uint8 {
-    return gconv.Uint8(s.Get(k))
-}
+func (s *Session) GetTime(key string, format...string) time.Time       { return gconv.Time(s.Get(key), format...) }
+func (s *Session) GetTimeDuration(key string)          time.Duration   { return gconv.TimeDuration(s.Get(key)) }
 
-func (s *Session) GetFloat32 (k string) float32 {
-    return gconv.Float32(s.Get(k))
-}
-
-func (s *Session) GetFloat64 (k string) float64 {
-    return gconv.Float64(s.Get(k))
+// 将变量转换为对象，注意 objPointer 参数必须为struct指针
+func (s *Session) GetStruct(key string, objPointer interface{}, attrMapping...map[string]string) error {
+    return gconv.Struct(s.Get(key), objPointer, attrMapping...)
 }
 
 // 删除session
-func (s *Session) Remove (k string) {
-    s.data.Remove(k)
+func (s *Session) Remove (key string) {
+    s.data.Remove(key)
+}
+
+// 清空session
+func (s *Session) Clear () {
+    s.data.Clear()
 }
 
 // 更新过期时间(如果用在守护进程中长期使用，需要手动调用进行更新，防止超时被清除)
--- geg/other/test.go
+++ geg/other/test.go
@@ -3,13 +3,11 @@ package main
 import (
     "fmt"
     "gitee.com/johng/gf/g/util/grand"
-    "os"
 )
 
 func main() {
-    fmt.Println(uint(-1))
-    os.Exit(0)
     for i := 0; i < 10; i++ {
+        //fmt.Println(grand.RandStr(3))
         fmt.Println(grand.Rand(100, 200))
     }
 }
