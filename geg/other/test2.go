810252a00e9a4b8f91a034011d46b4c32d54ee69
john <john@johng.cn>
2018-10-26 18:39:03 +0800

    gfile.ScanDir支持pattern多个文件模式匹配，使用','符号分隔多个匹配模式

--- geg/other/test2.go
+++ geg/other/test2.go
@@ -2,17 +2,9 @@ package main
 
 import (
     "gitee.com/johng/gf/g"
-    "gitee.com/johng/gf/g/net/ghttp"
+    "gitee.com/johng/gf/g/os/gfile"
 )
 
 func main() {
-    s := g.Server()
-    s.BindHandler("/{class}-{course}/:name/*act", func(r *ghttp.Request){
-        r.Response.Writeln(r.Get("class"))
-        r.Response.Writeln(r.Get("course"))
-        r.Response.Writeln(r.Get("name"))
-        r.Response.Writeln(r.Get("act"))
-    })
-    s.SetPort(8199)
-    s.Run()
+    g.Dump(gfile.ScanDir("/var/log", "*.log, *.gz", true))
 }
