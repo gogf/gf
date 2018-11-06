105e492b65f41a2aa19d03a8f09f4d8654da074f
john <john@johng.cn>
2018-11-06 10:59:32 +0800

    改进gtime对日期时间字符串的解析

--- geg/other/test.go
+++ geg/other/test.go
@@ -2,13 +2,12 @@ package main
 
 import (
     "fmt"
-    "gitee.com/johng/gf/g/os/gtime"
-    "math"
+    "strings"
 )
 
 func main() {
-    fmt.Println(gtime.NewFromStr("2018-10-24 00:00:00").Nanosecond())
-    fmt.Println(math.MaxInt64)
-    fmt.Println(gtime.Second())
-    fmt.Println(gtime.Nanosecond())
+    fmt.Println(strings.Trim(`  1  `, "./- \n\r"))
+    //fmt.Println(math.MaxInt64)
+    //fmt.Println(gtime.Second())
+    //fmt.Println(gtime.Nanosecond())
 }
