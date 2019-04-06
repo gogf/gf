2107061a46181e51a33b79630b0d7ebfcc71a444
hailaz <739476267@qq.com>
2019-04-06 22:48:47 +0800

    Add gtime unit test

---
+++ g/os/gtime/gtime_z_unit_format_test.go
@@ -0,0 +1,65 @@
+package gtime_test
+
+import (
+	"testing"
+
+	"github.com/gogf/gf/g/os/gtime"
+	"github.com/gogf/gf/g/test/gtest"
+)
+
+/*
+func Test_formatToStdLayout(t *testing.T) {
+	gtest.Case(t, func() {
+		gtest.Assert(formatToStdLayout("Y-m-d H:i:s"), "2006-01-02 15:04:05")
+		gtest.Assert(formatToStdLayout("\\T\\i\\m\\e Y-m-d H:i:s"), "Time 2006-01-02 15:04:05")
+		gtest.Assert(formatToStdLayout("Y-m-d H:i:s\\"), "2006-01-02 15:04:05")
+		gtest.Assert(formatToStdLayout("Y-m-j G:i:s.u"), "2006-01-02 15:04:05.000")
+		gtest.Assert(formatToStdLayout("Y-m-j G:i:su"), "2006-01-02 15:04:05.000")
+	})
+}
+*/
+func Test_Format(t *testing.T) {
+	gtest.Case(t, func() {
+		timeTemp, err := gtime.StrToTime("2006-01-11 15:04:05", "Y-m-d H:i:s")
+		timeTemp.ToZone("Asia/Shanghai")
+		if err != nil {
+			t.Error("test fail")
+		}
+		gtest.Assert(timeTemp.Format("\\T\\i\\m\\e中文Y-m-j G:i:s.u\\"), "Time中文2006-01-11 15:04:05.000")
+
+		gtest.Assert(timeTemp.Format("d D j l"), "11 Wed 11 Wednesday")
+
+		gtest.Assert(timeTemp.Format("F m M n"), "January 01 Jan 1")
+
+		gtest.Assert(timeTemp.Format("Y y"), "2006 06")
+
+		gtest.Assert(timeTemp.Format("a A g G h H i s u .u"), "pm PM 3 15 03 15 04 05 000 .000")
+
+		gtest.Assert(timeTemp.Format("O P T"), "+0800 +08:00 CST")
+
+		gtest.Assert(timeTemp.Format("r"), "Wed, 11 Jan 06 15:04 CST")
+
+		gtest.Assert(timeTemp.Format("c"), "2006-01-11T15:04:05+08:00")
+
+		//补零
+		timeTemp1, err := gtime.StrToTime("2006-01-02 03:04:05", "Y-m-d H:i:s")
+		if err != nil {
+			t.Error("test fail")
+		}
+		gtest.Assert(timeTemp1.Format("Y-m-d h:i:s"), "2006-01-02 03:04:05")
+		//不补零
+		timeTemp2, err := gtime.StrToTime("2006-01-02 03:04:05", "Y-m-d H:i:s")
+		if err != nil {
+			t.Error("test fail")
+		}
+		gtest.Assert(timeTemp2.Format("Y-n-j G:i:s"), "2006-1-2 3:04:05")
+
+	})
+}
+
+func Test_Layout(t *testing.T) {
+	gtest.Case(t, func() {
+		timeTemp := gtime.Now()
+		gtest.Assert(timeTemp.Layout("2006-01-02 15:04:05"), timeTemp.Time.Format("2006-01-02 15:04:05"))
+	})
+}
