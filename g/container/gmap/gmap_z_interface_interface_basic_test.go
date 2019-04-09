277b7a453641f3f84fe829490248d721272a0a9a
Jay <976739120@qq.com>
2019-04-09 15:06:12 +0800

    测试修改

--- g/container/gmap/gmap_z_interface_interface_basic_test.go
+++ g/container/gmap/gmap_z_interface_interface_basic_test.go
@@ -5,46 +5,82 @@ import (
 	"github.com/gogf/gf/g/test/gtest"
 	"testing"
 )
-func getValue()interface{}{
+
+func getValue() interface{} {
 	return 3
 }
-
+func callBack(k interface{}, v interface{}) bool {
+	return true
+}
 func Test_Map_Basic(t *testing.T) {
 	gtest.Case(t, func() {
 		m := gmap.New()
 		m.Set("key1", "val1")
-		gtest.Assert(m.Keys(),[]interface{}{"key1"})
-		gtest.Assert(m.Values(),[]interface{}{"val1"})
+		gtest.Assert(m.Keys(), []interface{}{"key1"})
+		gtest.Assert(m.Values(), []interface{}{"val1"})
 
 		gtest.Assert(m.Get("key1"), "val1")
-		m.BatchSet(map[interface{}]interface{}{1: 1, "key2": "val2", "key3": "val3"})
-		gtest.Assert(m.Size(), 4)
+		gtest.Assert(m.Size(), 1)
 		gtest.Assert(m.IsEmpty(), false)
-		gtest.Assert(m.GetOrSet("key4", "val4"), "val4")
-		gtest.Assert(m.SetIfNotExist("key4", "val4"), false)
+
+		gtest.Assert(m.GetOrSet("key2", "val2"), "val2")
+		gtest.Assert(m.SetIfNotExist("key2", "val2"), false)
+
+		gtest.Assert(m.SetIfNotExist("key3", "val3"), true)
+
 		gtest.Assert(m.Remove("key2"), "val2")
-		m.BatchRemove([]interface{}{"key1", 1})
-		gtest.Assert(m.Contains("key3"), true)
+		gtest.Assert(m.Contains("key2"), false)
+
+		gtest.AssertIN("key3", m.Keys())
+		gtest.AssertIN("key1", m.Keys())
+		gtest.AssertIN("val3", m.Values())
+		gtest.AssertIN("val1", m.Values())
+
 		m.Flip()
-		gtest.Assert(m.Map(), map[interface{}]interface{}{"val3": "key3", "val4": "key4"})
-		m.GetOrSetFunc("fun",getValue)
-		m.GetOrSetFuncLock("funlock",getValue)
-		gtest.Assert(m.Get("funlock"),3)
-		gtest.Assert(m.Get("fun"),3)
-		m.GetOrSetFunc("fun",getValue)
-		gtest.Assert(m.SetIfNotExistFunc("fun",getValue),false)
-		gtest.Assert(m.SetIfNotExistFuncLock("funlock",getValue),false)
+		gtest.Assert(m.Map(), map[interface{}]interface{}{"val3": "key3", "val1": "key1"})
 
 		m.Clear()
 		gtest.Assert(m.Size(), 0)
+		gtest.Assert(m.IsEmpty(), true)
+
 		m2 := gmap.NewFrom(map[interface{}]interface{}{1: 1, "key1": "val1"})
 		gtest.Assert(m2.Map(), map[interface{}]interface{}{1: 1, "key1": "val1"})
 		m3 := gmap.NewFromArray([]interface{}{1, "key1"}, []interface{}{1, "val1"})
 		gtest.Assert(m3.Map(), map[interface{}]interface{}{1: 1, "key1": "val1"})
-		m4 := m3.Clone()
-		gtest.Assert(m4.Map(), map[interface{}]interface{}{1: 1, "key1": "val1"})
 
 	})
+}
+func Test_Map_Set_Fun(t *testing.T) {
+	m := gmap.New()
+	m.GetOrSetFunc("fun", getValue)
+	m.GetOrSetFuncLock("funlock", getValue)
+	gtest.Assert(m.Get("funlock"), 3)
+	gtest.Assert(m.Get("fun"), 3)
+	m.GetOrSetFunc("fun", getValue)
+	gtest.Assert(m.SetIfNotExistFunc("fun", getValue), false)
+	gtest.Assert(m.SetIfNotExistFuncLock("funlock", getValue), false)
+}
+
+func Test_Map_Batch(t *testing.T) {
+	m := gmap.New()
+	m.BatchSet(map[interface{}]interface{}{1: 1, "key1": "val1", "key2": "val2", "key3": "val3"})
+	m.Iterator(callBack)
+	gtest.Assert(m.Map(), map[interface{}]interface{}{1: 1, "key1": "val1", "key2": "val2", "key3": "val3"})
+	m.BatchRemove([]interface{}{"key1", 1})
+	gtest.Assert(m.Map(), map[interface{}]interface{}{"key2": "val2", "key3": "val3"})
+}
+
+func Test_Map_Clone(t *testing.T) {
+	//clone 方法是深克隆
+	m := gmap.NewFrom(map[interface{}]interface{}{1: 1, "key1": "val1"})
+	m_clone := m.Clone()
+	m.Remove(1)
+	//修改原 map,clone 后的 map 不影响
+	gtest.AssertIN(1, m_clone.Keys())
+
+	m_clone.Remove("key1")
+	//修改clone map,原 map 不影响
+	gtest.AssertIN("key1", m.Keys())
 }
 func Test_Map_Basic_Merge(t *testing.T) {
 	m1 := gmap.New()
