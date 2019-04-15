2f7d4cd80daecfeba387f464a06c1a8d6e9e2723
John <john@johng.cn>
2019-04-15 22:55:12 +0800

    fix issue in Add function for gtype.Float32/Float64

--- geg/other/test.go
+++ geg/other/test.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/gogf/gf/g/util/grand"
+	"github.com/gogf/gf/g/container/gtype"
+	"github.com/gogf/gf/g/test/gtest"
 )
 
-
-
 func main() {
-	for {
-		fmt.Println(grand.Intn(100))
-	}
+
+	var add float32
+	add = 3.1415926
+
+	myF32 := gtype.NewFloat32(add)
+	myAdd := myF32.Add(float32(6.2951413))
+
+	add += float32(6.2951413)
+
+	fmt.Println(myF32.Val())
+	gtest.AssertEQ(myAdd, add)
 
 }
