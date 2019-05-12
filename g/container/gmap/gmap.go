3c750c3c924a9f47e7a6685fce560ccfff6b5f1e
John <john@johng.cn>
2019-05-12 21:22:07 +0800

    copy treemap logics to new file

--- g/container/gmap/gmap.go
+++ g/container/gmap/gmap.go
@@ -7,17 +7,9 @@
 // Package gmap provides concurrent-safe/unsafe map containers.
 package gmap
 
-import (
-	"github.com/gogf/gf/g/container/gtree"
-)
-
 // Map based on hash table, alias of AnyAnyMap.
 type Map     = AnyAnyMap
 type HashMap = AnyAnyMap
-
-// Map based on red-black tree, alias of RedBlackTree.
-type TreeMap = gtree.RedBlackTree
-
 
 // New returns an empty hash map.
 // The param <unsafe> used to specify whether using map in un-concurrent-safety,
@@ -49,20 +41,4 @@ func NewHashMap(unsafe ...bool) *Map {
 // which is false in default.
 func NewHashMapFrom(data map[interface{}]interface{}, unsafe...bool) *Map {
 	return NewAnyAnyMapFrom(data, unsafe...)
-}
-
-// NewTreeMap instantiates a tree map with the custom comparator.
-// The param <unsafe> used to specify whether using tree in un-concurrent-safety,
-// which is false in default.
-func NewTreeMap(comparator func(v1, v2 interface{}) int, unsafe...bool) *TreeMap {
-	return gtree.NewRedBlackTree(comparator, unsafe...)
-}
-
-// NewTreeMapFrom instantiates a tree map with the custom comparator and <data> map.
-// Note that, the param <data> map will be set as the underlying data map(no deep copy),
-// there might be some concurrent-safe issues when changing the map outside.
-// The param <unsafe> used to specify whether using tree in un-concurrent-safety,
-// which is false in default.
-func NewTreeMapFrom(comparator func(v1, v2 interface{}) int, data map[interface{}]interface{}, unsafe...bool) *TreeMap {
-	return gtree.NewRedBlackTreeFrom(comparator, data, unsafe...)
 }
