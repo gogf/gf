be6f522cf3c911a718948c185fe0453f4f8e0a52
zhangbiao <zhangbiao@channelsoft.com>
2019-06-10 19:53:07 +0800

    add gaes test4:optimize Encrypt Decrypt test

--- g/crypto/gaes/gaes_test.go
+++ g/crypto/gaes/gaes_test.go
@@ -22,6 +22,7 @@ var (
 	content_24, _    = gbase64.Decode("0TXOaj5KMoLhNWmJ3lxY1A==")
 	content_32, _    = gbase64.Decode("qM/Waw1kkWhrwzek24rCSA==")
 	content_16_iv, _ = gbase64.Decode("DqQUXiHgW/XFb6Qs98+hrA==")
+	content_32_iv, _ = gbase64.Decode("ZuLgAOii+lrD5KJoQ7yQ8Q==")
 	// iv 长度必须等于blockSize，只能为16
 	iv         = []byte("Hello My GoFrame")
 	key_16     = []byte("1234567891234567")
@@ -47,6 +48,36 @@ func TestEncrypt(t *testing.T) {
 		data, err = gaes.Encrypt(content, key_16, iv)
 		gtest.Assert(err, nil)
 		gtest.Assert(string(data), content_16_iv)
+		data, err = gaes.Encrypt(content, key_32, iv)
+		gtest.Assert(err, nil)
+		gtest.Assert(string(data), content_32_iv)
+	})
+}
+
+func TestDecrypt(t *testing.T) {
+	gtest.Case(t, func() {
+		decrypt, err := gaes.Decrypt([]byte(content_16), key_16)
+		gtest.Assert(err, nil)
+		gtest.Assert(string(decrypt), string(content))
+
+		decrypt, err = gaes.Decrypt([]byte(content_24), key_24)
+		gtest.Assert(err, nil)
+		gtest.Assert(string(decrypt), string(content))
+
+		decrypt, err = gaes.Decrypt([]byte(content_32), key_32)
+		gtest.Assert(err, nil)
+		gtest.Assert(string(decrypt), string(content))
+
+		decrypt, err = gaes.Decrypt([]byte(content_16_iv), key_16, iv)
+		gtest.Assert(err, nil)
+		gtest.Assert(string(decrypt), string(content))
+
+		decrypt, err = gaes.Decrypt([]byte(content_32_iv), key_32, iv)
+		gtest.Assert(err, nil)
+		gtest.Assert(string(decrypt), string(content))
+
+		decrypt, err = gaes.Decrypt([]byte(content_32_iv), keys, iv)
+		gtest.Assert(err, "invalid padding")
 	})
 }
 
@@ -92,33 +123,5 @@ func TestPKCS5UnPaddingErr(t *testing.T) {
 		// PKCS5UnPadding src len > blockSize
 		_, err = gaes.PKCS5UnPadding(key_32_err, 32)
 		gtest.AssertNE(err, nil)
-	})
-}
-
-func TestDecrypt(t *testing.T) {
-	gtest.Case(t, func() {
-		encrypt, err := gaes.Encrypt(content, key_16)
-		decrypt, err := gaes.Decrypt(encrypt, key_16)
-		gtest.Assert(err, nil)
-		gtest.Assert(string(decrypt), string(content))
-
-		encrypt, err = gaes.Encrypt(content, key_24)
-		decrypt, err = gaes.Decrypt(encrypt, key_24)
-		gtest.Assert(err, nil)
-		gtest.Assert(string(decrypt), string(content))
-
-		encrypt, err = gaes.Encrypt(content, key_32)
-		decrypt, err = gaes.Decrypt(encrypt, key_32)
-		gtest.Assert(err, nil)
-		gtest.Assert(string(decrypt), string(content))
-
-		encrypt, err = gaes.Encrypt(content, key_32, iv)
-		decrypt, err = gaes.Decrypt(encrypt, key_32, iv)
-		gtest.Assert(err, nil)
-		gtest.Assert(string(decrypt), string(content))
-
-		encrypt, err = gaes.Encrypt(content, key_32, iv)
-		decrypt, err = gaes.Decrypt(encrypt, keys, iv)
-		gtest.Assert(err, "invalid padding")
 	})
 }
