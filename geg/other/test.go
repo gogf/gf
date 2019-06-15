fe152dfa63aca5be4037ca6e653fe66c3cb5de2f
John <john@johng.cn>
2019-06-15 16:53:36 +0800

    comment updates for gtree; fix issue in gtree.AVLTree.Remove

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"fmt"
+	"github.com/gogf/gf/g/container/gtree"
+	"github.com/gogf/gf/g/util/gutil"
 )
 
 func main() {
-	var i float64 = 0
-	for index := 0; index < 10; index++ {
-		i += 0.1
-		fmt.Println(i)
-	}
-}
-=======
-	"github.com/gogf/gf/g/encoding/gjson"
-)
+	expect := map[interface{}]interface{}{
+		20: "val20",
+		6:  "val6",
+		10: "val10",
+		12: "val12",
+		1:  "val1",
+		15: "val15",
+		19: "val19",
+		8:  "val8",
+		4:  "val4"}
+	m := gtree.NewAVLTreeFrom(gutil.ComparatorInt, expect)
+	m.Print()
 
-func main() {
-	j := gjson.New(`[1,2,3]`)
-	j.Remove("1")
-	j.Dump()
+	//m := avltree.NewWithIntComparator()
+	//m.Remove()
+	fmt.Println(1, m.Remove(1))// 应该输出val1，但输出nil
+	fmt.Println(2, m.Remove(1))
+	fmt.Println(3, m.Get(1))
+
+	fmt.Println(4, m.Remove(20))// 应该输出val20，但输出nil
+	fmt.Println(5, m.Remove(20))
+	fmt.Println(6, m.Get(20))
 }
->>>>>>> master
+
