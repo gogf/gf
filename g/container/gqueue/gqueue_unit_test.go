ab38b709b2ecc8370f930b711d4cef9127877086
jroam <yybjroam@qq.com>
2019-06-15 18:44:22 +0800

    add gqueue tests

--- g/container/gqueue/gqueue_unit_test.go
+++ g/container/gqueue/gqueue_unit_test.go
@@ -6,28 +6,28 @@ import (
 	"testing"
 )
 
-
-func TestQueue_Size(t *testing.T) {
-	q1:=gqueue.New(2)
+func TestQueue_Len(t *testing.T) {
+	q1 := gqueue.New(2)
 	q1.Push(1)
 	q1.Push(2)
-	gtest.Assert(q1.Len(),2)
+	gtest.Assert(q1.Len(), 2)
+	gtest.Assert(q1.Size(), 2)
 }
 
 func TestQueue_Pop(t *testing.T) {
-	q1:=gqueue.New()
+	q1 := gqueue.New()
 	q1.Push(1)
 	q1.Push(2)
 	q1.Push(3)
-	i1:=q1.Pop()
-	gtest.Assert(i1,1)
+	i1 := q1.Pop()
+	gtest.Assert(i1, 1)
 }
 
 func TestQueue_Close(t *testing.T) {
-	q1:=gqueue.New()
+	q1 := gqueue.New()
 	q1.Push(1)
 	q1.Push(2)
-	gtest.Assert(q1.Size(),2)
+	gtest.Assert(q1.Len(), 2)
 	q1.Close()
-	gtest.Assert(q1.Size(),2)
+	gtest.Assert(q1.Len(), 2)
 }
