677549ec1510d37beedb5f8fe6f950ae4e5fc433
jroam <yybjroam@qq.com>
2019-06-16 16:41:12 +0800

    add some gqueue tests

--- g/container/gqueue/gqueue_unit_test.go
+++ g/container/gqueue/gqueue_unit_test.go
@@ -7,17 +7,54 @@ import (
 )
 
 func TestQueue_Len(t *testing.T) {
-	q1 := gqueue.New()
-	q1.Push(1)
-	q1.Push(5)
-	gtest.Assert(q1.Len(), 2)
+	q1 := gqueue.New(300)
+	for i := 0; i < 200; i++ {
+		q1.Push(i)
+	}
+	gtest.Assert(q1.Len(), 200)
 }
 
 func TestQueue_Pop(t *testing.T) {
 	q1 := gqueue.New()
+
 	q1.Push(1)
 	q1.Push(2)
-	i1:=q1.Pop()
+	i1 := q1.Pop()
 	gtest.Assert(i1, 1)
+	q1.Close()
+	i1 = q1.Pop()
+	gtest.Assert(i1, 2)
+
+	maxs := 12
+	q2 := gqueue.New(maxs)
+	for i := 0; i < maxs; i++ {
+		q2.Push(i)
+	}
+
+	i3 := q2.Pop()
+	gtest.Assert(i3, 0)
+
 }
 
+func TestQueue_Pop2(t *testing.T) {
+
+	maxs := 14
+	q2 := gqueue.New(maxs)
+	for i := 1; i < maxs; i++ {
+		q2.Push(i)
+	}
+	i3 := q2.Pop()
+	gtest.Assert(i3, 1)
+
+}
+
+func TestQueue_Close(t *testing.T) {
+	q1 := gqueue.New()
+	q1.Push(1)
+	q1.Push(2)
+	gtest.Assert(q1.Len(), 2)
+
+	q1.Close()
+	gtest.Assert(q1.Len(), 2)
+
+}
