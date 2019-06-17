f7d1613d62ea67e25eefbce302c9794df96a3423
John <john@johng.cn>
2019-06-17 19:54:00 +0800

    improve gqueue

--- geg/other/test.go
+++ geg/other/test.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"github.com/gogf/gf/g/container/gqueue"
-	"github.com/gogf/gf/g/test/gtest"
+	"fmt"
+	"sync"
 )
 
 func main() {
-	max := 100
-	q := gqueue.New(max)
-	for i := 1; i < max; i++ {
-		q.Push(i)
-	}
-	q.Close()
-	gtest.Assert(q.Len(), 1)
-
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	wg.Add(-100)
+	wg.Add()
+	wg.Wait()
+	fmt.Println(1)
 }
