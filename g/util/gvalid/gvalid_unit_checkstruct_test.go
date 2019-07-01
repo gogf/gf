eb8f6fb98ac325a6112805e48552652387d3ec7e
陈国潮 <2892931976@qq.com>
2019-07-01 16:02:09 +0800

    gvalid test 20190701 15:58

--- g/util/gvalid/gvalid_unit_checkstruct_test.go
+++ g/util/gvalid/gvalid_unit_checkstruct_test.go
@@ -7,66 +7,218 @@
 package gvalid_test
 
 import (
-    "github.com/gogf/gf/g/test/gtest"
-    "github.com/gogf/gf/g/util/gvalid"
-    "testing"
+	"testing"
+
+	"github.com/gogf/gf/g/test/gtest"
+	"github.com/gogf/gf/g/util/gvalid"
 )
 
 func Test_CheckStruct(t *testing.T) {
-    gtest.Case(t, func() {
-        type Object struct {
-            Name string
-            Age  int
-        }
-        rules := map[string]string {
-            "Name" : "required|length:6,16",
-            "Age"  : "between:18,30",
-        }
-        msgs  := map[string]interface{} {
-            "Name" : map[string]string {
-                "required" : "名称不能为空",
-                "length"   : "名称长度为:min到:max个字符",
-            },
-            "Age"  : "年龄为18到30周岁",
-        }
-        obj := &Object{"john", 16}
-        err := gvalid.CheckStruct(obj, rules, msgs)
-        gtest.AssertNE(err,                          nil)
-        gtest.Assert(len(err.Maps()),                2)
-        gtest.Assert(err.Maps()["Name"]["required"], "")
-        gtest.Assert(err.Maps()["Name"]["length"],   "名称长度为6到16个字符")
-        gtest.Assert(err.Maps()["Age"]["between"],   "年龄为18到30周岁")
-    })
+	gtest.Case(t, func() {
+		type Object struct {
+			Name string
+			Age  int
+		}
+		rules := []string{
+			"@required|length:6,16",
+			"@between:18,30",
+		}
+		msgs := map[string]interface{}{
+			"Name": map[string]string{
+				"required": "名称不能为空",
+				"length":   "名称长度为:min到:max个字符",
+			},
+			"Age": "年龄为18到30周岁",
+		}
+		obj := &Object{"john", 16}
+		err := gvalid.CheckStruct(obj, rules, msgs)
+		gtest.Assert(err, nil)
+	})
 
-    gtest.Case(t, func() {
-        type LoginRequest struct {
-            Username string `json:"username" gvalid:"username@required#用户名不能为空"`
-            Password string `json:"password" gvalid:"password@required#登录密码不能为空"`
-        }
-        var login LoginRequest
-        err := gvalid.CheckStruct(login, nil)
-        gtest.AssertNE(err,                              nil)
-        gtest.Assert(len(err.Maps()),                    2)
-        gtest.Assert(err.Maps()["username"]["required"], "用户名不能为空")
-        gtest.Assert(err.Maps()["password"]["required"], "登录密码不能为空")
-    })
+	gtest.Case(t, func() {
+		type Object struct {
+			Name string
+			Age  int
+		}
+		rules := []string{
+			"Name@required|length:6,16#名称不能为空",
+			"Age@between:18,30",
+		}
+		msgs := map[string]interface{}{
+			"Name": map[string]string{
+				"required": "名称不能为空",
+				"length":   "名称长度为:min到:max个字符",
+			},
+			"Age": "年龄为18到30周岁",
+		}
+		obj := &Object{"john", 16}
+		err := gvalid.CheckStruct(obj, rules, msgs)
+		gtest.AssertNE(err, nil)
+		gtest.Assert(len(err.Maps()), 2)
+		gtest.Assert(err.Maps()["Name"]["required"], "")
+		gtest.Assert(err.Maps()["Name"]["length"], "名称长度为6到16个字符")
+		gtest.Assert(err.Maps()["Age"]["between"], "年龄为18到30周岁")
+	})
 
-    gtest.Case(t, func() {
-        type User struct {
-            Id       int    `gvalid:"uid@required|min:10#|ID不能为空"`
-            Age      int    `gvalid:"age@required#年龄不能为空"`
-            Username string `json:"username" gvalid:"username@required#用户名不能为空"`
-            Password string `json:"password" gvalid:"password@required#登录密码不能为空"`
-        }
-        user := &User{
-            Id       : 1,
-            Username : "john",
-            Password : "123456",
-        }
-        err := gvalid.CheckStruct(user, nil)
-        gtest.AssertNE(err,                              nil)
-        gtest.Assert(len(err.Maps()),                    1)
-        gtest.Assert(err.Maps()["uid"]["min"],           "ID不能为空")
-    })
+	gtest.Case(t, func() {
+		type Object struct {
+			Name string
+			Age  int
+		}
+		rules := []string{
+			"Name@required|length:6,16#名称不能为空|",
+			"Age@between:18,30",
+		}
+		msgs := map[string]interface{}{
+			"Name": map[string]string{
+				"required": "名称不能为空",
+				"length":   "名称长度为:min到:max个字符",
+			},
+			"Age": "年龄为18到30周岁",
+		}
+		obj := &Object{"john", 16}
+		err := gvalid.CheckStruct(obj, rules, msgs)
+		gtest.AssertNE(err, nil)
+		gtest.Assert(len(err.Maps()), 2)
+		gtest.Assert(err.Maps()["Name"]["required"], "")
+		gtest.Assert(err.Maps()["Name"]["length"], "名称长度为6到16个字符")
+		gtest.Assert(err.Maps()["Age"]["between"], "年龄为18到30周岁")
+	})
 
+	gtest.Case(t, func() {
+		type Object struct {
+			Name string
+			Age  int
+		}
+		rules := map[string]string{
+			"Name": "required|length:6,16",
+			"Age":  "between:18,30",
+		}
+		msgs := map[string]interface{}{
+			"Name": map[string]string{
+				"required": "名称不能为空",
+				"length":   "名称长度为:min到:max个字符",
+			},
+			"Age": "年龄为18到30周岁",
+		}
+		obj := &Object{"john", 16}
+		err := gvalid.CheckStruct(obj, rules, msgs)
+		gtest.AssertNE(err, nil)
+		gtest.Assert(len(err.Maps()), 2)
+		gtest.Assert(err.Maps()["Name"]["required"], "")
+		gtest.Assert(err.Maps()["Name"]["length"], "名称长度为6到16个字符")
+		gtest.Assert(err.Maps()["Age"]["between"], "年龄为18到30周岁")
+	})
+
+	gtest.Case(t, func() {
+		type LoginRequest struct {
+			Username string `json:"username" gvalid:"username@required#用户名不能为空"`
+			Password string `json:"password" gvalid:"password@required#登录密码不能为空"`
+		}
+		var login LoginRequest
+		err := gvalid.CheckStruct(login, nil)
+		gtest.AssertNE(err, nil)
+		gtest.Assert(len(err.Maps()), 2)
+		gtest.Assert(err.Maps()["username"]["required"], "用户名不能为空")
+		gtest.Assert(err.Maps()["password"]["required"], "登录密码不能为空")
+	})
+
+	gtest.Case(t, func() {
+		type LoginRequest struct {
+			Username string `json:"username" gvalid:"@required#用户名不能为空"`
+			Password string `json:"password" gvalid:"@required#登录密码不能为空"`
+		}
+		var login LoginRequest
+		err := gvalid.CheckStruct(login, nil)
+		gtest.Assert(err, nil)
+	})
+
+	gtest.Case(t, func() {
+		type LoginRequest struct {
+			username string `json:"username" gvalid:"username@required#用户名不能为空"`
+			Password string `json:"password" gvalid:"password@required#登录密码不能为空"`
+		}
+		var login LoginRequest
+		err := gvalid.CheckStruct(login, nil)
+		gtest.AssertNE(err, nil)
+		gtest.Assert(err.Maps()["password"]["required"], "登录密码不能为空")
+	})
+
+	// gvalid tag
+	gtest.Case(t, func() {
+		type User struct {
+			Id       int    `gvalid:"uid@required|min:10#|ID不能为空"`
+			Age      int    `gvalid:"age@required#年龄不能为空"`
+			Username string `json:"username" gvalid:"username@required#用户名不能为空"`
+			Password string `json:"password" gvalid:"password@required#登录密码不能为空"`
+		}
+		user := &User{
+			Id:       1,
+			Username: "john",
+			Password: "123456",
+		}
+		err := gvalid.CheckStruct(user, nil)
+		gtest.AssertNE(err, nil)
+		gtest.Assert(len(err.Maps()), 1)
+		gtest.Assert(err.Maps()["uid"]["min"], "ID不能为空")
+	})
+
+	gtest.Case(t, func() {
+		type User struct {
+			Id       int    `gvalid:"uid@required|min:10#|ID不能为空"`
+			Age      int    `gvalid:"age@required#年龄不能为空"`
+			Username string `json:"username" gvalid:"username@required#用户名不能为空"`
+			Password string `json:"password" gvalid:"password@required#登录密码不能为空"`
+		}
+		user := &User{
+			Id:       1,
+			Username: "john",
+			Password: "123456",
+		}
+
+		rules := []string{
+			"username@required#用户名不能为空",
+		}
+		
+		err := gvalid.CheckStruct(user, rules)
+		gtest.AssertNE(err, nil)
+		gtest.Assert(len(err.Maps()), 1)
+		gtest.Assert(err.Maps()["uid"]["min"], "ID不能为空")
+	})
+
+	gtest.Case(t, func() {
+		type User struct {
+			Id       int    `gvalid:"uid@required|min:10#ID不能为空"`
+			Age      int    `gvalid:"age@required#年龄不能为空"`
+			Username string `json:"username" gvalid:"username@required#用户名不能为空"`
+			Password string `json:"password" gvalid:"password@required#登录密码不能为空"`
+		}
+		user := &User{
+			Id:       1,
+			Username: "john",
+			Password: "123456",
+		}
+		err := gvalid.CheckStruct(user, nil)
+		gtest.AssertNE(err, nil)
+		gtest.Assert(len(err.Maps()), 1)
+	})
+
+	// valid tag
+	gtest.Case(t, func() {
+		type User struct {
+			Id       int    `valid:"uid@required|min:10#|ID不能为空"`
+			Age      int    `valid:"age@required#年龄不能为空"`
+			Username string `json:"username" gvalid:"username@required#用户名不能为空"`
+			Password string `json:"password" gvalid:"password@required#登录密码不能为空"`
+		}
+		user := &User{
+			Id:       1,
+			Username: "john",
+			Password: "123456",
+		}
+		err := gvalid.CheckStruct(user, nil)
+		gtest.AssertNE(err, nil)
+		gtest.Assert(len(err.Maps()), 1)
+		gtest.Assert(err.Maps()["uid"]["min"], "ID不能为空")
+	})
 }
--- g/util/gvalid/gvalid_unit_customerror_test.go
+++ g/util/gvalid/gvalid_unit_customerror_test.go
@@ -7,74 +7,88 @@
 package gvalid_test
 
 import (
-    "github.com/gogf/gf/g/util/gvalid"
-    "strings"
-    "testing"
+	"strings"
+	"testing"
+
+	"github.com/gogf/gf/g/test/gtest"
+	"github.com/gogf/gf/g/util/gvalid"
 )
 
+func Test_Map(t *testing.T) {
+	err := &gvalid.Error{}
+	m := err.Map()
+	gtest.Assert(m, nil)
+}
+
+func Test_FirstString(t *testing.T) {
+	err := &gvalid.Error{}
+	n := err.FirstString()
+	gtest.Assert(n, "")
+}
+
 func Test_SetDefaultErrorMsgs(t *testing.T) {
-    rule := "integer|length:6,16"
-    msgs := map[string]string {
-        "integer" : "请输入一个整数",
-        "length"  : "参数长度不对啊老铁",
-    }
-    gvalid.SetDefaultErrorMsgs(msgs)
-    e := gvalid.Check("6.66", rule, nil)
-    if e == nil || len(e.Map()) != 2 {
-        t.Error("规则校验失败")
-    } else {
-        if v, ok := e.Map()["integer"]; ok {
-            if strings.Compare(v, msgs["integer"]) != 0 {
-                t.Error("错误信息不匹配")
-            }
-        }
-        if v, ok := e.Map()["length"]; ok {
-            if strings.Compare(v, msgs["length"]) != 0 {
-                t.Error("错误信息不匹配")
-            }
-        }
-    }
+	rule := "integer|length:6,16"
+	msgs := map[string]string{
+		"integer": "请输入一个整数",
+		"length":  "参数长度不对啊老铁",
+	}
+	gvalid.SetDefaultErrorMsgs(msgs)
+	e := gvalid.Check("6.66", rule, nil)
+	if e == nil || len(e.Map()) != 2 {
+		t.Error("规则校验失败")
+	} else {
+		if v, ok := e.Map()["integer"]; ok {
+			if strings.Compare(v, msgs["integer"]) != 0 {
+				t.Error("错误信息不匹配")
+			}
+		}
+		if v, ok := e.Map()["length"]; ok {
+			if strings.Compare(v, msgs["length"]) != 0 {
+				t.Error("错误信息不匹配")
+			}
+		}
+	}
 }
 
 func Test_CustomError1(t *testing.T) {
-    rule := "integer|length:6,16"
-    msgs := map[string]string {
-        "integer" : "请输入一个整数",
-        "length"  : "参数长度不对啊老铁",
-    }
-    e := gvalid.Check("6.66", rule, msgs)
-    if e == nil || len(e.Map()) != 2 {
-        t.Error("规则校验失败")
-    } else {
-        if v, ok := e.Map()["integer"]; ok {
-            if strings.Compare(v, msgs["integer"]) != 0 {
-                t.Error("错误信息不匹配")
-            }
-        }
-        if v, ok := e.Map()["length"]; ok {
-            if strings.Compare(v, msgs["length"]) != 0 {
-                t.Error("错误信息不匹配")
-            }
-        }
-    }
+	rule := "integer|length:6,16"
+	msgs := map[string]string{
+		"integer": "请输入一个整数",
+		"length":  "参数长度不对啊老铁",
+	}
+	e := gvalid.Check("6.66", rule, msgs)
+	if e == nil || len(e.Map()) != 2 {
+		t.Error("规则校验失败")
+	} else {
+		if v, ok := e.Map()["integer"]; ok {
+			if strings.Compare(v, msgs["integer"]) != 0 {
+				t.Error("错误信息不匹配")
+			}
+		}
+		if v, ok := e.Map()["length"]; ok {
+			if strings.Compare(v, msgs["length"]) != 0 {
+				t.Error("错误信息不匹配")
+			}
+		}
+	}
 }
 
 func Test_CustomError2(t *testing.T) {
-    rule := "integer|length:6,16"
-    msgs := "请输入一个整数|参数长度不对啊老铁"
-    e := gvalid.Check("6.66", rule, msgs)
-    if e == nil || len(e.Map()) != 2 {
-        t.Error("规则校验失败")
-    } else {
-        if v, ok := e.Map()["integer"]; ok {
-            if strings.Compare(v, "请输入一个整数") != 0 {
-                t.Error("错误信息不匹配")
-            }
-        }
-        if v, ok := e.Map()["length"]; ok {
-            if strings.Compare(v, "参数长度不对啊老铁") != 0 {
-                t.Error("错误信息不匹配")
-            }
-        }
-    }
+	rule := "integer|length:6,16"
+	msgs := "请输入一个整数|参数长度不对啊老铁"
+	e := gvalid.Check("6.66", rule, msgs)
+	if e == nil || len(e.Map()) != 2 {
+		t.Error("规则校验失败")
+	} else {
+		if v, ok := e.Map()["integer"]; ok {
+			if strings.Compare(v, "请输入一个整数") != 0 {
+				t.Error("错误信息不匹配")
+			}
+		}
+		if v, ok := e.Map()["length"]; ok {
+			if strings.Compare(v, "参数长度不对啊老铁") != 0 {
+				t.Error("错误信息不匹配")
+			}
+		}
+	}
 }
