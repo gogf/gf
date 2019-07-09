145fccdf6e1e85fa767053e00cb54db84c5310cb
john <john@johng.cn>
2019-07-09 14:50:56 +0800

    improve configuraion style for gdb

--- geg/database/gdb/mysql/gdb_config3.go
+++ geg/database/gdb/mysql/gdb_config3.go
@@ -6,7 +6,15 @@ import (
 )
 
 func main() {
+	g.Config().SetFileName("config3.toml")
 	if r, err := g.DB().Table("user").Where("uid=?", 1).One(); err == nil {
+		fmt.Println(r["uid"].Int())
+		fmt.Println(r["name"].String())
+	} else {
+		fmt.Println(err)
+	}
+
+	if r, err := g.DB("user").Table("user").Where("uid=?", 1).One(); err == nil {
 		fmt.Println(r["uid"].Int())
 		fmt.Println(r["name"].String())
 	} else {
