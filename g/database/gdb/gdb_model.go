28e5c33e8137dac039c8d5c0948f4b08532abb8b
John <john@johng.cn>
2019-07-13 11:35:22 +0800

    comment updates for gdb

--- g/database/gdb/gdb_model.go
+++ g/database/gdb/gdb_model.go
@@ -3,8 +3,6 @@
 // This Source Code Form is subject to the terms of the MIT License.
 // If a copy of the MIT was not distributed with this file,
 // You can obtain one at https://github.com/gogf/gf.
-//
-// @author john, ymrjqyy
 
 package gdb
 
