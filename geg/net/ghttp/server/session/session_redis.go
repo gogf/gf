a36e00efeb0d60175328bd80e38f96411ccba2e4
John <john@johng.cn>
2019-07-28 13:10:34 +0800

    improve ghttp for session storage with redis server

--- geg/net/ghttp/server/session/session_redis.go
+++ geg/net/ghttp/server/session/session_redis.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-
 	"github.com/gogf/gf/g"
 	"github.com/gogf/gf/g/net/ghttp"
 	"github.com/gogf/gf/g/os/gtime"
@@ -22,17 +20,21 @@ func SessionGet(r *ghttp.Request) {
 // 请求处理之前将Redis中的数据读取出来并存储到SESSION对象中。
 func RedisHandlerGet(r *ghttp.Request) {
 	if !r.IsFileRequest() {
-		sessionId := r.Cookie.GetSessionId()
-		if sessionId == "" {
+		id := r.Cookie.GetSessionId()
+		if id == "" {
 			return
 		}
-		value, err := g.Redis().DoVar("GET", sessionId)
+		// 当内存中的SESSION存在时不需要读取Redis
+		if r.Session.Size() > 0 {
+			return
+		}
+		// SESSION不存在时，例如服务重启，自动从Redis读取并恢复数据
+		value, err := g.Redis().DoVar("GET", id)
 		if err != nil {
 			panic(err)
 		}
 		if !value.IsNil() {
-			err := r.Session.RestoreFromJson(value.Bytes())
-			if err != nil {
+			if err := r.Session.Restore(value.Bytes()); err != nil {
 				panic(err)
 			}
 		}
@@ -42,25 +44,30 @@ func RedisHandlerGet(r *ghttp.Request) {
 // 请求结束时将SESSION数据存储到Redis中，或者在SESSION删除时也删除Redis中的数据。
 func RedisHandlerSet(r *ghttp.Request) {
 	if !r.IsFileRequest() {
-		sessionId := r.Cookie.GetSessionId()
-		if sessionId == "" {
+		id := r.Cookie.GetSessionId()
+		if id == "" {
 			return
 		}
 		err := (error)(nil)
+		value := ([]byte)(nil)
 		if r.Session.Size() > 0 {
-			value, err := json.Marshal(r.Session)
-			if err != nil {
-				panic(err)
-			}
-			_, err = g.Redis().Do("SETEX", r.Cookie.GetSessionId(), r.Server.GetSessionMaxAge(), value)
-			if err != nil {
-				panic(err)
+			if value, err = r.Session.Export(); err == nil {
+				if len(value) == 0 {
+					return
+				} else if !r.Session.IsDirty() {
+					// 更新过期时间
+					_, err = g.Redis().Do("EXPIRE", id, r.Server.GetSessionMaxAge())
+				} else {
+					// 更新Redis数据
+					_, err = g.Redis().Do("SETEX", id, r.Server.GetSessionMaxAge(), value)
+				}
 			}
 		} else {
-			_, err = g.Redis().Do("DEL", r.Cookie.GetSessionId())
-			if err != nil {
-				panic(err)
-			}
+			// 清空SESSION后自动删除Redis数据
+			_, err = g.Redis().Do("DEL", id)
+		}
+		if err != nil {
+			panic(err)
 		}
 	}
 }
