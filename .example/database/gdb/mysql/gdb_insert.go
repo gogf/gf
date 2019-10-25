734728fa9c221b2882494d2c93095efe455b76d0
John <john@johng.cn>
2019-10-25 19:54:02 +0800

    improve time handling for gdb

--- .example/database/gdb/mysql/gdb_insert.go
+++ .example/database/gdb/mysql/gdb_insert.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/gogf/gf/database/gdb"
 	"time"
-
-	"github.com/gogf/gf/frame/g"
 )
 
 func main() {
-	db := g.DB()
-	// 开启调试模式，以便于记录所有执行的SQL
+	//db := g.DB()
+
+	gdb.AddDefaultConfigNode(gdb.ConfigNode{
+		LinkInfo: "root:12345678@tcp(127.0.0.1:3306)/test?parseTime=true&loc=Local",
+		Type:     "mysql",
+		Charset:  "utf8",
+	})
+	db, _ := gdb.New()
+
 	db.SetDebug(true)
 
-	fmt.Println(time.Now())
-	r, e := db.Table("user").Data(g.Map{
-		"create_time": time.Now().Local(),
-	}).Insert()
+	type User struct {
+		CreateTime time.Time `orm:"create_time"`
+	}
+	r, e := db.Table("user").Data(User{CreateTime: time.Now()}).Insert()
 	if e != nil {
 		panic(e)
 	}
