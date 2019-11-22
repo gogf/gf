9b1cc6e9c7ae85866050929441b3ba849babaf23
John <john@johng.cn>
2019-11-22 19:45:42 +0800

    add special handle for configuration of mssql for gdb

--- frame/gins/gins.go
+++ frame/gins/gins.go
@@ -205,8 +205,14 @@ func parseDBConfigNode(value interface{}) *gdb.ConfigNode {
 	if node.LinkInfo != "" && node.Type == "" {
 		match, _ := gregex.MatchString(`([a-z]+):(.+)`, node.LinkInfo)
 		if len(match) == 3 {
-			node.Type = match[1]
-			node.LinkInfo = match[2]
+			// Special handle for mssql for common usage purpose.
+			if match[1] == "sqlserver" {
+				node.Type = "mssql"
+				node.LinkInfo = node.LinkInfo
+			} else {
+				node.Type = match[1]
+				node.LinkInfo = match[2]
+			}
 		}
 	}
 	return node
