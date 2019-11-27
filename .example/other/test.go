f4654bf4465a4187016331ab96d887cca095546c
John <john@johng.cn>
2019-11-27 23:19:07 +0800

    fix comment issue in example codes for gmap

--- .example/other/test.go
+++ .example/other/test.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gogf/gf/text/gstr"
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
 )
 
 func main() {
-	fmt.Println(gstr.CamelCase("/auth/routerName.html"))
+	s := g.Server()
+	s.SetIndexFolder(true)
+	s.BindHandler("/admin.html", func(r *ghttp.Request) {
+		r.Response.Write("admin")
+	})
+	s.BindHandler("/admin-do-{page}.html", func(r *ghttp.Request) {
+		r.Response.Write("admin-do-" + r.GetString("page"))
+	})
+	s.SetPort(8999)
+	s.Run()
 }
