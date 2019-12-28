134e4cf28fadd9f91eb5de519586f6196987f939
John <john@johng.cn>
2019-12-28 13:55:05 +0800

    improve gfile.CopyDir function; add Model function for gdb.Model

--- .example/other/test.go
+++ .example/other/test.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"github.com/gogf/gf/net/ghttp"
+	"github.com/gogf/gf/frame/g"
 )
 
 func main() {
-	s := ghttp.GetServer()
-	s.BindHandler("/log/path", func(r *ghttp.Request) {
-		r.Response.Writeln("请到/tmp/gf.log目录查看日志")
-	})
-	s.SetLogPath("/tmp/gf.log")
-	s.SetAccessLogEnabled(true)
-	s.SetErrorLogEnabled(true)
-	s.SetPort(8199)
-	s.Run()
+	db := g.DB()
+	// 开启调试模式，以便于记录所有执行的SQL
+	db.SetDebug(true)
+
+	db.Table("user").Delete("score < ", 60)
 }
