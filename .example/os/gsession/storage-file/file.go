506552c3a93a9094c96699bdf62c533b5b4f42c6
John <john@johng.cn>
2020-01-04 17:19:50 +0800

    example codes update; donator updates

--- .example/os/gsession/storage-file/file.go
+++ .example/os/gsession/storage-file/file.go
@@ -12,16 +12,18 @@ func main() {
 	s.SetConfigWithMap(g.Map{
 		"SessionMaxAge": time.Minute,
 	})
-	s.BindHandler("/set", func(r *ghttp.Request) {
-		r.Session.Set("time", gtime.Second())
-		r.Response.Write("ok")
-	})
-	s.BindHandler("/get", func(r *ghttp.Request) {
-		r.Response.Write(r.Session.Map())
-	})
-	s.BindHandler("/del", func(r *ghttp.Request) {
-		r.Session.Clear()
-		r.Response.Write("ok")
+	s.Group("/", func(group *ghttp.RouterGroup) {
+		group.ALL("/set", func(r *ghttp.Request) {
+			r.Session.Set("time", gtime.Timestamp())
+			r.Response.Write("ok")
+		})
+		group.ALL("/get", func(r *ghttp.Request) {
+			r.Response.Write(r.Session.Map())
+		})
+		group.ALL("/del", func(r *ghttp.Request) {
+			r.Session.Clear()
+			r.Response.Write("ok")
+		})
 	})
 	s.SetPort(8199)
 	s.Run()
--- .example/os/gsession/storage-redis/redis.go
+++ .example/os/gsession/storage-redis/redis.go
@@ -14,16 +14,18 @@ func main() {
 		"SessionMaxAge":  time.Minute,
 		"SessionStorage": gsession.NewStorageRedis(g.Redis()),
 	})
-	s.BindHandler("/set", func(r *ghttp.Request) {
-		r.Session.Set("time", gtime.Second())
-		r.Response.Write("ok")
-	})
-	s.BindHandler("/get", func(r *ghttp.Request) {
-		r.Response.Write(r.Session.Map())
-	})
-	s.BindHandler("/del", func(r *ghttp.Request) {
-		r.Session.Clear()
-		r.Response.Write("ok")
+	s.Group("/", func(group *ghttp.RouterGroup) {
+		group.ALL("/set", func(r *ghttp.Request) {
+			r.Session.Set("time", gtime.Timestamp())
+			r.Response.Write("ok")
+		})
+		group.ALL("/get", func(r *ghttp.Request) {
+			r.Response.Write(r.Session.Map())
+		})
+		group.ALL("/del", func(r *ghttp.Request) {
+			r.Session.Clear()
+			r.Response.Write("ok")
+		})
 	})
 	s.SetPort(8199)
 	s.Run()
