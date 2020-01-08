f3a208f02f98a6761be3303d10fe80f768563d7f
John <john@johng.cn>
2020-01-08 20:00:42 +0800

    fix issue in router value retrieving for ghttp.Request

--- .example/other/test.go
+++ .example/other/test.go
@@ -1,14 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/os/gfile"
+)
 
 func main() {
-	type User struct {
-		Id int
-	}
-	u1 := &User{1}
-	u2 := *u1
-	u2.Id = 2
-	fmt.Println(u1)
-	fmt.Println(u2)
+	g.Dump(gfile.ScanDirFile("/Users/john/Temp/test", "*", true))
+	//if err := gfile.ReplaceDir("gf-empty", "app", "/Users/john/Temp/test", "*.*", true); err != nil {
+	//	glog.Fatal("content replacing failed,", err.Error())
+	//}
+
 }
