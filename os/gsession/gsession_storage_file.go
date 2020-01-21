d9f4e6eaa63bad64291f89eae4618609c1125a83
chikaku <chenbc7@gmail.com>
2020-01-21 14:55:08 +0800

    remove redundant code in gsession

--- os/gsession/gsession_storage_file.go
+++ os/gsession/gsession_storage_file.go
@@ -54,7 +54,7 @@ func NewStorageFile(path ...string) *StorageFile {
 	if len(path) > 0 && path[0] != "" {
 		storagePath, _ = gfile.Search(path[0])
 		if storagePath == "" {
-			panic(fmt.Sprintf(fmt.Sprintf("'%s' does not exist", path[0])))
+			panic(fmt.Sprintf("'%s' does not exist", path[0]))
 		}
 		if !gfile.IsWritable(storagePath) {
 			panic(fmt.Sprintf("'%s' is not writable", path[0]))
