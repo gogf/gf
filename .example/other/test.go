9e38b2cb90873010b613bd0303163f23a2e4c9c6
John <john@johng.cn>
2020-04-23 17:14:11 +0800

    add performance testing result

--- .example/other/test.go
+++ .example/other/test.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
-	"github.com/gogf/gf/text/gstr"
 )
 
 func main() {
-	filename := "FDJT02·WS·2013·DQ·D30·0002-11"
-	fmt.Println(len(filename))
-	fmt.Println(gstr.RuneLen(filename)) //值是29
-	fmt.Println(gstr.SubStrRune(filename, 0, 26))
+	b := []byte{3, 0, 0}
+	fmt.Println(string(b))
+	fmt.Println(hex.EncodeToString(b))
 }
