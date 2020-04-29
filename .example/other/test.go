938c46fec97d40bd347eaeefcb6c391a06531e0c
John <john@johng.cn>
2020-04-29 19:33:14 +0800

    readme update

--- .example/other/test.go
+++ .example/other/test.go
@@ -1,92 +1,20 @@
 package main
 
 import (
-	"github.com/gogf/gf/errors/gerror"
+	"fmt"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 )
 
-// ListsInput ListsInput
-type ListsInput struct {
-	Page  int `v:"min:1#page不能小于1"`
-	Limit int `v:"between:10,100#limit必须是10-100之间的整数"`
-	Search
-}
-
-type ListRequest struct {
-	ListsInput
-}
-
-// Search Search
-type Search struct {
-	GoodsName    string
-	GoodsSource  string
-	FreeShipping string
-	MarketPrice  struct {
-		Min       int
-		Max       int
-		IsInclude bool
-	}
-	GuidePrice struct {
-		Min       int
-		Max       int
-		IsInclude bool
-	}
-	AgreementPrice struct {
-		Min       int
-		Max       int
-		IsInclude bool
-	}
-	NormalProfitMargin struct {
-		Min       int
-		Max       int
-		IsInclude bool
-	}
-	ActivityProfitMargin struct {
-		Min       int
-		Max       int
-		IsInclude bool
-	}
-	Sales struct {
-		Cycle    string
-		Quantity struct {
-			Min       int
-			Max       int
-			IsInclude bool
-		}
-	}
-	SalesReturn struct {
-		Cycle    string
-		Quantity struct {
-			Min       int
-			Max       int
-			IsInclude bool
-		}
-	}
-	ChooseGoods struct {
-		Cycle    string
-		Quantity struct {
-			Min       int
-			Max       int
-			IsInclude bool
-		}
-	}
-}
-
-// Lists Lists
-func Lists(r *ghttp.Request) {
-	var params *ListRequest
-	if err := r.Parse(&params); err != nil {
-		r.Response.WriteExit(gerror.Stack(err))
-	}
-
-	r.Response.Write(params)
-}
-
 func main() {
 	s := g.Server()
 	s.Group("/", func(group *ghttp.RouterGroup) {
-		group.POST("/test", Lists)
+		group.ALL("/", func(r *ghttp.Request) {
+			fmt.Println(r.GetBodyString())
+			fmt.Println(r.Header)
+			r.Response.Write(r.GetBodyString())
+			r.Response.Write(r.Header)
+		})
 	})
 	s.SetPort(8199)
 	s.Run()
