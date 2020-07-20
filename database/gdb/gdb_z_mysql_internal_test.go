ee5ddaab523d6824d0efc4fe3a207fe308ec53de
john <john@johng.cn>
2020-07-20 22:47:12 +0800

    fix place holder for pgsql

--- database/gdb/gdb_z_mysql_internal_test.go
+++ database/gdb/gdb_z_mysql_internal_test.go
@@ -81,7 +81,7 @@ func Test_Func_FormatSqlWithArgs(t *testing.T) {
 	// pgsql
 	gtest.C(t, func(t *gtest.T) {
 		var s string
-		s = FormatSqlWithArgs("select * from table where id>=$v1 and sex=$v2", []interface{}{100, 1})
+		s = FormatSqlWithArgs("select * from table where id>=$1 and sex=$2", []interface{}{100, 1})
 		t.Assert(s, "select * from table where id>=100 and sex=1")
 	})
 	// oracle
