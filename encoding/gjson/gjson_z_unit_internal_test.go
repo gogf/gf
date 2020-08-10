4d501fd2f4c7e4031c75613a6b3bf6194571c8d1
Jack <jianchenma@qq.com>
2020-08-10 23:03:35 +0800

    improve function Set for package gjson

---
+++ encoding/gjson/gjson_z_unit_internal_test.go
@@ -0,0 +1,17 @@
+// Copyright 2017 gf Author(https://github.com/gogf/gf). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
+package gjson
+
+//func Test_Load_YAML3(t *testing.T) {
+//	data := []byte(`
+//bb           = """
+//                    dig := dig;                         END;"""
+//`)
+//	gtest.C(t, func(t *gtest.T) {
+//		t.Assert(checkDataType(data), "toml")
+//	})
+//}
