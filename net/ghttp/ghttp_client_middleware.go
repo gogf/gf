c5145dc4f60a9ff591a5bdfaf36bdf0ac8a04deb
eyasliu <liuyuesongde@163.com>
2021-01-17 17:26:53 +0800

    remove abort, actually the abort is unuse

--- net/ghttp/ghttp_client_middleware.go
+++ net/ghttp/ghttp_client_middleware.go
@@ -1,7 +1,6 @@
 package ghttp
 
 import (
-	"github.com/gogf/gf/errors/gerror"
 	"net/http"
 )
 
@@ -29,17 +28,6 @@ func (c *Client) MiddlewareNext(req *http.Request) (*ClientResponse, error) {
 	return c.callRequest(req)
 }
 
-// MiddlewareAbort stop call after all middleware, so it will not send http request
-// this is should only be call in ClientHandlerFunc
-func (c *Client) MiddlewareAbort(req *http.Request) (*ClientResponse, error) {
-	m, ok := req.Context().Value(gfHTTPClientMiddlewareKey).(*clientMiddleware)
-	if ok {
-		m.Abort()
-		return m.resp, m.err
-	}
-	return nil, gerror.New("http request abort")
-}
-
 // ClientHandlerFunc middleware handler func
 type ClientHandlerFunc = func(c *Client, r *http.Request) (*ClientResponse, error)
 
@@ -48,14 +36,13 @@ type clientMiddleware struct {
 	client       *Client             // http client
 	handlers     []ClientHandlerFunc // mdl handlers
 	handlerIndex int                 // current handler index
-	abort        bool                // abort call after handlers
 	resp         *ClientResponse     // save resp
 	err          error               // save err
 }
 
 // Next call next middleware handler, if abort,
 func (m *clientMiddleware) Next(req *http.Request) (resp *ClientResponse, err error) {
-	if m.abort || m.err != nil {
+	if m.err != nil {
 		return m.resp, m.err
 	}
 	if m.handlerIndex < len(m.handlers) {
@@ -65,11 +52,4 @@ func (m *clientMiddleware) Next(req *http.Request) (resp *ClientResponse, err er
 		m.err = err
 	}
 	return
-}
-
-func (m *clientMiddleware) Abort() {
-	m.abort = true
-	if m.err == nil {
-		m.err = gerror.New("http request abort")
-	}
 }
