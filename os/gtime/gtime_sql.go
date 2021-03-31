ed71d2b2acb9d359a545939ad3078aed7c02a4d2
yys <4752017@qq.com>
2021-03-31 14:07:28 +0800

    add test

--- os/gtime/gtime_sql.go
+++ os/gtime/gtime_sql.go
@@ -4,16 +4,25 @@ import (
 	"database/sql/driver"
 )
 
-//add Scanner
+//Scanner is an interface used by Scan.
+//Scan value from database
+//database/sql
 func (t *Time) Scan(value interface{}) error {
+	if t == nil {
+		return nil
+	}
 	newTime := New(value)
 	t.Time = newTime.Time
 	return nil
 }
 
-//add valuer
+// Valuer is the interface providing the Value method. database/sql/driver
+// Value insert into mysql need this function.
 func (t *Time) Value() (driver.Value, error) {
 	if t == nil {
+		return nil, nil
+	}
+	if t.IsZero() {
 		return nil, nil
 	}
 	return t.Time, nil
