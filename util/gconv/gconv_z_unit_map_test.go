eb723e47c2047544d9ae073c2860f7be5a0ffd17
John Guo <john@johng.cn>
2021-06-05 08:58:54 +0800

    fix issue in gconv.MapDeep

--- util/gconv/gconv_z_unit_map_test.go
+++ util/gconv/gconv_z_unit_map_test.go
@@ -317,6 +317,60 @@ func Test_MapDeep2(t *testing.T) {
 	})
 }
 
+func Test_MapDeep3(t *testing.T) {
+	type Base struct {
+		Id   int    `c:"id"`
+		Date string `c:"date"`
+	}
+	type User struct {
+		UserBase Base   `c:"base"`
+		Passport string `c:"passport"`
+		Password string `c:"password"`
+		Nickname string `c:"nickname"`
+	}
+
+	gtest.C(t, func(t *gtest.T) {
+		user := &User{
+			UserBase: Base{
+				Id:   1,
+				Date: "2019-10-01",
+			},
+			Passport: "john",
+			Password: "123456",
+			Nickname: "JohnGuo",
+		}
+		m := gconv.MapDeep(user)
+		t.Assert(m, g.Map{
+			"base": g.Map{
+				"id":   user.UserBase.Id,
+				"date": user.UserBase.Date,
+			},
+			"passport": user.Passport,
+			"password": user.Password,
+			"nickname": user.Nickname,
+		})
+	})
+
+	gtest.C(t, func(t *gtest.T) {
+		user := &User{
+			UserBase: Base{
+				Id:   1,
+				Date: "2019-10-01",
+			},
+			Passport: "john",
+			Password: "123456",
+			Nickname: "JohnGuo",
+		}
+		m := gconv.Map(user)
+		t.Assert(m, g.Map{
+			"base":     user.UserBase,
+			"passport": user.Passport,
+			"password": user.Password,
+			"nickname": user.Nickname,
+		})
+	})
+}
+
 func Test_MapDeepWithAttributeTag(t *testing.T) {
 	type Ids struct {
 		Id  int `c:"id"`
