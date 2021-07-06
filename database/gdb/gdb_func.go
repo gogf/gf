2970864158bd5ae694659f5bed46d11275803b38
John Guo <john@johng.cn>
2021-07-06 21:18:26 +0800

    fix issue #1240

--- database/gdb/gdb_func.go
+++ database/gdb/gdb_func.go
@@ -264,7 +264,7 @@ func DataToMapDeep(value interface{}) map[string]interface{} {
 		name = ""
 		fieldTag = rtField.Tag
 		for _, tag := range structTagPriority {
-			if s := fieldTag.Get(tag); s != "" && gregex.IsMatchString(regularFieldNameWithoutDotRegPattern, s) {
+			if s := fieldTag.Get(tag); s != "" {
 				name = s
 				break
 			}
