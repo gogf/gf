2d319d0856c79c19bf64d313e37e226898b5d287
John Guo <john@johng.cn>
2021-07-08 21:46:21 +0800

    update sql of unit testing cases for package gdb

--- database/gdb/testdata/with_multiple_depends.sql
+++ database/gdb/testdata/with_multiple_depends.sql
@@ -3,7 +3,7 @@ CREATE TABLE `table_a`  (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '',
     PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB;
 
 INSERT INTO `table_a` VALUES (1, 'table_a_test1');
 INSERT INTO `table_a` VALUES (2, 'table_a_test2');
@@ -13,7 +13,7 @@ CREATE TABLE `table_b`  (
     `table_a_id` int(11) NOT NULL,
     `alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '',
     PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB;
 
 INSERT INTO `table_b` VALUES (10, 1, 'table_b_test1');
 INSERT INTO `table_b` VALUES (20, 2, 'table_b_test2');
@@ -25,7 +25,7 @@ CREATE TABLE `table_c`  (
     `table_b_id` int(11) NOT NULL,
     `alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '',
     PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB;
 
 INSERT INTO `table_c` VALUES (100, 10, 'table_c_test1');
 INSERT INTO `table_c` VALUES (200, 10, 'table_c_test2');
