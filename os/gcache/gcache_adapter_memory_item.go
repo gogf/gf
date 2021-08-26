5b57c35522804a8282e888abb69f059c765c25f8
John <john@johng.cn>
2021-08-27 00:01:15 +0800

    comment updates for package gcache

--- os/gcache/gcache_adapter_memory_item.go
+++ os/gcache/gcache_adapter_memory_item.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gogf/gf/os/gtime"
 )
 
-// IsExpired checks whether <item> is expired.
+// IsExpired checks whether `item` is expired.
 func (item *adapterMemoryItem) IsExpired() bool {
 	// Note that it should use greater than or equal judgement here
 	// imagining that the cache time is only 1 millisecond.
