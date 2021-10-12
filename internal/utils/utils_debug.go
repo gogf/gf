18ceebeffd02e16f6c5d6433016f12d32aeede63
John Guo <john@johng.cn>
2021-10-12 17:25:45 +0800

    filter key updates for stack information

--- internal/utils/utils_debug.go
+++ internal/utils/utils_debug.go
@@ -11,8 +11,12 @@ import (
 )
 
 const (
-	commandEnvKeyForDebugKey = "gf.debug"            // Debug key for checking if in debug mode.
-	StackFilterKeyForGoFrame = "github.com/gogf/gf@" // Stack filtering key for all GoFrame module paths.
+	// Debug key for checking if in debug mode.
+	commandEnvKeyForDebugKey = "gf.debug"
+
+	// StackFilterKeyForGoFrame is the stack filtering key for all GoFrame module paths.
+	// Eg: .../pkg/mod/github.com/gogf/gf/v2@v2.0.0-20211011134327-54dd11f51122/debug/gdebug/gdebug_caller.go
+	StackFilterKeyForGoFrame = "github.com/gogf/gf/v"
 )
 
 var (
