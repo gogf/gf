183d800f4cf5a5083003a51092972bbfea826dc1
daguang <lianghuanan1997@gmail.com>
2021-11-01 12:17:53 +0800

    :memo: add example

---
+++ text/gregex/gregex_z_example_test.go
@@ -0,0 +1,67 @@
+// Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
+package gregex_test
+
+func ExampleIsMatch() {
+
+}
+
+func ExampleIsMatchString() {
+
+}
+
+func ExampleMatch() {
+
+}
+
+func ExampleMatchAll() {
+
+}
+
+func ExampleMatchAllString() {
+
+}
+
+func ExampleMatchString() {
+
+}
+
+func ExampleQuote() {
+
+}
+
+func ExampleReplace() {
+
+}
+
+func ExampleReplaceFunc() {
+
+}
+
+func ExampleReplaceFuncMatch() {
+
+}
+
+func ExampleReplaceString() {
+
+}
+
+func ExampleReplaceStringFunc() {
+
+}
+
+func ExampleReplaceStringFuncMatch() {
+
+}
+
+func ExampleSplit() {
+
+}
+
+func ExampleValidate() {
+
+}
