2866ee75a1eed4fa24667dd1cc0ec9e7ef6e4947
huangqian <huangqian1985@qq.com>
2021-11-24 23:23:14 +0800

    Implemented BTree Example 1.Clone 2.Set 3.Sets 4.Get 5.GetOrSet 6.GetOrSetFunc 7.GetOrSetFuncLock

--- container/gtree/gtree_z_example_btree_test.go
+++ container/gtree/gtree_z_example_btree_test.go
@@ -6,32 +6,121 @@
 
 package gtree_test
 
-func ExampleBTree_Clone() {
+import (
+	"fmt"
+	"github.com/gogf/gf/v2/container/gtree"
+	"github.com/gogf/gf/v2/util/gconv"
+	"github.com/gogf/gf/v2/util/gutil"
+)
 
+func ExampleBTree_Clone() {
+	b := gtree.NewBTree(3, gutil.ComparatorString)
+	for i := 0; i < 6; i++ {
+		b.Set("key"+gconv.String(i), "val"+gconv.String(i))
+	}
+
+	newTree := b.Clone()
+
+	fmt.Println(newTree.Map())
+	fmt.Println(newTree.Size())
+
+	// output:
+	// map[key0:val0 key1:val1 key2:val2 key3:val3 key4:val4 key5:val5]
+	// 6
 }
 
 func ExampleBTree_Set() {
+	newTree := gtree.NewBTree(3, gutil.ComparatorString)
+	for i := 0; i < 6; i++ {
+		newTree.Set("key"+gconv.String(i), "val"+gconv.String(i))
+	}
 
+	fmt.Println(newTree.Map())
+	fmt.Println(newTree.Size())
+
+	// output:
+	// map[key0:val0 key1:val1 key2:val2 key3:val3 key4:val4 key5:val5]
+	// 6
 }
 
 func ExampleBTree_Sets() {
+	newTree := gtree.NewBTree(3, gutil.ComparatorString)
 
+	newTree.Sets(map[interface{}]interface{}{
+		"key1": "val1",
+		"key2": "val2",
+	})
+
+	fmt.Println(newTree.Map())
+	fmt.Println(newTree.Size())
+
+	// output:
+	// map[key1:val1 key2:val2]
+	// 2
 }
 
 func ExampleBTree_Get() {
+	newTree := gtree.NewBTree(3, gutil.ComparatorString)
+	for i := 0; i < 6; i++ {
+		newTree.Set("key"+gconv.String(i), "val"+gconv.String(i))
+	}
 
+	fmt.Println(newTree.Get("key1"))
+	fmt.Println(newTree.Get("key10"))
+
+	// output:
+	// val1
+	// <nil>
 }
 
 func ExampleBTree_GetOrSet() {
+	newTree := gtree.NewBTree(3, gutil.ComparatorString)
+	for i := 0; i < 6; i++ {
+		newTree.Set("key"+gconv.String(i), "val"+gconv.String(i))
+	}
 
+	fmt.Println(newTree.GetOrSet("key1", "newVal1"))
+	fmt.Println(newTree.GetOrSet("key6", "val6"))
+
+	// output:
+	// val1
+	// val6
 }
 
 func ExampleBTree_GetOrSetFunc() {
+	newTree := gtree.NewBTree(3, gutil.ComparatorString)
+	for i := 0; i < 6; i++ {
+		newTree.Set("key"+gconv.String(i), "val"+gconv.String(i))
+	}
 
+	fmt.Println(newTree.GetOrSetFunc("key1", func() interface{} {
+		return "newVal1"
+	}))
+	fmt.Println(newTree.GetOrSetFunc("key6", func() interface{} {
+		return "val6"
+	}))
+
+	// output:
+	// val1
+	// val6
 }
 
 func ExampleBTree_GetOrSetFuncLock() {
+	newTree := gtree.NewBTree(3, gutil.ComparatorString)
+	for i := 0; i < 6; i++ {
+		newTree.Set("key"+gconv.String(i), "val"+gconv.String(i))
+	}
 
+	fmt.Println(newTree.GetOrSetFuncLock("key1", func() interface{} {
+		return "newVal1"
+	}))
+	fmt.Println(newTree.GetOrSetFuncLock("key6", func() interface{} {
+		return "val6"
+	}))
+
+	// output:
+	// val1
+	// val6
 }
 
 func ExampleBTree_GetVar() {
