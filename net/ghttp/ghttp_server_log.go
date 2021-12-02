b840405af65291278752e06b5e8375974db0206f
John Guo <john@johng.cn>
2021-12-02 21:22:02 +0800

    improve error logging format for ghttp.Server

--- net/ghttp/ghttp_server_log.go
+++ net/ghttp/ghttp_server_log.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 
 	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/text/gstr"
 )
 
 // handleAccessLog handles the access logging for server.
@@ -26,8 +27,7 @@ func (s *Server) handleAccessLog(r *Request) {
 		Printf(
 			r.Context(),
 			`%d "%s %s %s %s %s" %.3f, %s, "%s", "%s"`,
-			r.Response.Status,
-			r.Method, scheme, r.Host, r.URL.String(), r.Proto,
+			r.Response.Status, r.Method, scheme, r.Host, r.URL.String(), r.Proto,
 			float64(r.LeaveTime-r.EnterTime)/1000,
 			r.GetClientIp(), r.Referer(), r.UserAgent(),
 		)
@@ -39,16 +39,24 @@ func (s *Server) handleErrorLog(err error, r *Request) {
 	if !s.IsErrorLogEnabled() {
 		return
 	}
-
-	scheme := "http"
+	var (
+		code          = gerror.Code(err)
+		scheme        = "http"
+		codeDetail    = code.Detail()
+		codeDetailStr string
+	)
 	if r.TLS != nil {
 		scheme = "https"
 	}
+	if codeDetail != nil {
+		codeDetailStr = gstr.Replace(fmt.Sprintf(`%+v`, codeDetail), "\n", " ")
+	}
 	content := fmt.Sprintf(
-		`%d "%s %s %s %s %s" %.3f, %s, "%s", "%s"`,
+		`%d "%s %s %s %s %s" %.3f, %s, "%s", "%s", %d, "%s", "%+v"`,
 		r.Response.Status, r.Method, scheme, r.Host, r.URL.String(), r.Proto,
 		float64(r.LeaveTime-r.EnterTime)/1000,
 		r.GetClientIp(), r.Referer(), r.UserAgent(),
+		code.Code(), code.Message(), codeDetailStr,
 	)
 	if s.config.ErrorStack {
 		if stack := gerror.Stack(err); stack != "" {
