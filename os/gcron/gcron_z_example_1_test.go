a4399ce9ad2b42e2da7ca5028ca4ead6c2cbf9a0
546104865 <564104865@qq.com>
2022-02-09 11:31:31 +0800

    更新 gcron_z_example_1_test.go

--- os/gcron/gcron_z_example_1_test.go
+++ os/gcron/gcron_z_example_1_test.go
@@ -8,6 +8,10 @@ package gcron_test
 
 import (
 	"context"
+	"fmt"
+	"github.com/gogf/gf/v2/container/garray"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
 	"time"
 
 	"github.com/gogf/gf/v2/os/gcron"
@@ -20,4 +24,162 @@ func Example_cronAddSingleton() {
 		time.Sleep(2 * time.Second)
 	})
 	select {}
+}
+
+func Example_cronAddOnce() {
+	var (
+		ctx = gctx.New()
+	)
+	cron := gcron.New()
+	array := garray.New(true)
+	cron.AddOnce(ctx, "@every 2s", func(ctx context.Context) {
+		array.Append(1)
+	})
+	fmt.Println(cron.Size(), array.Len())
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println(cron.Size(), array.Len())
+
+	// Output:
+	// 1 0
+	// 0 1
+}
+
+func Example_cronAddTimes() {
+	var (
+		ctx = gctx.New()
+	)
+	cron := gcron.New()
+	array := garray.New(true)
+	cron.AddTimes(ctx, "@every 2s", 2, func(ctx context.Context) {
+		array.Append(1)
+	})
+	fmt.Println(cron.Size(), array.Len())
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println(cron.Size(), array.Len())
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println(cron.Size(), array.Len())
+
+	// Output:
+	// 1 0
+	// 1 1
+	// 0 2
+}
+
+func Example_cronEntries() {
+	var (
+		ctx = gctx.New()
+	)
+	cron := gcron.New()
+	array := garray.New(true)
+	cron.AddTimes(ctx, "@every 1s", 2, func(ctx context.Context) {
+		array.Append(1)
+	}, "cron1")
+	cron.AddOnce(ctx, "@every 1s", func(ctx context.Context) {
+		array.Append(1)
+	}, "cron2")
+	entries := cron.Entries()
+	for k, v := range entries {
+		fmt.Println(k, v.Name, v.Time)
+
+	}
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println(array.Len())
+
+	// May Output:
+	// 0 cron2 2022-02-09 10:11:47.2421345 +0800 CST m=+0.159116501
+	// 1 cron1 2022-02-09 10:11:47.2421345 +0800 CST m=+0.159116501
+	// 3
+}
+
+func Example_cronSearch() {
+	var (
+		ctx = gctx.New()
+	)
+	cron := gcron.New()
+	array := garray.New(true)
+	cron.AddTimes(ctx, "@every 1s", 2, func(ctx context.Context) {
+		array.Append(1)
+	}, "cron1")
+	cron.AddOnce(ctx, "@every 1s", func(ctx context.Context) {
+		array.Append(1)
+	}, "cron2")
+	search := cron.Search("cron2")
+
+	g.Log().Print(ctx, search)
+
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println(array.Len())
+
+	// Output:
+	// 3
+}
+
+func Example_cronStop() {
+	var (
+		ctx = gctx.New()
+	)
+	cron := gcron.New()
+	array := garray.New(true)
+	cron.AddTimes(ctx, "@every 2s", 1, func(ctx context.Context) {
+		array.Append(1)
+	}, "cron1")
+	cron.AddOnce(ctx, "@every 2s", func(ctx context.Context) {
+
+		array.Append(1)
+	}, "cron2")
+	fmt.Println(array.Len(), cron.Size())
+	cron.Stop("cron2")
+	fmt.Println(array.Len(), cron.Size())
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println(array.Len(), cron.Size())
+	// Output:
+	// 0 2
+	// 0 2
+	// 1 1
+}
+
+func Example_cronRemove() {
+	var (
+		ctx = gctx.New()
+	)
+	cron := gcron.New()
+	array := garray.New(true)
+	cron.AddTimes(ctx, "@every 2s", 1, func(ctx context.Context) {
+		array.Append(1)
+	}, "cron1")
+	cron.AddOnce(ctx, "@every 2s", func(ctx context.Context) {
+
+		array.Append(1)
+	}, "cron2")
+	fmt.Println(array.Len(), cron.Size())
+	cron.Remove("cron2")
+	fmt.Println(array.Len(), cron.Size())
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println(array.Len(), cron.Size())
+	// Output:
+	// 0 2
+	// 0 1
+	// 1 0
+}
+
+func Example_cronStart() {
+	var (
+		ctx = gctx.New()
+	)
+	cron := gcron.New()
+	array := garray.New(true)
+	cron.AddOnce(ctx, "@every 2s", func(ctx context.Context) {
+
+		array.Append(1)
+	}, "cron2")
+	cron.Stop("cron2")
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println(array.Len(), cron.Size())
+	cron.Start("cron2")
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println(array.Len(), cron.Size())
+
+	// Output:
+	// 0 1
+	// 1 0
 }
