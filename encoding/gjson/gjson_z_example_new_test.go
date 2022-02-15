4fc27f6509c91292134e11a5c79a895b59489d6a
huangqian <huangqian1985@qq.com>
2022-02-15 22:08:51 +0800

    Implemented gjson Example 1. ExampleLoad 2.ExampleLoadJson 3.ExampleLoadXml 4.ExampleLoadIni 5.ExampleLoadYaml 6.ExampleLoadToml 7.ExampleLoadContent 8.ExampleLoadContentType 9.ExampleIsValidDataType

--- encoding/gjson/gjson_z_example_new_test.go
+++ encoding/gjson/gjson_z_example_new_test.go
@@ -12,67 +12,6 @@ import (
 	"github.com/gogf/gf/v2/encoding/gjson"
 )
 
-func Example_newFromJson() {
-	jsonContent := `{"name":"john", "score":"100"}`
-	j := gjson.New(jsonContent)
-	fmt.Println(j.Get("name"))
-	fmt.Println(j.Get("score"))
-	// Output:
-	// john
-	// 100
-}
-
-func Example_newFromXml() {
-	jsonContent := `<?xml version="1.0" encoding="UTF-8"?><doc><name>john</name><score>100</score></doc>`
-	j := gjson.New(jsonContent)
-	// Note that there's root node in the XML content.
-	fmt.Println(j.Get("doc.name"))
-	fmt.Println(j.Get("doc.score"))
-	// Output:
-	// john
-	// 100
-}
-
-func Example_newFromStruct() {
-	type Me struct {
-		Name  string `json:"name"`
-		Score int    `json:"score"`
-	}
-	me := Me{
-		Name:  "john",
-		Score: 100,
-	}
-	j := gjson.New(me)
-	fmt.Println(j.Get("name"))
-	fmt.Println(j.Get("score"))
-	// Output:
-	// john
-	// 100
-}
-
-func Example_newFromStructWithTag() {
-	type Me struct {
-		Name  string `tag:"name"`
-		Score int    `tag:"score"`
-		Title string
-	}
-	me := Me{
-		Name:  "john",
-		Score: 100,
-		Title: "engineer",
-	}
-	// The parameter `tags` specifies custom priority tags for struct conversion to map,
-	// multiple tags joined with char ','.
-	j := gjson.NewWithTag(me, "tag")
-	fmt.Println(j.Get("name"))
-	fmt.Println(j.Get("score"))
-	fmt.Println(j.Get("Title"))
-	// Output:
-	// john
-	// 100
-	// engineer
-}
-
 func ExampleNew() {
 	jsonContent := `{"name":"john", "score":"100"}`
 	j := gjson.New(jsonContent)
@@ -129,4 +68,32 @@ func ExampleNewWithOptions() {
 	// john
 	// 100
 	// engineer
+}
+
+func Example_newFromXml() {
+	jsonContent := `<?xml version="1.0" encoding="UTF-8"?><doc><name>john</name><score>100</score></doc>`
+	j := gjson.New(jsonContent)
+	// Note that there's root node in the XML content.
+	fmt.Println(j.Get("doc.name"))
+	fmt.Println(j.Get("doc.score"))
+	// Output:
+	// john
+	// 100
+}
+
+func Example_newFromStruct() {
+	type Me struct {
+		Name  string `json:"name"`
+		Score int    `json:"score"`
+	}
+	me := Me{
+		Name:  "john",
+		Score: 100,
+	}
+	j := gjson.New(me)
+	fmt.Println(j.Get("name"))
+	fmt.Println(j.Get("score"))
+	// Output:
+	// john
+	// 100
 }
