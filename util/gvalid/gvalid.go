5ee387672bce7716ad84560af69cef45dcd424c1
John Guo <john@johng.cn>
2022-03-21 22:36:06 +0800

    enhancement from issue #1689

--- util/gvalid/gvalid.go
+++ util/gvalid/gvalid.go
@@ -8,9 +8,11 @@
 package gvalid
 
 import (
+	"context"
 	"regexp"
 	"strings"
 
+	"github.com/gogf/gf/v2/internal/intlog"
 	"github.com/gogf/gf/v2/text/gregex"
 )
 
@@ -204,7 +206,14 @@ func ParseTagValue(tag string) (field, rule, msg string) {
 	// Complete sequence tag.
 	// Example: name@required|length:2,20|password3|same:password1#||密码强度不足|两次密码不一致
 	match, _ := gregex.MatchString(`\s*((\w+)\s*@){0,1}\s*([^#]+)\s*(#\s*(.*)){0,1}\s*`, tag)
-	return strings.TrimSpace(match[2]), strings.TrimSpace(match[3]), strings.TrimSpace(match[5])
+	if len(match) > 5 {
+		msg = strings.TrimSpace(match[5])
+		rule = strings.TrimSpace(match[3])
+		field = strings.TrimSpace(match[2])
+	} else {
+		intlog.Errorf(context.TODO(), `invalid validation tag value: %s`, tag)
+	}
+	return
 }
 
 // GetTags returns the validation tags.
