215a50675e4087357e28c0a7c26068537b5bdd72
John Guo <john@johng.cn>
2022-04-22 18:17:10 +0800

    add command gen service for cli

--- cmd/gf/internal/utility/utils/utils.go
+++ cmd/gf/internal/utility/utils/utils.go
@@ -2,17 +2,25 @@ package utils
 
 import (
 	"fmt"
+
 	"github.com/gogf/gf/v2/os/gproc"
 )
 
 var (
-	// gofmtPath is the binary path of command `gofmt`.
-	gofmtPath = gproc.SearchBinaryPath("gofmt")
+	gofmtPath     = gproc.SearchBinaryPath("gofmt")     // gofmtPath is the binary path of command `gofmt`.
+	goimportsPath = gproc.SearchBinaryPath("goimports") // gofmtPath is the binary path of command `goimports`.
 )
 
 // GoFmt formats the source file using command `gofmt -w -s PATH`.
 func GoFmt(path string) {
 	if gofmtPath != "" {
 		gproc.ShellExec(fmt.Sprintf(`%s -w -s %s`, gofmtPath, path))
+	}
+}
+
+// GoImports formats the source file using command `goimports -w PATH`.
+func GoImports(path string) {
+	if goimportsPath != "" {
+		gproc.ShellExec(fmt.Sprintf(`%s -w %s`, goimportsPath, path))
 	}
 }
