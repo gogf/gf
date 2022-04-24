c256d2d4af9628bce4713332260c418104d13ee2
John Guo <john@johng.cn>
2022-04-24 22:23:56 +0800

    fix issue in gstr.RepliaceI; add command gen service for cli

--- cmd/gf/internal/utility/utils/utils.go
+++ cmd/gf/internal/utility/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 
+	"github.com/gogf/gf/cmd/gf/v2/internal/utility/mlog"
 	"github.com/gogf/gf/v2/os/gproc"
 )
 
@@ -13,14 +14,17 @@ var (
 
 // GoFmt formats the source file using command `gofmt -w -s PATH`.
 func GoFmt(path string) {
-	if gofmtPath != "" {
-		gproc.ShellExec(fmt.Sprintf(`%s -w -s %s`, gofmtPath, path))
+	if gofmtPath == "" {
+		mlog.Debug(`command "gofmt" not found`)
+		return
 	}
+	gproc.ShellExec(fmt.Sprintf(`%s -w -s %s`, gofmtPath, path))
 }
 
 // GoImports formats the source file using command `goimports -w PATH`.
 func GoImports(path string) {
-	if goimportsPath != "" {
-		gproc.ShellExec(fmt.Sprintf(`%s -w %s`, goimportsPath, path))
+	if goimportsPath == "" {
+		mlog.Debug(`command "goimports" not found`)
 	}
+	gproc.ShellExec(fmt.Sprintf(`%s -w %s`, goimportsPath, path))
 }
