8e41142822448dda0b4de57163742bb8c2e38aeb
houseme <housemecn@gmail.com>
2022-05-11 20:49:11 +0800

    test

--- contrib/registry/polaris/registry_test.go
+++ contrib/registry/polaris/registry_test.go
@@ -170,7 +170,7 @@ func TestWatch(t *testing.T) {
 		Endpoints: []string{"tcp://127.0.0.1:9000?isSecure=false"},
 	}
 
-	watch, err := r.Watch(context.Background(), "goframe-provider-4-tcp")
+	watch, err := r.Watch(context.Background(), svc.Key())
 	if err != nil {
 		t.Fatal(err)
 	}
