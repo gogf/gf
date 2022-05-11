da7eac03ad41d2c1aba95605722dcfdefbee88d5
unknown <12gzk@163.com>
2022-05-12 00:25:30 +0800

    delete package comment

--- encoding/gproperties/gproperties_z_unit_test.go
+++ encoding/gproperties/gproperties_z_unit_test.go
@@ -3,8 +3,6 @@
 // This Source Code Form is subject to the terms of the MIT License.
 // If a copy of the MIT was not distributed with this file,
 // You can obtain one at https://github.com/gogf/gf.
-
-// Package gproperties provides accessing and converting for properties content.
 package gproperties_test
 
 import (
