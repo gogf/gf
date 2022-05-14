85a1a795e834859acebd974ac09fd340dcb77dc1
chaggle <chaggle@foxmail.com>
2022-05-14 16:51:09 +0800

    fix: the file begin / to //

--- contrib/drivers/pgsql/pgsql_basic_test.go
+++ contrib/drivers/pgsql/pgsql_basic_test.go
@@ -1,4 +1,4 @@
-/ Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
+// Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
 //
 // This Source Code Form is subject to the terms of the MIT License.
 // If a copy of the MIT was not distributed with this file,
