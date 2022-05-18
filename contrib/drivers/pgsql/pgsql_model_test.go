746337625d38894266c7dba8ab1ebbf64595ea36
chaggle <chaggle@foxmail.com>
2022-05-18 12:47:10 +0800

    test: update pgsql.go and add pgsql_model_test

---
+++ contrib/drivers/pgsql/pgsql_model_test.go
@@ -0,0 +1,127 @@
+// Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
+package pgsql_test
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gtime"
+	"github.com/gogf/gf/v2/test/gtest"
+	"testing"
+)
+
+func Test_Model_Insert(t *testing.T) {
+	table := createTable()
+	defer dropTable(table)
+
+	gtest.C(t, func(t *gtest.T) {
+		user := db.Model(table)
+		result, err := user.Data(g.Map{
+			"id":          1,
+			"uid":         1,
+			"passport":    "t1",
+			"password":    "25d55ad283aa400af464c76d713c07ad",
+			"nickname":    "name_1",
+			"create_time": gtime.Now().String(),
+		}).Insert()
+		t.AssertNil(err)
+		n, _ := result.RowsAffected()
+		t.Assert(n, 1)
+
+		result, err = db.Model(table).Data(g.Map{
+			"id":          "2",
+			"uid":         "2",
+			"passport":    "t2",
+			"password":    "25d55ad283aa400af464c76d713c07ad",
+			"nickname":    "name_2",
+			"create_time": gtime.Now().String(),
+		}).Insert()
+		t.AssertNil(err)
+		n, _ = result.RowsAffected()
+		t.Assert(n, 1)
+
+		type User struct {
+			Id         int         `gconv:"id"`
+			Uid        int         `gconv:"uid"`
+			Passport   string      `json:"passport"`
+			Password   string      `gconv:"password"`
+			Nickname   string      `gconv:"nickname"`
+			CreateTime *gtime.Time `json:"create_time"`
+		}
+		// Model inserting.
+		result, err = db.Model(table).Data(User{
+			Id:       3,
+			Uid:      3,
+			Passport: "t3",
+			Password: "25d55ad283aa400af464c76d713c07ad",
+			Nickname: "name_3",
+		}).Insert()
+		t.AssertNil(err)
+		n, _ = result.RowsAffected()
+		t.Assert(n, 1)
+		value, err := db.Model(table).Fields("passport").Where("id=3").Value()
+		t.AssertNil(err)
+		t.Assert(value.String(), "t3")
+
+		result, err = db.Model(table).Data(&User{
+			Id:         4,
+			Uid:        4,
+			Passport:   "t4",
+			Password:   "25d55ad283aa400af464c76d713c07ad",
+			Nickname:   "T4",
+			CreateTime: gtime.Now(),
+		}).Insert()
+		t.AssertNil(err)
+		n, _ = result.RowsAffected()
+		t.Assert(n, 1)
+		value, err = db.Model(table).Fields("passport").Where("id=4").Value()
+		t.AssertNil(err)
+		t.Assert(value.String(), "t4")
+
+		result, err = db.Model(table).Where("id>?", 1).Delete()
+		t.AssertNil(err)
+		n, _ = result.RowsAffected()
+		t.Assert(n, 3)
+	})
+}
+
+func Test_Model_Update(t *testing.T) {
+	table := createInitTable()
+	defer dropTable(table)
+
+	gtest.C(t, func(t *gtest.T) {
+		result, err := db.Model(table).Data("passport", "user_22").Where("passport=?", "user_2").Update()
+		t.AssertNil(err)
+		n, _ := result.RowsAffected()
+		t.Assert(n, 1)
+	})
+
+	gtest.C(t, func(t *gtest.T) {
+		result, err := db.Model(table).Data("passport", "user_2").Where("passport='user_22'").Update()
+		t.AssertNil(err)
+		n, _ := result.RowsAffected()
+		t.Assert(n, 1)
+	})
+
+	// Update + Data(string)
+	gtest.C(t, func(t *gtest.T) {
+		result, err := db.Model(table).Data("passport='user_33'").Where("passport='user_3'").Update()
+		t.AssertNil(err)
+		n, _ := result.RowsAffected()
+		t.Assert(n, 1)
+	})
+
+	// Update + Fields(string)
+	gtest.C(t, func(t *gtest.T) {
+		result, err := db.Model(table).Fields("passport").Data(g.Map{
+			"passport": "user_44",
+			"none":     "none",
+		}).Where("passport='user_4'").Update()
+		t.AssertNil(err)
+		n, _ := result.RowsAffected()
+		t.Assert(n, 1)
+	})
+}
