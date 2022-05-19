a0dbe61a1272718f0a2a709f0e4da7ba2ada64e8
houseme <housemecn@gmail.com>
2022-05-19 17:46:02 +0800

    feat:modify polaris log dir

--- example/registry/polaris/client/main.go
+++ example/registry/polaris/client/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	"github.com/polarismesh/polaris-go/api"
 	"github.com/polarismesh/polaris-go/pkg/config"
 
 	"github.com/gogf/gf/contrib/registry/polaris/v2"
-
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/gsvc"
 	"github.com/gogf/gf/v2/os/gctx"
@@ -15,6 +16,11 @@ import (
 
 func main() {
 	conf := config.NewDefaultConfiguration([]string{"192.168.100.222:8091"})
+	conf.Consumer.LocalCache.SetPersistDir("/tmp/polaris/backup")
+	err := api.SetLoggersDir("/tmp/polaris/log")
+	if err != nil {
+		g.Log().Fatal(context.Background(), err)
+	}
 
 	gsvc.SetRegistry(polaris.NewWithConfig(conf, polaris.WithTTL(100)))
 
