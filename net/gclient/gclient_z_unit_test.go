1cfa72cd6a13e5d6de63d8b8f3bb20cdedc96433
huangqian <huangqian1985@qq.com>
2022-06-08 21:24:01 +0800

    add CustomProvider function

--- net/gclient/gclient_z_unit_test.go
+++ net/gclient/gclient_z_unit_test.go
@@ -12,6 +12,10 @@ import (
 	"crypto/tls"
 	"fmt"
 	"github.com/gogf/gf/v2/debug/gdebug"
+	"github.com/gogf/gf/v2/internal/tracing"
+	"go.opentelemetry.io/otel"
+	sdkTrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 	"io/ioutil"
 	"net/http"
 	"testing"
@@ -638,4 +642,58 @@ func TestClient_RequestVar(t *testing.T) {
 		t.AssertNil(err)
 		t.AssertNE(users, nil)
 	})
+}
+
+type CustomProvider struct {
+	*sdkTrace.TracerProvider
+}
+
+func NewCustomProvider() *CustomProvider {
+	return &CustomProvider{
+		TracerProvider: sdkTrace.NewTracerProvider(
+			sdkTrace.WithIDGenerator(NewCustomIDGenerator()),
+		),
+	}
+}
+
+type CustomIDGenerator struct{}
+
+func NewCustomIDGenerator() *CustomIDGenerator {
+	return &CustomIDGenerator{}
+}
+
+func (id *CustomIDGenerator) NewIDs(ctx context.Context) (traceID trace.TraceID, spanID trace.SpanID) {
+	return tracing.NewIDs()
+}
+
+func (id *CustomIDGenerator) NewSpanID(ctx context.Context, traceID trace.TraceID) (spanID trace.SpanID) {
+	return tracing.NewSpanID()
+}
+
+func TestClient_CustomProvider(t *testing.T) {
+	//
+	otel.SetTracerProvider(NewCustomProvider())
+
+	p, _ := gtcp.GetFreePort()
+	s := g.Server(p)
+	s.BindHandler("/hello", func(r *ghttp.Request) {
+		r.Response.WriteHeader(200)
+		r.Response.WriteJson(g.Map{"field": "test_for_response_body"})
+	})
+	s.SetPort(p)
+	s.SetDumpRouterMap(false)
+	s.Start()
+	defer s.Shutdown()
+
+	time.Sleep(100 * time.Millisecond)
+	gtest.C(t, func(t *gtest.T) {
+		c := g.Client()
+		url := fmt.Sprintf("127.0.0.1:%d/hello", p)
+		resp, err := c.DoRequest(ctx, http.MethodGet, url)
+		t.AssertNil(err)
+		t.AssertNE(resp, nil)
+		t.Assert(resp.ReadAllString(), "{\"field\":\"test_for_response_body\"}")
+	})
+
+	//otel.SetTracerProvider(provider)
 }
