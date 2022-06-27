a474a6de8fd6c1da7192ae81ca775ff289ce62dd
曾洪亮 <hongliang.zeng@i-soft.com.cn>
2022-06-27 15:50:15 +0800

    redis add sentinel slaveOnly filed

--- database/gredis/gredis_z_unit_conn_sentinel_test.go
+++ database/gredis/gredis_z_unit_conn_sentinel_test.go
@@ -1,12 +1,8 @@
-/**
- * package gredis
- *
- * @Author 曾洪亮<zenghongl@126.com>
- * @Email  zenghongl@126.com
- * User: whoSafe
- * Date: 2022/6/27
- * Time: 15:19
- */
+// Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
 
 package gredis_test
 
