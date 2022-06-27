210781f612151b7326b2396cb6b35ea7247324be
曾洪亮 <hongliang.zeng@i-soft.com.cn>
2022-06-27 15:03:38 +0800

    redis add sentinel test

--- database/gredis/gredis_z_unit_conn_sentinel_test.go
+++ database/gredis/gredis_z_unit_conn_sentinel_test.go
@@ -21,8 +21,9 @@ import (
 var (
 	sentinelCtx    = context.TODO()
 	sentinelConfig = &gredis.Config{
-		Address:    `192.168.41.162:26379,192.168.41.174:26379,192.168.41.192:26379`,
-		MasterName: `master`,
+		Address:    `127.0.0.1:26379,127.0.0.1:26380,127.0.0.1:26381`,
+		MasterName: `mymaster`,
+		Pass:       "111111",
 	}
 )
 
