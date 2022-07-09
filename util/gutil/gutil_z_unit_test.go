1989f11fbe045e8f56f12a38e7ba2a730f5812a9
huangqian <huangqian1985@qq.com>
2022-07-09 14:49:19 +0800

    gutil code converage

--- util/gutil/gutil_z_unit_test.go
+++ util/gutil/gutil_z_unit_test.go
@@ -9,6 +9,7 @@ package gutil_test
 import (
 	"context"
 	"github.com/gogf/gf/v2/errors/gerror"
+	"reflect"
 	"testing"
 
 	"github.com/gogf/gf/v2/frame/g"
@@ -78,6 +79,12 @@ func Test_Throw(t *testing.T) {
 }
 
 func Test_Keys(t *testing.T) {
+	// not support int
+	gtest.C(t, func(t *gtest.T) {
+		var val int = 1
+		keys := gutil.Keys(reflect.ValueOf(val))
+		t.AssertEQ(len(keys), 0)
+	})
 	// map
 	gtest.C(t, func(t *gtest.T) {
 		keys := gutil.Keys(map[int]int{
@@ -86,6 +93,13 @@ func Test_Keys(t *testing.T) {
 		})
 		t.AssertIN("1", keys)
 		t.AssertIN("2", keys)
+
+		strKeys := gutil.Keys(map[string]interface{}{
+			"key1": 1,
+			"key2": 2,
+		})
+		t.AssertIN("key1", strKeys)
+		t.AssertIN("key2", strKeys)
 	})
 	// *map
 	gtest.C(t, func(t *gtest.T) {
@@ -128,15 +142,38 @@ func Test_Keys(t *testing.T) {
 }
 
 func Test_Values(t *testing.T) {
+	// not support int
 	gtest.C(t, func(t *gtest.T) {
-		values := gutil.Keys(map[int]int{
+		var val int = 1
+		keys := gutil.Values(reflect.ValueOf(val))
+		t.AssertEQ(len(keys), 0)
+	})
+	// map
+	gtest.C(t, func(t *gtest.T) {
+		values := gutil.Values(map[int]int{
 			1: 10,
 			2: 20,
 		})
-		t.AssertIN("1", values)
-		t.AssertIN("2", values)
-	})
+		t.AssertIN(10, values)
+		t.AssertIN(20, values)
 
+		values = gutil.Values(map[string]interface{}{
+			"key1": 10,
+			"key2": 20,
+		})
+		t.AssertIN(10, values)
+		t.AssertIN(20, values)
+	})
+	// *map
+	gtest.C(t, func(t *gtest.T) {
+		keys := gutil.Values(&map[int]int{
+			1: 10,
+			2: 20,
+		})
+		t.AssertIN(10, keys)
+		t.AssertIN(20, keys)
+	})
+	// struct
 	gtest.C(t, func(t *gtest.T) {
 		type T struct {
 			A string
