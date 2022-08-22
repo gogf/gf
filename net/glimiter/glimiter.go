c3b7d3b861640b3bed42420b1ac335ec4a173f83
Zhangyi <flywithyou_ren@126.com>
2022-08-22 16:34:03 +0800

    format code && moidify comment

--- net/glimiter/glimiter.go
+++ net/glimiter/glimiter.go
@@ -11,37 +11,38 @@ import "context"
 
 type Adaper interface {
 
-	// All interfaces add context , you can add special parameters through the content value, and make judgments through RefFilterInfo in Adapter, such as ip, user_id, etc
-	// is acquire  
-	Acquire(ctx context.Context, reqCount ... int64) bool
+	// All interfaces add context , you can add special parameters through the content value,
+	// and make judgments through RefFilterInfo in Adapter, such as ip, user_id, etc
 
-	// reset limiter status
-	ResetStatus(ctx context.Context) 
+	// Acquires permits from this RateLimiter, blocking until the request can be granted.
+	Acquire(ctx context.Context, reqCount ...int64) bool
 
-	// try acquire . Reference:   guava RateLimiter  
-	TryAcuqire(ctx context.Context, reqCount ...  int64) bool 
+	// Reset limiter status
+	ResetStatus(ctx context.Context)
 
+	//Acquires permits from this RateLimiter if it can be acquired immediately without delay.
+	TryAcuqire(ctx context.Context, reqCount ...int64) bool
 }
 
 type localAdapter = Adaper
 
 type Limiter struct {
-	localAdapter 
+	localAdapter
 }
 
-// Crate a new limiter with given adapter 
+// Crate a new limiter with given adapter
 func NewWithAdapter(adapter Adaper) *Limiter {
 	return &Limiter{adapter}
 }
 
-
-// Crate a new limiter with given rate and burst . default adapter is TokenBucketAdapter(use golang.org/x/time/rate)
-func New(rate float64, burstCount int64) *Limiter{
+// Crate a new limiter with given rate and burst .
+// default adapter is TokenBucketAdapter(use golang.org/x/time/rate)
+func New(rate float64, burstCount int64) *Limiter {
 	tba := &TokenBucketAdapter{rate, burstCount}
 	return &Limiter{tba}
 }
 
-func (l *Limiter ) IsAcquire(ctx context.Context, reqCount ... int64) bool {
+func (l *Limiter) IsAcquire(ctx context.Context, reqCount ...int64) bool {
 	return l.Acquire(ctx, reqCount...)
 }
 
@@ -49,6 +50,6 @@ func (l *Limiter) Reset(ctx context.Context) {
 	l.ResetStatus(ctx)
 }
 
-func (l *Limiter) TryReqAcquire(ctx context.Context, reqCount ... int64) bool {
-	return l.TryAcuqire(ctx, reqCount...)	
+func (l *Limiter) TryReqAcquire(ctx context.Context, reqCount ...int64) bool {
+	return l.TryAcuqire(ctx, reqCount...)
 }
--- net/glimiter/tokenlimter.go
+++ net/glimiter/tokenlimter.go
@@ -2,26 +2,26 @@ package glimiter
 
 import "context"
 
-// TokenBucket Adapter provider  , Reference:   golang.org/x/time/rate
+// TokenBucket Adapter provider , Reference: golang.org/x/time/rate
 type TokenBucketAdapter struct {
-	Limit float64 // allows events up to rate
-	Bursts  int64 //permits bursts of at most b tokens. 
+	Limit  float64 // allows events up to rate
+	Bursts int64   //permits bursts of at most b tokens.
 }
 
-
-func (t *TokenBucketAdapter) Acquire(ctx context.Context, reqCount ... int64) bool {
-
+// Acquir reports whether n events may happen at time now.
+// Use this method if you intend to drop / skip events that exceed the rate limit.
+// Otherwise use Reserve or Wait.
+func (t *TokenBucketAdapter) Acquire(ctx context.Context, reqCount ...int64) bool {
 	return false
 }
 
-
+// Reset Status of limiter  
 func (t *TokenBucketAdapter) ResetStatus(ctx context.Context) {
-	
+
 }
 
-func (t *TokenBucketAdapter) TryAcuqire(ctx context.Context, reqCount ...  int64) bool {
+//TryAcuqire: Acquires  permits from this RateLimiter if it can be acquired immediately without delay.
+func (t *TokenBucketAdapter) TryAcuqire(ctx context.Context, reqCount ...int64) bool {
 
 	return false
 }
-
-
