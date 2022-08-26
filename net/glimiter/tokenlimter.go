350e32e72675c2dd657e95548dc3e3a3f875b84d
houseme <housemecn@gmail.com>
2022-08-26 17:19:38 +0800

    Update tokenlimter.go

--- net/glimiter/tokenlimter.go
+++ net/glimiter/tokenlimter.go
@@ -1,11 +1,17 @@
+// Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
 package glimiter
 
 import "context"
 
-// TokenBucket Adapter provider , Reference: golang.org/x/time/rate
+// TokenBucketAdapter token bucket adapter provider , Reference: golang.org/x/time/rate
 type TokenBucketAdapter struct {
 	Limit  float64 // allows events up to rate
-	Bursts int64   //permits bursts of at most b tokens.
+	Bursts int64   // permits bursts of at most b tokens.
 }
 
 // Acquire reports whether n events may happen at time now.
@@ -15,12 +21,12 @@ func (t *TokenBucketAdapter) Acquire(ctx context.Context, reqCount ...int64) boo
 	return false
 }
 
-// Reset Status of limiter
+// ResetStatus reset status of limiter
 func (t *TokenBucketAdapter) ResetStatus(ctx context.Context) {
 
 }
 
-// TryAcuqire: Acquires  permits from this RateLimiter if it can be acquired immediately without delay.
+// TryAcuqire  Acquires  permits from this RateLimiter if it can be acquired immediately without delay.
 func (t *TokenBucketAdapter) TryAcuqire(ctx context.Context, reqCount ...int64) bool {
 
 	return false
