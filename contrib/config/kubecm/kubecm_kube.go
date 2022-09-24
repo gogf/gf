af33b93ee7b9fbd508f7309434d6fc23ac7ed28e
John Guo <john@johng.cn>
2022-09-25 00:12:20 +0800

    add package contrib/config/kubecm

---
+++ contrib/config/kubecm/kubecm_kube.go
@@ -0,0 +1,63 @@
+// Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
+package kubecm
+
+import (
+	"context"
+
+	"github.com/gogf/gf/v2/os/gfile"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	defaultKubernetesUserAgent  = `kubecm.Client`
+	kubernetesNamespaceFilePath = `/var/run/secrets/kubernetes.io/serviceaccount/namespace`
+)
+
+// Namespace retrieves and returns the namespace of current pod.
+// Note that this function should be called in kubernetes pod.
+func Namespace() string {
+	return gfile.GetContents(kubernetesNamespaceFilePath)
+}
+
+// NewDefaultKubeClient creates and returns a default kubernetes client.
+// It is commonly used when the service is running inside kubernetes cluster.
+func NewDefaultKubeClient(ctx context.Context) (*kubernetes.Clientset, error) {
+	return NewKubeClientFromPath(ctx, "")
+}
+
+// NewKubeClientFromPath creates and returns a kubernetes REST client by given `kubeConfigFilePath`.
+func NewKubeClientFromPath(ctx context.Context, kubeConfigFilePath string) (*kubernetes.Clientset, error) {
+	restConfig, err := NewKubeConfigFromPath(ctx, kubeConfigFilePath)
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(restConfig)
+}
+
+// NewKubeClientFromConfig creates and returns client by given `rest.Config`.
+func NewKubeClientFromConfig(ctx context.Context, config *rest.Config) (*kubernetes.Clientset, error) {
+	return kubernetes.NewForConfig(config)
+}
+
+// NewDefaultKubeConfig creates and returns a default kubernetes config.
+// It is commonly used when the service is running inside kubernetes cluster.
+func NewDefaultKubeConfig(ctx context.Context) (*rest.Config, error) {
+	return NewKubeConfigFromPath(ctx, "")
+}
+
+// NewKubeConfigFromPath creates and returns rest.Config object from given `kubeConfigFilePath`.
+func NewKubeConfigFromPath(ctx context.Context, kubeConfigFilePath string) (*rest.Config, error) {
+	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigFilePath)
+	if err != nil {
+		return nil, err
+	}
+	restConfig.UserAgent = defaultKubernetesUserAgent
+	return restConfig, nil
+}
---
+++ contrib/config/kubecm/kubecm_test.go
@@ -0,0 +1,86 @@
+// Copyright 2019 gf Author(https://github.com/gogf/gf). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
+package kubecm_test
+
+import (
+	"testing"
+
+	"github.com/gogf/gf/contrib/config/kubecm/v2"
+	"github.com/gogf/gf/v2/encoding/gjson"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
+	"github.com/gogf/gf/v2/test/gtest"
+	"github.com/gogf/gf/v2/util/guid"
+	v1 "k8s.io/api/core/v1"
+	kubeMetaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+const (
+	namespace          = "default"
+	configmap          = "test-configmap"
+	dataItem           = "config.yaml"
+	configmapFileName  = "configmap.yaml"
+	kubeConfigFilePath = `/Users/john/.kube/config`
+)
+
+var (
+	ctx = gctx.New()
+)
+
+func TestAvailable(t *testing.T) {
+	var (
+		err        error
+		kubeClient *kubernetes.Clientset
+	)
+	// Configmap apply.
+	gtest.C(t, func(t *gtest.T) {
+		kubeClient, err = kubecm.NewKubeClientFromPath(ctx, kubeConfigFilePath)
+		t.AssertNil(err)
+		var (
+			configMap v1.ConfigMap
+			content   = gtest.DataContent(configmapFileName)
+		)
+		err = gjson.New(content).Scan(&configMap)
+		t.AssertNil(err)
+		_, err = kubeClient.CoreV1().ConfigMaps(namespace).Create(
+			ctx, &configMap, kubeMetaV1.CreateOptions{},
+		)
+		t.AssertNil(err)
+	})
+	defer func() {
+		gtest.C(t, func(t *gtest.T) {
+			err = kubeClient.CoreV1().ConfigMaps(namespace).Delete(
+				ctx, configmap, kubeMetaV1.DeleteOptions{},
+			)
+			t.AssertNil(err)
+		})
+	}()
+
+	gtest.C(t, func(t *gtest.T) {
+		adapter, err := kubecm.New(ctx, kubecm.Config{
+			ConfigMap:  configmap,
+			DataItem:   dataItem,
+			Namespace:  namespace,
+			KubeClient: kubeClient,
+		})
+		t.AssertNil(err)
+
+		config := g.Cfg(guid.S())
+		config.SetAdapter(adapter)
+
+		t.Assert(config.Available(ctx), true)
+
+		m, err := config.Data(ctx)
+		t.AssertNil(err)
+		t.AssertGT(len(m), 0)
+
+		v, err := config.Get(ctx, "server.address")
+		t.AssertNil(err)
+		t.Assert(v.String(), ":8888")
+	})
+}
