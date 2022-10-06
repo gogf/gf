391d6e4cf403b9c8943cf7fdc4bee69a5d7bf9c2
houseme <housemecn@gmail.com>
2022-10-06 23:28:03 +0800

    feat: create polaris config

---
+++ contrib/config/polaris/polaris.go
@@ -0,0 +1,176 @@
+// Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
+// Package polaris implements gcfg.Adapter using polaris service.
+package polaris
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/polarismesh/polaris-go"
+	"github.com/polarismesh/polaris-go/api"
+	"github.com/polarismesh/polaris-go/pkg/model"
+
+	"github.com/gogf/gf/v2/encoding/gjson"
+	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gcfg"
+	"github.com/gogf/gf/v2/text/gstr"
+)
+
+// LogDir sets the log directory for polaris.
+func LogDir(dir string) error {
+	return api.SetLoggersDir(dir)
+}
+
+// Config is the configuration for polaris.
+type Config struct {
+	// The namespace of the configuration.
+	Namespace string `v:"required#namespace is required"`
+	// The group of the configuration.
+	FileGroup string `v:"required#file group is required"`
+	// The name of the configuration.
+	FileName string `v:"required#file name is required"`
+	// The path of the polaris configuration file.
+	ConfigPath string `v:"required#config path is required"`
+	// The log directory for polaris.
+	LogDir string
+	// Whether to watch the configuration.
+	Watch bool
+}
+
+// Client implements gcfg.Adapter implementing using polaris service.
+type Client struct {
+	config Config
+	client model.ConfigFile
+	value  *g.Var
+}
+
+const defaultLogDir = "/tmp/polaris/log"
+
+// New creates and returns gcfg.Adapter implementing using polaris service.
+func New(ctx context.Context, config Config) (adapter gcfg.Adapter, err error) {
+	if err = g.Validator().Data(config).Run(ctx); err != nil {
+		err = gerror.Wrap(err, "invalid polaris config")
+		return nil, err
+	}
+	var (
+		client = &Client{
+			config: config,
+			value:  g.NewVar(nil, true),
+		}
+		configAPI polaris.ConfigAPI
+	)
+
+	if configAPI, err = polaris.NewConfigAPIByFile(config.ConfigPath); err != nil {
+		err = gerror.Wrapf(err, "Polaris configuration initialization failed  with config: %+v", config)
+		return
+	}
+	// set log dir
+	if gstr.Trim(config.LogDir) == "" {
+		config.LogDir = defaultLogDir
+	}
+	if err = LogDir(config.LogDir); err != nil {
+		err = gerror.Wrap(err, "set polaris log dir failed")
+		return
+	}
+
+	if client.client, err = configAPI.GetConfigFile(config.Namespace, config.FileGroup, config.FileName); err != nil {
+		err = gerror.Wrapf(err, "failed to read data from Polaris configuration center  with config: %+v", config)
+		return
+	}
+
+	return client, nil
+}
+
+// Available checks and returns the backend configuration service is available.
+// The optional parameter `resource` specifies certain configuration resource.
+//
+// Note that this function does not return error as it just does simply check for
+// backend configuration service.
+func (c *Client) Available(ctx context.Context, resource ...string) (ok bool) {
+	if len(resource) == 0 && !c.value.IsNil() {
+		return true
+	}
+
+	var namespace = c.config.Namespace
+	if len(resource) > 0 {
+		namespace = resource[0]
+	}
+
+	return c.client.GetNamespace() == namespace
+}
+
+// Get retrieves and returns value by specified `pattern` in current resource.
+// Pattern like:
+// "x.y.z" for map item.
+// "x.0.y" for slice item.
+func (c *Client) Get(ctx context.Context, pattern string) (value interface{}, err error) {
+	if c.value.IsNil() {
+		if err = c.updateLocalValueAndWatch(ctx); err != nil {
+			return nil, err
+		}
+	}
+	return c.value.Val().(*gjson.Json).Get(pattern).Val(), nil
+}
+
+// Data retrieves and returns all configuration data in current resource as map.
+// Note that this function may lead lots of memory usage if configuration data is too large,
+// you can implement this function if necessary.
+func (c *Client) Data(ctx context.Context) (data map[string]interface{}, err error) {
+	if c.value.IsNil() {
+		if err = c.updateLocalValueAndWatch(ctx); err != nil {
+			return nil, err
+		}
+	}
+	return c.value.Val().(*gjson.Json).Map(), nil
+}
+
+// init retrieves and caches the configmap content.
+func (c *Client) updateLocalValueAndWatch(ctx context.Context) (err error) {
+	if err = c.doUpdate(ctx); err != nil {
+		err = gerror.Wrap(err, "failed to update local value")
+		return err
+	}
+	if err = c.doWatch(ctx); err != nil {
+		err = gerror.Wrap(err, "failed to watch configmap")
+		return err
+	}
+	return nil
+}
+
+func (c *Client) doUpdate(ctx context.Context) (err error) {
+	if !c.client.HasContent() {
+		g.Log().Debug(ctx, "config file is empty")
+		return gerror.New("config file is empty")
+	}
+	var j *gjson.Json
+	if j, err = gjson.LoadContent(c.client.GetContent()); err != nil {
+		return gerror.Wrap(err, `parse config map item from polaris failed`)
+	}
+	c.value.Set(j)
+	return nil
+}
+
+func (c *Client) doWatch(ctx context.Context) (err error) {
+	if !c.config.Watch {
+		return nil
+	}
+	var changeChan = make(chan model.ConfigFileChangeEvent)
+	c.client.AddChangeListenerWithChannel(changeChan)
+	go func() {
+		for {
+			select {
+			case event := <-changeChan:
+				fmt.Println(fmt.Sprintf("received change event by channel. %+v", event))
+				g.Log().Debugf(ctx, "received change event by channel. %+v", event)
+				_ = c.doUpdate(ctx)
+			}
+		}
+	}()
+	return nil
+}
---
+++ example/config/polaris/boot/boot.go
@@ -0,0 +1,32 @@
+package boot
+
+import (
+	"github.com/gogf/gf/contrib/config/polaris/v2"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
+)
+
+func init() {
+	var (
+		ctx        = gctx.GetInitCtx()
+		namespace  = "default"
+		fileGroup  = "goframe"
+		fileName   = "config.yaml"
+		configPath = "testdata/polaris.yaml"
+		logDir     = "/tmp/polaris/log"
+	)
+	// Create polaris Client that implements gcfg.Adapter.
+	adapter, err := polaris.New(ctx, polaris.Config{
+		Namespace:  namespace,
+		FileGroup:  fileGroup,
+		FileName:   fileName,
+		ConfigPath: configPath,
+		LogDir:     logDir,
+		Watch:      true,
+	})
+	if err != nil {
+		g.Log().Fatalf(ctx, `%+v`, err)
+	}
+	// Change the adapter of default configuration instance.
+	g.Cfg().SetAdapter(adapter)
+}
