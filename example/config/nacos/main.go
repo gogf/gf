7902bbdef6996dc7f939bcb6551d7847f7c3555a
qinyuguang <qinyuguang@gmail.com>
2022-10-22 12:49:32 +0800

    feat: add `gcfg.Adapter` implements using nacos service

---
+++ example/config/nacos/main.go
@@ -0,0 +1,20 @@
+package nacos
+
+import (
+	_ "github.com/gogf/gf/example/config/nacos/boot"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
+)
+
+func main() {
+	var ctx = gctx.GetInitCtx()
+
+	// Available checks.
+	g.Dump(g.Cfg().Available(ctx))
+
+	// All key-value configurations.
+	g.Dump(g.Cfg().Data(ctx))
+
+	// Retrieve certain value by key.
+	g.Dump(g.Cfg().MustGet(ctx, "server.address"))
+}
