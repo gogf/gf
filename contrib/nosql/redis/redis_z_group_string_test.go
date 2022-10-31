60aa8b19cb6dc61e62c838a2d42a5685319021a8
John Guo <john@johng.cn>
2022-10-31 23:28:41 +0800

    ut cases for group string

--- contrib/nosql/redis/redis_z_group_string_test.go
+++ contrib/nosql/redis/redis_z_group_string_test.go
@@ -226,3 +226,200 @@ func Test_GroupString_Append(t *testing.T) {
 		t.Assert(r2.String(), v1+v2)
 	})
 }
+
+func Test_GroupString_SetRange(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		defer redis.FlushDB(ctx)
+		var (
+			k1 = "k1"
+			v1 = "v1"
+			v2 = "v2"
+		)
+		_, err := redis.GroupString().Set(ctx, k1, v1)
+		t.AssertNil(err)
+
+		r1, err := redis.GroupString().SetRange(ctx, k1, 2, v2)
+		t.AssertNil(err)
+		t.Assert(r1, len(v1+v2))
+
+		r2, err := redis.GroupString().Get(ctx, k1)
+		t.AssertNil(err)
+		t.Assert(r2.String(), v1+v2)
+	})
+}
+
+func Test_GroupString_GetRange(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		defer redis.FlushDB(ctx)
+		var (
+			k1 = "k1"
+			v1 = "hello gf"
+		)
+		_, err := redis.GroupString().Set(ctx, k1, v1)
+		t.AssertNil(err)
+
+		r1, err := redis.GroupString().GetRange(ctx, k1, 6, 8)
+		t.AssertNil(err)
+		t.Assert(r1, "gf")
+	})
+}
+
+func Test_GroupString_Incr(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		defer redis.FlushDB(ctx)
+		var (
+			k1 = "k1"
+			v1 = 1
+		)
+		_, err := redis.GroupString().Set(ctx, k1, v1)
+		t.AssertNil(err)
+
+		r1, err := redis.GroupString().Incr(ctx, k1)
+		t.AssertNil(err)
+		t.Assert(r1, 2)
+	})
+}
+
+func Test_GroupString_IncrBy(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		defer redis.FlushDB(ctx)
+		var (
+			k1 = "k1"
+			v1 = 1
+		)
+		_, err := redis.GroupString().Set(ctx, k1, v1)
+		t.AssertNil(err)
+
+		r1, err := redis.GroupString().IncrBy(ctx, k1, 10)
+		t.AssertNil(err)
+		t.Assert(r1, 11)
+	})
+}
+
+func Test_GroupString_IncrByFloat(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		defer redis.FlushDB(ctx)
+		var (
+			k1 = "k1"
+			v1 = 1
+		)
+		_, err := redis.GroupString().Set(ctx, k1, v1)
+		t.AssertNil(err)
+
+		r1, err := redis.GroupString().IncrByFloat(ctx, k1, 1.01)
+		t.AssertNil(err)
+		t.Assert(r1, 2.01)
+	})
+}
+
+func Test_GroupString_Decr(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		defer redis.FlushDB(ctx)
+		var (
+			k1 = "k1"
+			v1 = 10
+		)
+		_, err := redis.GroupString().Set(ctx, k1, v1)
+		t.AssertNil(err)
+
+		r1, err := redis.GroupString().Decr(ctx, k1)
+		t.AssertNil(err)
+		t.Assert(r1, 9)
+	})
+}
+
+func Test_GroupString_DecrBy(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		defer redis.FlushDB(ctx)
+		var (
+			k1 = "k1"
+			v1 = 10
+		)
+		_, err := redis.GroupString().Set(ctx, k1, v1)
+		t.AssertNil(err)
+
+		r1, err := redis.GroupString().DecrBy(ctx, k1, 3)
+		t.AssertNil(err)
+		t.Assert(r1, 7)
+	})
+}
+
+func Test_GroupString_MSet(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		defer redis.FlushDB(ctx)
+		var (
+			k1 = guid.S()
+			v1 = guid.S()
+			k2 = guid.S()
+			v2 = guid.S()
+		)
+		err := redis.GroupString().MSet(ctx, map[string]interface{}{
+			k1: v1,
+			k2: v2,
+		})
+		t.AssertNil(err)
+
+		r1, err := redis.GroupString().Get(ctx, k1)
+		t.AssertNil(err)
+		t.Assert(r1.String(), v1)
+
+		r2, err := redis.GroupString().Get(ctx, k2)
+		t.AssertNil(err)
+		t.Assert(r2.String(), v2)
+	})
+}
+
+func Test_GroupString_MSetNX(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		defer redis.FlushDB(ctx)
+		var (
+			k1 = guid.S()
+			v1 = guid.S()
+			k2 = guid.S()
+			v2 = guid.S()
+		)
+		ok, err := redis.GroupString().MSetNX(ctx, map[string]interface{}{
+			k1: v1,
+		})
+		t.AssertNil(err)
+		t.Assert(ok, true)
+
+		ok, err = redis.GroupString().MSetNX(ctx, map[string]interface{}{
+			k1: v1,
+			k2: v2,
+		})
+		t.AssertNil(err)
+		t.Assert(ok, false)
+
+		r1, err := redis.GroupString().Get(ctx, k1)
+		t.AssertNil(err)
+		t.Assert(r1.String(), v1)
+
+		r2, err := redis.GroupString().Get(ctx, k2)
+		t.AssertNil(err)
+		t.Assert(r2.String(), "")
+	})
+}
+
+func Test_GroupString_MGet(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		defer redis.FlushDB(ctx)
+		var (
+			k1 = guid.S()
+			v1 = guid.S()
+			k2 = guid.S()
+			v2 = guid.S()
+		)
+		err := redis.GroupString().MSet(ctx, map[string]interface{}{
+			k1: v1,
+			k2: v2,
+		})
+		t.AssertNil(err)
+
+		r1, err := redis.GroupString().MGet(ctx, k1, k2)
+		t.AssertNil(err)
+		t.Assert(len(r1), 2)
+		t.Assert(r1[k1].String(), v1)
+		t.Assert(r1[k2].String(), v2)
+	})
+}
