240c04721cacbd52de7ad2ab8da3b9fc0410035d
huangqian <huangqian1985@qq.com>
2022-11-05 17:23:05 +0800

    improve gconv_convert.go code coverage

--- util/gconv/gconv_z_unit_all_test.go
+++ util/gconv/gconv_z_unit_all_test.go
@@ -750,6 +750,7 @@ func Test_Convert_All(t *testing.T) {
 		t.AssertEQ(gconv.Convert([]byte{}, "[]byte"), []uint8{})
 		t.AssertEQ(gconv.Convert([]string{}, "[]string"), []string{})
 		t.AssertEQ(gconv.Convert([2]int{1, 2}, "[]int"), []int{1, 2})
+		t.AssertEQ(gconv.Convert([2]uint8{1, 2}, "[]uint8"), []uint8{1, 2})
 		t.AssertEQ(gconv.Convert("1989-01-02", "Time", "Y-m-d"), gconv.Time("1989-01-02", "Y-m-d"))
 		t.AssertEQ(gconv.Convert(1989, "Time"), gconv.Time("1970-01-01 08:33:09 +0800 CST"))
 		t.AssertEQ(gconv.Convert(gtime.Now(), "gtime.Time", 1), *gtime.New())
@@ -760,6 +761,63 @@ func Test_Convert_All(t *testing.T) {
 		t.AssertEQ(gconv.Convert(1989, "Duration"), time.Duration(int64(1989)))
 		t.AssertEQ(gconv.Convert("1989", "Duration"), time.Duration(int64(1989)))
 		t.AssertEQ(gconv.Convert("1989", ""), "1989")
+
+		var intNum int = 1
+		t.Assert(gconv.Convert(&intNum, "*int"), int(1))
+		var int8Num int8 = 1
+		t.Assert(gconv.Convert(int8Num, "*int8"), int(1))
+		t.Assert(gconv.Convert(&int8Num, "*int8"), int(1))
+		var int16Num int16 = 1
+		t.Assert(gconv.Convert(int16Num, "*int16"), int(1))
+		t.Assert(gconv.Convert(&int16Num, "*int16"), int(1))
+		var int32Num int32 = 1
+		t.Assert(gconv.Convert(int32Num, "*int32"), int(1))
+		t.Assert(gconv.Convert(&int32Num, "*int32"), int(1))
+		var int64Num int64 = 1
+		t.Assert(gconv.Convert(int64Num, "*int64"), int(1))
+		t.Assert(gconv.Convert(&int64Num, "*int64"), int(1))
+
+		var uintNum uint = 1
+		t.Assert(gconv.Convert(&uintNum, "*uint"), int(1))
+		var uint8Num uint8 = 1
+		t.Assert(gconv.Convert(uint8Num, "*uint8"), int(1))
+		t.Assert(gconv.Convert(&uint8Num, "*uint8"), int(1))
+		var uint16Num uint16 = 1
+		t.Assert(gconv.Convert(uint16Num, "*uint16"), int(1))
+		t.Assert(gconv.Convert(&uint16Num, "*uint16"), int(1))
+		var uint32Num uint32 = 1
+		t.Assert(gconv.Convert(uint32Num, "*uint32"), int(1))
+		t.Assert(gconv.Convert(&uint32Num, "*uint32"), int(1))
+		var uint64Num uint64 = 1
+		t.Assert(gconv.Convert(uint64Num, "*uint64"), int(1))
+		t.Assert(gconv.Convert(&uint64Num, "*uint64"), int(1))
+
+		var float32Num float32 = 1.1
+		t.Assert(gconv.Convert(float32Num, "*float32"), float32(1.1))
+		t.Assert(gconv.Convert(&float32Num, "*float32"), float32(1.1))
+
+		var float64Num float64 = 1.1
+		t.Assert(gconv.Convert(float64Num, "*float64"), float64(1.1))
+		t.Assert(gconv.Convert(&float64Num, "*float64"), float64(1.1))
+
+		var boolValue bool = true
+		t.Assert(gconv.Convert(boolValue, "*bool"), true)
+		t.Assert(gconv.Convert(&boolValue, "*bool"), true)
+
+		var stringValue string = "1"
+		t.Assert(gconv.Convert(stringValue, "*string"), "1")
+		t.Assert(gconv.Convert(&stringValue, "*string"), "1")
+
+		var durationValue time.Duration = 1989
+		var expectDurationValue = time.Duration(int64(1989))
+		t.AssertEQ(gconv.Convert(&durationValue, "*time.Duration"), &expectDurationValue)
+		t.AssertEQ(gconv.Convert(durationValue, "*time.Duration"), &expectDurationValue)
+
+		var string_interface_map = map[string]interface{}{"k1": 1}
+		var string_int_map = map[string]int{"k1": 1}
+		var string_string_map = map[string]string{"k1": "1"}
+		t.AssertEQ(gconv.Convert(string_int_map, "map[string]string"), string_string_map)
+		t.AssertEQ(gconv.Convert(string_int_map, "map[string]interface{}"), string_interface_map)
 	})
 }
 
