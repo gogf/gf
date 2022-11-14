7e1838b7a330c3c926ea1b80c9b0fabd7250c8a5
huyuanxin <huyuanxin1999@outlook.com>
2022-11-14 08:25:52 +0800

    change file name

--- contrib/registry/zookeeper/zookeeper_func.go
+++ contrib/registry/zookeeper/zookeeper_func.go
@@ -4,7 +4,6 @@
 // If a copy of the MIT was not distributed with this file,
 // You can obtain one at https://github.com/gogf/gf.
 
-// Package etcd implements service Registry and Discovery using zookeeper.
 package zookeeper
 
 import (
