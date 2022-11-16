fd8dd67e91bf122a3d25a1f9745ae68faf13fb8b
huangqian <huangqian1985@qq.com>
2022-11-17 07:47:36 +0800

    add ut cases for package gerror

--- errors/gerror/gerror_z_unit_test.go
+++ errors/gerror/gerror_z_unit_test.go
@@ -95,10 +95,14 @@ func Test_Wrapf(t *testing.T) {
 		t.AssertNE(err, nil)
 		t.Assert(err.Error(), "1")
 	})
+	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.Wrapf(nil, ""), nil)
+	})
 }
 
 func Test_WrapSkip(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.WrapSkip(1, nil, "2"), nil)
 		err := errors.New("1")
 		err = gerror.WrapSkip(1, err, "2")
 		err = gerror.WrapSkip(1, err, "3")
@@ -122,6 +126,7 @@ func Test_WrapSkip(t *testing.T) {
 
 func Test_WrapSkipf(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.WrapSkipf(1, nil, "2"), nil)
 		err := errors.New("1")
 		err = gerror.WrapSkipf(1, err, "2")
 		err = gerror.WrapSkipf(1, err, "3")
@@ -145,6 +150,7 @@ func Test_WrapSkipf(t *testing.T) {
 
 func Test_Cause(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.Cause(nil), nil)
 		err := errors.New("1")
 		t.Assert(gerror.Cause(err), err)
 	})
@@ -166,6 +172,12 @@ func Test_Cause(t *testing.T) {
 		err = gerror.Wrap(err, "2")
 		err = gerror.Wrap(err, "3")
 		t.Assert(gerror.Cause(err), "1")
+	})
+
+	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.Stack(nil), "")
+		err := errors.New("1")
+		t.Assert(gerror.Stack(err), err)
 	})
 }
 
@@ -229,6 +241,7 @@ func Test_Stack(t *testing.T) {
 
 func Test_Current(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.Current(nil), nil)
 		err := errors.New("1")
 		err = gerror.Wrap(err, "2")
 		err = gerror.Wrap(err, "3")
@@ -239,6 +252,7 @@ func Test_Current(t *testing.T) {
 
 func Test_Unwrap(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.Unwrap(nil), nil)
 		err := errors.New("1")
 		err = gerror.Wrap(err, "2")
 		err = gerror.Wrap(err, "3")
@@ -282,6 +296,7 @@ func Test_Code(t *testing.T) {
 		t.Assert(err.Error(), "123")
 	})
 	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.WrapCode(gcode.New(1, "", nil), nil, "3"), nil)
 		err := errors.New("1")
 		err = gerror.Wrap(err, "2")
 		err = gerror.WrapCode(gcode.New(1, "", nil), err, "3")
@@ -289,6 +304,7 @@ func Test_Code(t *testing.T) {
 		t.Assert(err.Error(), "3: 2: 1")
 	})
 	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.WrapCodef(gcode.New(1, "", nil), nil, "%s", "3"), nil)
 		err := errors.New("1")
 		err = gerror.Wrap(err, "2")
 		err = gerror.WrapCodef(gcode.New(1, "", nil), err, "%s", "3")
@@ -296,6 +312,7 @@ func Test_Code(t *testing.T) {
 		t.Assert(err.Error(), "3: 2: 1")
 	})
 	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.WrapCodeSkip(gcode.New(1, "", nil), 100, nil, "3"), nil)
 		err := errors.New("1")
 		err = gerror.Wrap(err, "2")
 		err = gerror.WrapCodeSkip(gcode.New(1, "", nil), 100, err, "3")
@@ -303,11 +320,54 @@ func Test_Code(t *testing.T) {
 		t.Assert(err.Error(), "3: 2: 1")
 	})
 	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.WrapCodeSkipf(gcode.New(1, "", nil), 100, nil, "%s", "3"), nil)
 		err := errors.New("1")
 		err = gerror.Wrap(err, "2")
 		err = gerror.WrapCodeSkipf(gcode.New(1, "", nil), 100, err, "%s", "3")
 		t.Assert(gerror.Code(err).Code(), 1)
 		t.Assert(err.Error(), "3: 2: 1")
+	})
+}
+
+func TestError_Error(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		var e *gerror.Error = nil
+		t.Assert(e.Error(), nil)
+	})
+}
+
+func TestError_Cause(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		var e *gerror.Error = nil
+		t.Assert(e.Cause(), nil)
+	})
+}
+
+func TestError_Current(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		var e *gerror.Error = nil
+		t.Assert(e.Current(), nil)
+	})
+}
+
+func TestError_Unwrap(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		var e *gerror.Error = nil
+		t.Assert(e.Unwrap(), nil)
+	})
+}
+
+func TestError_Equal(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		err := errors.New("1")
+		t.Assert(err.(gerror.Error).Equal(err), nil)
+	})
+}
+
+func TestError_Code(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		var e *gerror.Error = nil
+		t.Assert(e.Code(), gcode.CodeNil)
 	})
 }
 
@@ -375,6 +435,7 @@ func Test_HashError(t *testing.T) {
 
 func Test_HashCode(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gerror.HasCode(nil, gcode.CodeNotAuthorized), false)
 		err1 := errors.New("1")
 		err2 := gerror.WrapCode(gcode.CodeNotAuthorized, err1, "2")
 		err3 := gerror.Wrap(err2, "3")
@@ -383,5 +444,16 @@ func Test_HashCode(t *testing.T) {
 		t.Assert(gerror.HasCode(err2, gcode.CodeNotAuthorized), true)
 		t.Assert(gerror.HasCode(err3, gcode.CodeNotAuthorized), true)
 		t.Assert(gerror.HasCode(err4, gcode.CodeNotAuthorized), true)
+	})
+}
+
+func Test_NewOption(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		t.AssertNE(gerror.NewOption(gerror.Option{
+			Error: errors.New("NewOptionError"),
+			Stack: true,
+			Text:  "Text",
+			Code:  gcode.CodeNotAuthorized,
+		}), gerror.New("NewOptionError"))
 	})
 }
