e3add3845ade43bd1e16b69dc61073f92179f143
huangqian <huangqian1985@qq.com>
2022-12-07 22:07:11 +0800

    add ut cases for package gcache

--- os/gcmd/gcmd_z_unit_test.go
+++ os/gcmd/gcmd_z_unit_test.go
@@ -12,6 +12,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/gogf/gf/v2/errors/gcode"
@@ -64,7 +65,8 @@ func Test_BuildOptions(t *testing.T) {
 			"n1": "john",
 			"n2": "huang",
 		})
-		t.Assert(s, "-n1=john -n2=huang")
+		t.Assert(strings.Contains(s, "-n1=john"), true)
+		t.Assert(strings.Contains(s, "-n2=huang"), true)
 	})
 }
 
