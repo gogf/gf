347a2bc3543306b5441edb061aea46d105e4756f
Charles <8595764@qq.com>
2022-12-16 17:13:00 +0800

    change ghttp GetPage totalSize to int64

--- net/ghttp/ghttp_request_param_page.go
+++ net/ghttp/ghttp_request_param_page.go
@@ -17,7 +17,7 @@ import (
 // GetPage creates and returns the pagination object for given `totalSize` and `pageSize`.
 // NOTE THAT the page parameter name from clients is constantly defined as gpage.DefaultPageName
 // for simplification and convenience.
-func (r *Request) GetPage(totalSize, pageSize int) *gpage.Page {
+func (r *Request) GetPage(totalSize int64, pageSize int) *gpage.Page {
 	// It must have Router object attribute.
 	if r.Router == nil {
 		panic("Router object not found")
