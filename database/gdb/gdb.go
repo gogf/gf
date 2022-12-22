e007bf35b2747f2d3c6ce145a26d370842ad1727
Sany <sanrentai@163.com>
2022-12-22 17:33:51 +0800

    parseConfigNodeLink support Chinese database name #2231 (#2238)

--- database/gdb/gdb.go
+++ database/gdb/gdb.go
@@ -316,7 +316,7 @@ const (
 	ctxKeyInternalProducedSQL gctx.StrKey = `CtxKeyInternalProducedSQL`
 
 	// type:[username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
-	linkPattern = `(\w+):([\w\-]*):(.*?)@(\w+?)\((.+?)\)/{0,1}([\w\-]*)\?{0,1}(.*)`
+	linkPattern = `(\w+):([\w\-]*):(.*?)@(\w+?)\((.+?)\)/{0,1}([^\?]*)\?{0,1}(.*)`
 )
 
 const (
