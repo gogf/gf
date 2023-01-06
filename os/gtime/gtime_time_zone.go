ab471cb2a87ea59e4c7d11cbaed034332a9bc281
John Guo <john@johng.cn>
2023-01-06 15:10:11 +0800

    improve function SetTimeZone for package gtime

--- os/gtime/gtime_time_zone.go
+++ os/gtime/gtime_time_zone.go
@@ -7,6 +7,7 @@
 package gtime
 
 import (
+	"os"
 	"sync"
 	"time"
 
@@ -14,13 +15,52 @@ import (
 )
 
 var (
-	// locationMap is time zone name to its location object.
-	// Time zone name is like: Asia/Shanghai.
-	locationMap = make(map[string]*time.Location)
-
-	// locationMu is used for concurrent safety for `locationMap`.
-	locationMu = sync.RWMutex{}
+	setTimeZoneMu   sync.Mutex
+	setTimeZoneName string
+	zoneMap         = make(map[string]*time.Location)
+	zoneMu          sync.RWMutex
 )
+
+// SetTimeZone sets the time zone for current whole process.
+// The parameter `zone` is an area string specifying corresponding time zone,
+// eg: Asia/Shanghai.
+//
+// PLEASE VERY NOTE THAT:
+// 1. This should be called before package "time" import.
+// 2. This function should be called once.
+// 3. Please refer to issue: https://github.com/golang/go/issues/34814
+func SetTimeZone(zone string) (err error) {
+	setTimeZoneMu.Lock()
+	defer setTimeZoneMu.Unlock()
+	if setTimeZoneName != "" {
+		return gerror.Newf(`process timezone already set using "%s"`, setTimeZoneName)
+	}
+	defer func() {
+		if err == nil {
+			setTimeZoneName = zone
+		}
+	}()
+
+	// Load zone info from specified name.
+	location, err := time.LoadLocation(zone)
+	if err != nil {
+		err = gerror.Wrapf(err, `time.LoadLocation failed for zone "%s"`, zone)
+		return err
+	}
+
+	// Update the time.Local for once.
+	time.Local = location
+
+	// Update the timezone environment for *nix systems.
+	var (
+		envKey   = "TZ"
+		envValue = location.String()
+	)
+	if err = os.Setenv(envKey, envValue); err != nil {
+		err = gerror.Wrapf(err, `set environment failed with key "%s", value "%s"`, envKey, envValue)
+	}
+	return
+}
 
 // ToLocation converts current time to specified location.
 func (t *Time) ToLocation(location *time.Location) *Time {
@@ -39,18 +79,18 @@ func (t *Time) ToZone(zone string) (*Time, error) {
 }
 
 func (t *Time) getLocationByZoneName(name string) (location *time.Location, err error) {
-	locationMu.RLock()
-	location = locationMap[name]
-	locationMu.RUnlock()
+	zoneMu.RLock()
+	location = zoneMap[name]
+	zoneMu.RUnlock()
 	if location == nil {
 		location, err = time.LoadLocation(name)
 		if err != nil {
 			err = gerror.Wrapf(err, `time.LoadLocation failed for name "%s"`, name)
 		}
 		if location != nil {
-			locationMu.Lock()
-			locationMap[name] = location
-			locationMu.Unlock()
+			zoneMu.Lock()
+			zoneMap[name] = location
+			zoneMu.Unlock()
 		}
 	}
 	return
