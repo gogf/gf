17b22312e4d7f56b7b42926c33115dea11b74a48
John Guo <john@johng.cn>
2023-02-14 16:32:10 +0800

    fix issue #2447

--- net/ghttp/ghttp_response.go
+++ net/ghttp/ghttp_response.go
@@ -10,6 +10,7 @@ package ghttp
 import (
 	"bytes"
 	"fmt"
+	"github.com/gogf/gf/v2/net/ghttp/internal/response"
 	"io"
 	"net/http"
 	"net/url"
@@ -34,7 +35,7 @@ func newResponse(s *Server, w http.ResponseWriter) *Response {
 	r := &Response{
 		Server: s,
 		ResponseWriter: &ResponseWriter{
-			writer: w,
+			writer: response.NewWriter(w),
 			buffer: bytes.NewBuffer(nil),
 		},
 	}
@@ -152,7 +153,6 @@ func (r *Response) ClearBuffer() {
 //
 // See http.ServeContent
 func (r *Response) ServeContent(name string, modTime time.Time, content io.ReadSeeker) {
-	r.wroteHeader = true
 	http.ServeContent(r.Writer.RawWriter(), r.Request.Request, name, modTime, content)
 }
 
