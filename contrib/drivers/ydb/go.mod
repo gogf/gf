f2bba8d0673c8e23002702abea1b5da54c9bad8a
rasha108bik <rasha108bick@yandex.ru>
2023-02-15 23:18:57 +0800

    add ydb

---
+++ contrib/drivers/ydb/go.mod
@@ -0,0 +1,10 @@
+module github.com/gogf/gf/contrib/drivers/ydb/v2
+
+go 1.16
+
+require (
+	github.com/gogf/gf/v2 v2.3.2
+	github.com/ydb-platform/ydb-go-sdk/v3 v3.42.10
+)
+
+replace github.com/gogf/gf/v2 => ../../../
---
+++ contrib/drivers/ydb/ydb.go
@@ -0,0 +1,146 @@
+// Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
+// Package ydb implements gdb.Driver, which supports operations for database YDB.
+package ydb
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/gogf/gf/v2/database/gdb"
+	"github.com/gogf/gf/v2/errors/gcode"
+	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/util/gutil"
+	_ "github.com/ydb-platform/ydb-go-sdk/v3"
+)
+
+// Driver is the driver for ydb database.
+type Driver struct {
+	*gdb.Core
+}
+
+func init() {
+	if err := gdb.Register(`ydb`, New()); err != nil {
+		panic(err)
+	}
+}
+
+// New create and returns a driver that implements gdb.Driver, which supports operations for YDB.
+func New() gdb.Driver {
+	return &Driver{}
+}
+
+// New creates and returns a database object for ydb.
+// It implements the interface of gdb.Driver for extra database driver installation.
+func (d *Driver) New(core *gdb.Core, node *gdb.ConfigNode) (gdb.DB, error) {
+	return &Driver{
+		Core: core,
+	}, nil
+}
+
+// Open creates and returns an underlying sql.DB object for ydb.
+// Note that it converts time.Time argument to local timezone in default.
+func (d *Driver) Open(config *gdb.ConfigNode) (db *sql.DB, err error) {
+	var (
+		source               string
+		underlyingDriverName = "ydb"
+	)
+
+	if config.Name == "" {
+		return nil, fmt.Errorf(
+			`ydb.Open failed for driver "%s" without DB Name`, underlyingDriverName,
+		)
+	}
+	// Data Source Name of YDB:
+	// grpcs://userName:password@ip:port/dbname
+	source = fmt.Sprintf(
+		"grpcs://%s:%s@%s:%s/%s",
+		config.User, config.Pass, config.Host, config.Port, config.Name,
+	)
+	if config.Timezone != "" {
+		if strings.Contains(config.Timezone, "/") {
+			config.Timezone = url.QueryEscape(config.Timezone)
+		}
+	}
+	if db, err = sql.Open(underlyingDriverName, source); err != nil {
+		err = gerror.WrapCodef(
+			gcode.CodeDbOperationError, err,
+			`sql.Open failed for driver "%s" by source "%s"`, underlyingDriverName, source,
+		)
+		return nil, err
+	}
+	return
+}
+
+// Tables retrieves and returns the tables of current schema.
+// It's mainly used in cli tool chain for automatically generating the models.
+func (d *Driver) Tables(ctx context.Context, schema ...string) (tables []string, err error) {
+	var result gdb.Result
+	link, err := d.SlaveLink(schema...)
+	if err != nil {
+		return nil, err
+	}
+	result, err = d.DoSelect(ctx, link, `SHOW TABLES`)
+	if err != nil {
+		return
+	}
+	for _, m := range result {
+		for _, v := range m {
+			tables = append(tables, v.String())
+		}
+	}
+	return
+}
+
+// TableFields retrieves and returns the fields' information of specified table of current
+// schema.
+//
+// The parameter `link` is optional, if given nil it automatically retrieves a raw sql connection
+// as its link to proceed necessary sql query.
+//
+// Note that it returns a map containing the field name and its corresponding fields.
+// As a map is unsorted, the TableField struct has a "Index" field marks its sequence in
+// the fields.
+//
+// It's using cache feature to enhance the performance, which is never expired util the
+// process restarts.
+func (d *Driver) TableFields(
+	ctx context.Context, table string, schema ...string,
+) (fields map[string]*gdb.TableField, err error) {
+	var (
+		result    gdb.Result
+		link      gdb.Link
+		useSchema = gutil.GetOrDefaultStr(d.GetSchema(), schema...)
+	)
+	if link, err = d.SlaveLink(useSchema); err != nil {
+		return nil, err
+	}
+	result, err = d.DoSelect(
+		ctx, link,
+		fmt.Sprintf(`SHOW FULL COLUMNS FROM %s`, d.QuoteWord(table)),
+	)
+	if err != nil {
+		return nil, err
+	}
+	fields = make(map[string]*gdb.TableField)
+	for i, m := range result {
+		fields[m["Field"].String()] = &gdb.TableField{
+			Index:   i,
+			Name:    m["Field"].String(),
+			Type:    m["Type"].String(),
+			Null:    m["Null"].Bool(),
+			Key:     m["Key"].String(),
+			Default: m["Default"].Val(),
+			Extra:   m["Extra"].String(),
+			Comment: m["Comment"].String(),
+		}
+	}
+	return fields, nil
+}
