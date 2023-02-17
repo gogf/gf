c07fa80eb24852012d2103f1e5dd61d2f319f32b
John Guo <john@johng.cn>
2023-02-17 09:53:49 +0800

    up

--- contrib/rpc/grpcx/internal/utils/utils.go
+++ contrib/rpc/grpcx/internal/utils/utils.go
@@ -4,6 +4,7 @@
 // If a copy of the MIT was not distributed with this file,
 // You can obtain one at https://github.com/gogf/gf.
 
+// Package utils provides utilities for GRPC.
 package utils
 
 import (
