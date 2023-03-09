e2d43ecd862e1671695ac8ba795e70645776f2f8
John Guo <john@johng.cn>
2023-03-09 09:52:44 +0800

    up

--- contrib/rpc/grpcx/grpcx_grpc_server.go
+++ contrib/rpc/grpcx/grpcx_grpc_server.go
@@ -78,6 +78,7 @@ func (s modServer) New(conf ...*GrpcServerConfig) *GrpcServer {
 			s.UnaryRecover,
 			s.UnaryTracing,
 			s.UnaryError,
+			s.UnaryAllowNilRes,
 			grpcServer.internalUnaryLogger,
 		),
 		s.ChainStream(
