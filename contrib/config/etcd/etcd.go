54dcb20c26aa553adfad0de10e38ce0befdaae7b
junler <junler_top@qq.com>
2023-04-14 14:29:02 +0800

    add the copy rights comments

--- contrib/config/etcd/etcd.go
+++ contrib/config/etcd/etcd.go
@@ -1,3 +1,10 @@
+// Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
+// Package etcd implements gcfg.Adapter using etcd service.
 package etcd
 
 import (
