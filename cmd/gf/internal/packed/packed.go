7fb9c08867de463243edd32f6ef8848691efb3e7
John Guo <john@johng.cn>
2023-04-20 15:15:06 +0800

    add copyright comments to command go files

--- cmd/gf/internal/packed/packed.go
+++ cmd/gf/internal/packed/packed.go
@@ -1,1 +1,7 @@
+// Copyright GoFrame gf Author(https://goframe.org). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
 package packed
