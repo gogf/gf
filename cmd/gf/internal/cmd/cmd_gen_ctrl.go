a5f52341b47cdfef2d658658b0eedc75d8797bb6
John Guo <john@johng.cn>
2023-06-14 22:23:02 +0800

    add http sdk generating for command gen ctrl

--- cmd/gf/internal/cmd/cmd_gen_ctrl.go
+++ cmd/gf/internal/cmd/cmd_gen_ctrl.go
@@ -8,12 +8,6 @@ package cmd
 
 import (
 	"github.com/gogf/gf/cmd/gf/v2/internal/cmd/genctrl"
-	_ "github.com/gogf/gf/contrib/drivers/clickhouse/v2"
-	_ "github.com/gogf/gf/contrib/drivers/mssql/v2"
-	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
-	_ "github.com/gogf/gf/contrib/drivers/oracle/v2"
-	_ "github.com/gogf/gf/contrib/drivers/pgsql/v2"
-	_ "github.com/gogf/gf/contrib/drivers/sqlite/v2"
 )
 
 type (
