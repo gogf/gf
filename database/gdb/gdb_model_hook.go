72d5f7a710bacb53a3231901e106276761c83756
John Guo <john@johng.cn>
2023-07-11 22:02:14 +0800

    remove sharding feature from gdb.Model

--- database/gdb/gdb_model_hook.go
+++ database/gdb/gdb_model_hook.go
@@ -66,7 +66,7 @@ type internalParamHookDelete struct {
 // which is usually not be interesting for upper business hook handler.
 type HookSelectInput struct {
 	internalParamHookSelect
-	Model  *Model        // Current operation Model, which takes no effect if updated.
+	Model  *Model        // Current operation Model.
 	Table  string        // The table name that to be used. Update this attribute to change target table name.
 	Schema string        // The schema name that to be used. Update this attribute to change target schema name.
 	Sql    string        // The sql string that to be committed.
@@ -76,7 +76,7 @@ type HookSelectInput struct {
 // HookInsertInput holds the parameters for insert hook operation.
 type HookInsertInput struct {
 	internalParamHookInsert
-	Model  *Model         // Current operation Model, which takes no effect if updated.
+	Model  *Model         // Current operation Model.
 	Table  string         // The table name that to be used. Update this attribute to change target table name.
 	Schema string         // The schema name that to be used. Update this attribute to change target schema name.
 	Data   List           // The data records list to be inserted/saved into table.
@@ -86,7 +86,7 @@ type HookInsertInput struct {
 // HookUpdateInput holds the parameters for update hook operation.
 type HookUpdateInput struct {
 	internalParamHookUpdate
-	Model     *Model        // Current operation Model, which takes no effect if updated.
+	Model     *Model        // Current operation Model.
 	Table     string        // The table name that to be used. Update this attribute to change target table name.
 	Schema    string        // The schema name that to be used. Update this attribute to change target schema name.
 	Data      interface{}   // Data can be type of: map[string]interface{}/string. You can use type assertion on `Data`.
@@ -97,7 +97,7 @@ type HookUpdateInput struct {
 // HookDeleteInput holds the parameters for delete hook operation.
 type HookDeleteInput struct {
 	internalParamHookDelete
-	Model     *Model        // Current operation Model, which takes no effect if updated.
+	Model     *Model        // Current operation Model.
 	Table     string        // The table name that to be used. Update this attribute to change target table name.
 	Schema    string        // The schema name that to be used. Update this attribute to change target schema name.
 	Condition string        // The where condition string for deleting.
