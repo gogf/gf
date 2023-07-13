dfd6915cdcf445f8c9762e58d4b2c0c1b998373e
John Guo <john@johng.cn>
2023-07-13 16:17:16 +0800

    fix issue #2760

--- util/gconv/gconv_map.go
+++ util/gconv/gconv_map.go
@@ -255,10 +255,21 @@ func doMapConvertForMapOrStructValue(in doMapConvertForMapOrStructValueInput) in
 			dataMap = make(map[string]interface{})
 		)
 		for _, k := range mapKeys {
+			var (
+				mapKeyValue = reflectValue.MapIndex(k)
+				mapValue    interface{}
+			)
+			if mapKeyValue.IsZero() {
+				// in case of:
+				// exception recovered: reflect: call of reflect.Value.Interface on zero Value
+				mapValue = reflect.New(mapKeyValue.Type()).Elem()
+			} else {
+				mapValue = mapKeyValue.Interface()
+			}
 			dataMap[String(k.Interface())] = doMapConvertForMapOrStructValue(
 				doMapConvertForMapOrStructValueInput{
 					IsRoot:          false,
-					Value:           reflectValue.MapIndex(k).Interface(),
+					Value:           mapValue,
 					RecursiveType:   in.RecursiveType,
 					RecursiveOption: in.RecursiveType == recursiveTypeTrue,
 					Tags:            in.Tags,
