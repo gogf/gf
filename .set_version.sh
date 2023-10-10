a949e9c6b9e42dc82209b304ab33878f40a8dddc
海亮 <739476267@qq.com>
2023-10-10 10:20:07 +0000

    fix: 修复脚本无法修改版本问题

--- .set_version.sh
+++ .set_version.sh
@@ -39,11 +39,34 @@ for file in `find ${workdir} -name go.mod`; do
     echo ""
     echo "processing dir: $goModPath"
     cd $goModPath
+    if [ $goModPath = "./cmd/gf" ]; then
+        mv go.work go.work.version.bak
+        go mod edit -replace github.com/gogf/gf/v2=../../
+        go mod edit -replace github.com/gogf/gf/contrib/drivers/clickhouse/v2=../../contrib/drivers/clickhouse
+        go mod edit -replace github.com/gogf/gf/contrib/drivers/mssql/v2=../../contrib/drivers/mssql
+        go mod edit -replace github.com/gogf/gf/contrib/drivers/mysql/v2=../../contrib/drivers/mysql
+        go mod edit -replace github.com/gogf/gf/contrib/drivers/oracle/v2=../../contrib/drivers/oracle
+        go mod edit -replace github.com/gogf/gf/contrib/drivers/pgsql/v2=../../contrib/drivers/pgsql
+        go mod edit -replace github.com/gogf/gf/contrib/drivers/sqlite/v2=../../contrib/drivers/sqlite
+    # else
+    #     cd -
+    #     continue 1
+    fi
     go mod tidy
     # Upgrading only GF related libraries, sometimes even if a version number is specified, it may not be possible to successfully upgrade. Please confirm before submitting the code
     go list -f "{{if and (not .Indirect) (not .Main)}}{{.Path}}@${newVersion}{{end}}" -m all | grep "^github.com/gogf/gf"
     go list -f "{{if and (not .Indirect) (not .Main)}}{{.Path}}@${newVersion}{{end}}" -m all | grep "^github.com/gogf/gf" | xargs -L1 go get -v 
     go mod tidy
+    if [ $goModPath = "./cmd/gf" ]; then
+        go mod edit -dropreplace github.com/gogf/gf/v2
+        go mod edit -dropreplace github.com/gogf/gf/contrib/drivers/clickhouse/v2
+        go mod edit -dropreplace github.com/gogf/gf/contrib/drivers/mssql/v2
+        go mod edit -dropreplace github.com/gogf/gf/contrib/drivers/mysql/v2
+        go mod edit -dropreplace github.com/gogf/gf/contrib/drivers/oracle/v2
+        go mod edit -dropreplace github.com/gogf/gf/contrib/drivers/pgsql/v2
+        go mod edit -dropreplace github.com/gogf/gf/contrib/drivers/sqlite/v2
+        mv go.work.version.bak go.work
+    fi
     cd -
 done
 
