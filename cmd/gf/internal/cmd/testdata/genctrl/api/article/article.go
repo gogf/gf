1dbdf149d18fc303fdb8aea7a287f0923f8b3af1
oldme <tyyn1022@gmail.com>
2023-10-25 21:52:40 +0800

    fix gofmt

--- cmd/gf/internal/cmd/testdata/genctrl/api/article/article.go
+++ cmd/gf/internal/cmd/testdata/genctrl/api/article/article.go
@@ -1,12 +1,12 @@
 // =================================================================================
-// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT. 
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
 // =================================================================================
 
 package article
 
 import (
 	"context"
-	
+
 	"github.com/gogf/gf/cmd/gf/v2/internal/cmd/testdata/genctrl/api/article/v1"
 	"github.com/gogf/gf/cmd/gf/v2/internal/cmd/testdata/genctrl/api/article/v2"
 )
@@ -19,5 +19,3 @@ type IArticleV1 interface {
 type IArticleV2 interface {
 	Create(ctx context.Context, req *v2.CreateReq) (res *v2.CreateRes, err error)
 }
-
-
