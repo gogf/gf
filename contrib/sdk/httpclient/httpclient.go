cbea89e6f7fa7e6ac809c11165d1ad35a413b3f8
John Guo <john@johng.cn>
2023-11-02 09:48:39 +0800

    fix duplicated route dump for package ghttp (#3116)

--- contrib/sdk/httpclient/httpclient.go
+++ contrib/sdk/httpclient/httpclient.go
@@ -24,13 +24,13 @@ import (
 	"github.com/gogf/gf/v2/util/gtag"
 )
 
-// Client is a http client for SDK.
+// Client is an http client for SDK.
 type Client struct {
 	*gclient.Client
 	config Config
 }
 
-// New creates and returns a http client for SDK.
+// New creates and returns an http client for SDK.
 func New(config Config) *Client {
 	client := config.Client
 	if client == nil {
