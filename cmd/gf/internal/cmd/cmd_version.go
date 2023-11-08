85feea7210289dda3966db7669920bb0c9c1e141
海亮 <739476267@qq.com>
2023-11-08 11:26:06 +0000

    fix: 重新排版gf -v

--- cmd/gf/internal/cmd/cmd_version.go
+++ cmd/gf/internal/cmd/cmd_version.go
@@ -9,12 +9,16 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"runtime"
+	"strings"
+	"time"
 
 	"github.com/gogf/gf/v2"
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gbuild"
 	"github.com/gogf/gf/v2/os/gfile"
+	"github.com/gogf/gf/v2/os/gproc"
 	"github.com/gogf/gf/v2/text/gregex"
 	"github.com/gogf/gf/v2/text/gstr"
 
@@ -36,32 +40,94 @@ type cVersionInput struct {
 type cVersionOutput struct{}
 
 func (c cVersion) Index(ctx context.Context, in cVersionInput) (*cVersionOutput, error) {
+	var (
+		version   = gf.VERSION
+		gfVersion string
+		envDetail string
+		cliDetail = getBuildDetail()
+		docDetail string
+	)
+
+	goVersion, ok := getGoVersion()
+	if ok {
+		gfVersion = getGoFrameVersion()
+		goVersion = "\n  Go Version: " + goVersion
+	} else {
+		v, err := c.getGFVersionOfCurrentProject()
+		if err == nil {
+			gfVersion = v
+		}
+	}
+
+	envDetail = fmt.Sprintf(`
+Env Detail:
+  CLI Installed At: %s%s
+  GoFrame Version(go.mod): %s
+`, gfile.SelfPath(), goVersion, gfVersion)
+
+	docDetail = fmt.Sprintf(`
+Others Detail:
+  Docs: https://goframe.org
+  Now Time: %s`, time.Now().Format("2006-01-02 15:04:05"))
+
+	mlog.Print(version +
+		envDetail +
+		cliDetail +
+		docDetail)
+
+	return nil, nil
+}
+
+// getGoFrameVersion returns the goframe version of current project using.
+func getGoFrameVersion() (gfVersion string) {
+	pkgInfo, err := gproc.ShellExec(context.Background(), `go list -f "{{if (not .Main)}}{{.Path}}@{{.Version}}{{end}}" -m all`)
+	if err != nil {
+		return ""
+	}
+	pkgList := gstr.Split(pkgInfo, "\n")
+	for _, v := range pkgList {
+		if strings.HasPrefix(v, "github.com/gogf/gf") {
+			gfVersion += fmt.Sprintf("\n    %s", v)
+		}
+	}
+	return
+}
+
+// getGoVersion returns the go version
+func getGoVersion() (goVersion string, ok bool) {
+	goVersion, err := gproc.ShellExec(context.Background(), "go version")
+	if err != nil {
+		return "", false
+	}
+	goVersion = gstr.TrimLeftStr(goVersion, "go version ")
+	goVersion = gstr.TrimRightStr(goVersion, "\n")
+	return goVersion, true
+}
+
+// getBuildDetail returns the build information of current binary.
+func getBuildDetail() (cliDetail string) {
+	cliDetail = fmt.Sprintf(`
+GoFrame CLI Build Detail:`)
+
+	// build info
 	info := gbuild.Info()
 	if info.Git == "" {
 		info.Git = "none"
 	}
-	mlog.Printf(`GoFrame CLI Tool %s, https://goframe.org`, gf.VERSION)
-	gfVersion, err := c.getGFVersionOfCurrentProject()
-	if err != nil {
-		gfVersion = err.Error()
-	} else {
-		gfVersion = gfVersion + " in current go.mod"
-	}
-	mlog.Printf(`GoFrame Version: %s`, gfVersion)
-	mlog.Printf(`CLI Installed At: %s`, gfile.SelfPath())
 	if info.GoFrame == "" {
-		mlog.Print(`Current is a custom installed version, no installation information.`)
-		return nil, nil
+		cliDetail += fmt.Sprintf(`
+  Go Version: %s
+`, runtime.Version())
+		return
 	}
 
-	mlog.Print(gstr.Trim(fmt.Sprintf(`
-CLI Built Detail:
+	cliDetail += fmt.Sprintf(`
   Go Version:  %s
   GF Version:  %s
   Git Commit:  %s
   Build Time:  %s
-`, info.Golang, info.GoFrame, info.Git, info.Time)))
-	return nil, nil
+`, info.Golang, info.GoFrame, info.Git, info.Time)
+	return
 }
 
 // getGFVersionOfCurrentProject checks and returns the GoFrame version current project using.
