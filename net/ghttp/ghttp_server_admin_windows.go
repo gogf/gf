a683156ccbd909fc8bd122b8a843bb18304bb909
zhangyu <yuzhang_cn@qq.com>
2023-11-14 15:47:00 +0800

    fix issue: Windows Platform did not handle process signal

--- net/ghttp/ghttp_server_admin_windows.go
+++ net/ghttp/ghttp_server_admin_windows.go
@@ -9,7 +9,17 @@
 
 package ghttp
 
-// registerSignalHandler does nothing on window platform.
-func handleProcessSignal() {
+import (
+	"github.com/gogf/gf/v2/os/gproc"
+	"os"
+)
 
+// handleProcessSignal handles all signals from system in blocking way.
+func handleProcessSignal() {
+	var ctx = context.TODO()
+	gproc.AddSigHandlerShutdown(func(sig os.Signal) {
+		shutdownWebServersGracefully(ctx, sig)
+	})
+
+	gproc.Listen()
 }
