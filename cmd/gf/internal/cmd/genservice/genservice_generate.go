a0145d1f5f6eb2db778b52964a5d6801596e2f17
loveyfore <loveyfore@163.com>
2024-01-12 09:48:03 +0800

    Fix the issue of significant differences in the generated code every time when executing the 'gf gen service' command.Use gmap.ListMap.

--- cmd/gf/internal/cmd/genservice/genservice_generate.go
+++ cmd/gf/internal/cmd/genservice/genservice_generate.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 
 	"github.com/gogf/gf/v2/container/garray"
+	"github.com/gogf/gf/v2/container/gmap"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gfile"
 	"github.com/gogf/gf/v2/text/gregex"
@@ -23,7 +24,7 @@ import (
 type generateServiceFilesInput struct {
 	CGenServiceInput
 	DstFilePath         string // Absolute file path for generated service go file.
-	SrcStructFunctions  map[string]*garray.StrArray
+	SrcStructFunctions  *gmap.ListMap
 	SrcImportedPackages []string
 	SrcPackageName      string
 	DstPackageName      string
@@ -46,7 +47,8 @@ func (c CGenService) generateServiceFile(in generateServiceFilesInput) (ok bool,
 	// Type definitions.
 	generatedContent += "type("
 	generatedContent += "\n"
-	for structName, funcArray := range in.SrcStructFunctions {
+	in.SrcStructFunctions.Iterator(func(key, value interface{}) bool {
+		structName, funcArray := key.(string), value.(*garray.StrArray)
 		allFuncArray.Append(funcArray.Slice()...)
 		// Add comments to a method.
 		for index, funcName := range funcArray.Slice() {
@@ -60,7 +62,8 @@ func (c CGenService) generateServiceFile(in generateServiceFilesInput) (ok bool,
 			"{FuncDefinition}": funcArray.Join("\n\t"),
 		}))
 		generatedContent += "\n"
-	}
+		return true
+	})
 	generatedContent += ")"
 	generatedContent += "\n"
 
@@ -70,17 +73,19 @@ func (c CGenService) generateServiceFile(in generateServiceFilesInput) (ok bool,
 		generatingInterfaceCheck string
 	)
 	// Variable definitions.
-	for structName := range in.SrcStructFunctions {
+	in.SrcStructFunctions.Iterator(func(key, value interface{}) bool {
+		structName := key.(string)
 		generatingInterfaceCheck = fmt.Sprintf(`[^\w\d]+%s.I%s[^\w\d]`, in.DstPackageName, structName)
 		if gregex.IsMatchString(generatingInterfaceCheck, generatedContent) {
-			continue
+			return true
 		}
 		variableContent += gstr.Trim(gstr.ReplaceByMap(consts.TemplateGenServiceContentVariable, g.MapStrStr{
 			"{StructName}":    structName,
 			"{InterfaceName}": "I" + structName,
 		}))
 		variableContent += "\n"
-	}
+		return true
+	})
 	if variableContent != "" {
 		generatedContent += "var("
 		generatedContent += "\n"
@@ -89,17 +94,19 @@ func (c CGenService) generateServiceFile(in generateServiceFilesInput) (ok bool,
 		generatedContent += "\n"
 	}
 	// Variable register function definitions.
-	for structName := range in.SrcStructFunctions {
+	in.SrcStructFunctions.Iterator(func(key, value interface{}) bool {
+		structName := key.(string)
 		generatingInterfaceCheck = fmt.Sprintf(`[^\w\d]+%s.I%s[^\w\d]`, in.DstPackageName, structName)
 		if gregex.IsMatchString(generatingInterfaceCheck, generatedContent) {
-			continue
+			return true
 		}
 		generatedContent += gstr.Trim(gstr.ReplaceByMap(consts.TemplateGenServiceContentRegister, g.MapStrStr{
 			"{StructName}":    structName,
 			"{InterfaceName}": "I" + structName,
 		}))
 		generatedContent += "\n\n"
-	}
+		return true
+	})
 
 	// Replace empty braces that have new line.
 	generatedContent, _ = gregex.ReplaceString(`{[\s\t]+}`, `{}`, generatedContent)
