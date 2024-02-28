f35c3d04f615fad0a353efeb52b6306787416a92
oldme <tyyn1022@gmail.com>
2024-02-28 14:56:56 +0800

    up

--- contrib/drivers/mysql/mysql_table_fields.go
+++ contrib/drivers/mysql/mysql_table_fields.go
@@ -9,10 +9,9 @@ package mysql
 import (
 	"context"
 	"fmt"
-
-	"github.com/gogf/gf/v2/container/gset"
 	"github.com/gogf/gf/v2/container/gvar"
 	"github.com/gogf/gf/v2/database/gdb"
+	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/text/gstr"
 	"github.com/gogf/gf/v2/util/gutil"
 )
@@ -31,14 +30,53 @@ import (
 // process restarts.
 func (d *Driver) TableFields(ctx context.Context, table string, schema ...string) (fields map[string]*gdb.TableField, err error) {
 	var (
-		result             gdb.Result
-		link               gdb.Link
-		usedSchema         = gutil.GetOrDefaultStr(d.GetSchema(), schema...)
-		mariaJsonFiledName = gset.New(false)
+		link       gdb.Link
+		usedSchema = gutil.GetOrDefaultStr(d.GetSchema(), schema...)
 	)
 	if link, err = d.SlaveLink(usedSchema); err != nil {
 		return nil, err
 	}
+	dbType := d.GetConfig().Type
+	switch dbType {
+	case "mysql":
+		return d.doTableFieldsMysql(ctx, link, table)
+	case "mariadb":
+		return d.doTableFieldsMariadb(ctx, link, table)
+	}
+	return nil, gerror.New("The database type is not mysql or mariadb")
+}
+
+func (d *Driver) doTableFieldsMysql(ctx context.Context, link gdb.Link, table string) (fields map[string]*gdb.TableField, err error) {
+	result, err := d.DoSelect(
+		ctx, link,
+		fmt.Sprintf(`SHOW FULL COLUMNS FROM %s`, d.QuoteWord(table)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	fields = make(map[string]*gdb.TableField)
+	for i, m := range result {
+		fields[m["Field"].String()] = &gdb.TableField{
+			Index:   i,
+			Name:    m["Field"].String(),
+			Type:    m["Type"].String(),
+			Null:    m["Null"].Bool(),
+			Key:     m["Key"].String(),
+			Default: m["Default"].Val(),
+			Extra:   m["Extra"].String(),
+			Comment: m["Comment"].String(),
+		}
+	}
+	return fields, nil
+}
+
+func (d *Driver) doTableFieldsMariadb(ctx context.Context, link gdb.Link, table string) (fields map[string]*gdb.TableField, err error) {
+	var (
+		result                gdb.Result
+		checkConstraintResult gdb.Result
+	)
+
 	result, err = d.DoSelect(
 		ctx, link,
 		fmt.Sprintf(`SHOW FULL COLUMNS FROM %s`, d.QuoteWord(table)),
@@ -47,30 +85,24 @@ func (d *Driver) TableFields(ctx context.Context, table string, schema ...string
 		return nil, err
 	}
 
-	// Compatible with mariaDB json type
-	dbType := d.GetConfig().Type
-	if dbType == "mariadb" {
-		var checkConstraintResult gdb.Result
-		checkConstraintResult, err = d.DoSelect(
-			ctx, link,
-			fmt.Sprintf(`SELECT CONSTRAINT_NAME as filedName, CHECK_CLAUSE as filedCheck FROM information_schema.CHECK_CONSTRAINTS WHERE TABLE_NAME = '%s'`, table),
-		)
-		if err != nil {
-			return nil, err
-		}
-		for _, m := range checkConstraintResult {
-			if gstr.HasPrefix(m["filedCheck"].String(), "json_valid") {
-				mariaJsonFiledName.Add(m["filedName"].String())
-			}
-		}
+	checkConstraintResult, err = d.DoSelect(
+		ctx, link,
+		fmt.Sprintf(`SELECT CONSTRAINT_NAME as filedName, CHECK_CLAUSE as filedCheck FROM information_schema.CHECK_CONSTRAINTS WHERE TABLE_NAME = '%s'`, table),
+	)
+	if err != nil {
+		return nil, err
 	}
+	checkConstraintMap := checkConstraintResult.MapKeyStr("filedName")
 
 	fields = make(map[string]*gdb.TableField)
 	for i, m := range result {
-		// if filed exists in mariaJsonFiledName, replace its Type with "json"
-		if mariaJsonFiledName.Size() != 0 && mariaJsonFiledName.Contains(m["Field"].String()) {
-			m["Type"] = gvar.New("json")
+		// if the field type is json, replace its Type with "json"
+		if v, ok := checkConstraintMap[m["Field"].String()]; ok {
+			if gstr.HasPrefix(v["filedCheck"].(string), "json_valid") {
+				m["Type"] = gvar.New("json")
+			}
 		}
+
 		fields[m["Field"].String()] = &gdb.TableField{
 			Index:   i,
 			Name:    m["Field"].String(),
