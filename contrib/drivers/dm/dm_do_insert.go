199737cd0f3fa8e610d3d3039fd7493296b60c7a
oldme <45782393+oldme-git@users.noreply.github.com>
2024-03-12 20:40:20 +0800

    fix: `for` typo in comments for package `contrib/drivers` and `gdb` (#3366)

--- contrib/drivers/dm/dm_do_insert.go
+++ contrib/drivers/dm/dm_do_insert.go
@@ -20,7 +20,7 @@ import (
 	"github.com/gogf/gf/v2/util/gconv"
 )
 
-// DoInsert inserts or updates data forF given table.
+// DoInsert inserts or updates data for given table.
 func (d *Driver) DoInsert(
 	ctx context.Context, link gdb.Link, table string, list gdb.List, option gdb.DoInsertOption,
 ) (result sql.Result, err error) {
