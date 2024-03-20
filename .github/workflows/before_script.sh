c6457fd3012b3e051ac9b02919a41f381f190cf5
oldme <tyyn1022@gmail.com>
2024-03-20 23:28:00 +0800

    up

--- .github/workflows/before_script.sh
+++ .github/workflows/before_script.sh
@@ -7,14 +7,12 @@ fi
 echo "gofmt checks have passed."
 
 find . -name "*_test.go" -print0 | while IFS= read -r -d '' file; do
-    awk '/func Test[[:upper:]]/ { print $2 }' "$file" | while read -r funcName; do
-        if [[ ! $funcName =~ ^Test[[:upper:]] ]]; then
+    grep -oP 'func Test\K([a-z]+)' "$file" | while read -r funcName; do
+        if [[ $funcName =~ ^[[:upper:]] ]]; then
             echo "Notice: Func name $funcName in file $file checks have failed, please check that it is upper camel case before pr."
         fi
     done
 done
-
 echo "Func name of unit test checks have passed."
-
 
 sudo echo "127.0.0.1   local" | sudo tee -a /etc/hosts
