f55285ac0d43ed940a0943406fd6441ea1f5f010
oldme <tyyn1022@gmail.com>
2024-03-20 17:49:34 +0800

    up

--- .github/workflows/before_script.sh
+++ .github/workflows/before_script.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 find . -name "*.go" | xargs gofmt -w
-git diff --name-only --exit-code || if [ $? != 0 ]; then echo "Notice: gofmt check failed,please gofmt before pr." && exit 1; fi
-echo "gofmt check pass."
+git diff --name-only --exit-code || if [ $? != 0 ];
+    then echo "Notice: gofmt checks have failed, please gofmt before pr." && exit 1;
+fi
+echo "gofmt checks have passed."
 
 find . -name "*_test.go" -print0 | while IFS= read -r -d '' file; do
     awk '/func Test[[:upper:]][[:alnum:]]*\(/ {
         if ($0 !~ /func Test[[:upper:]][[:alnum:]]*\(/) {
-            print "Notice: It checks that the name of the unit test function fails,please check that it is upper camel case before pr"
+            print "Notice: Func name of unit test checks have failed, please check that it is upper camel case before pr"
             exit 1
         }
     }' "$file"
 done
-echo "It checks that the name of the unit test function passes."
+echo "Func name of unit test checks have passed."
 
 sudo echo "127.0.0.1   local" | sudo tee -a /etc/hosts
