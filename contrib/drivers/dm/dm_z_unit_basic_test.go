559e36f549cadd5e2cfe38ae149b34d51d9d0a44
oldme <tyyn1022@gmail.com>
2024-03-20 09:47:15 +0800

    up

--- contrib/drivers/dm/dm_z_unit_basic_test.go
+++ contrib/drivers/dm/dm_z_unit_basic_test.go
@@ -224,33 +224,6 @@ func TestModelInsert(t *testing.T) {
 
 func TestDBInsert(t *testing.T) {
 	table := "A_tables"
-	createInitTable("A_tables")
-	gtest.C(t, func(t *gtest.T) {
-		i := 300
-		data := g.Map{
-			"ID":           i,
-			"ACCOUNT_NAME": fmt.Sprintf(`A%dthress`, i),
-			"PWD_RESET":    3,
-			"ATTR_INDEX":   98,
-		}
-		_, err := db.Insert(ctx, table, &data)
-		gtest.Assert(err, nil)
-	})
-}
-
-func TestDBExec(t *testing.T) {
-	createInitTable("A_tables")
-	gtest.C(t, func(t *gtest.T) {
-		_, err := db.Exec(ctx, "SELECT ? from dual", 1)
-		t.AssertNil(err)
-
-		_, err = db.Exec(ctx, "ERROR")
-		t.AssertNE(err, nil)
-	})
-}
-
-func TestDBInsert(t *testing.T) {
-	table := "A_tables"
 	createInitTable(table)
 	gtest.C(t, func(t *gtest.T) {
 		// normal map
@@ -334,6 +307,17 @@ func TestDBInsert(t *testing.T) {
 		t.AssertNil(err)
 		t.Assert(one["ID"].Int(), 6000)
 		t.Assert(one["ACCOUNT_NAME"].String(), "t6000")
+	})
+}
+
+func TestDBExec(t *testing.T) {
+	createInitTable("A_tables")
+	gtest.C(t, func(t *gtest.T) {
+		_, err := db.Exec(ctx, "SELECT ? from dual", 1)
+		t.AssertNil(err)
+
+		_, err = db.Exec(ctx, "ERROR")
+		t.AssertNE(err, nil)
 	})
 }
 
