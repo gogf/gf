1639f34db6be39d9a5fae51a0f8b9e60795b2644
wln <732437837@qq.com>
2024-04-04 20:51:26 +0800

    up: Update test case data

--- cmd/gf/internal/cmd/testdata/issue/2746/issue_2746.go
+++ cmd/gf/internal/cmd/testdata/issue/2746/issue_2746.go
@@ -10,9 +10,9 @@ import (
 
 // Issue2746 is the golang structure for table issue2746.
 type Issue2746 struct {
-	Id       uint        `json:"ID"       ` // User ID
-	Nickname string      `json:"NICKNAME" ` // User Nickname
-	Tag      *gjson.Json `json:"TAG"      ` //
-	Info     string      `json:"INFO"     ` //
-	Tag2     *gjson.Json `json:"TAG_2"    ` // Tag2
+	Id       uint        `json:"ID"       orm:"id"       ` // User ID
+	Nickname string      `json:"NICKNAME" orm:"nickname" ` // User Nickname
+	Tag      *gjson.Json `json:"TAG"      orm:"tag"      ` //
+	Info     string      `json:"INFO"     orm:"info"     ` //
+	Tag2     *gjson.Json `json:"TAG_2"    orm:"tag2"     ` // Tag2
 }
