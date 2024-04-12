22b2e7853d84b6e2786a0723731efa98463ebb19
wln <732437837@qq.com>
2024-04-12 17:07:19 +0800

    up:

--- os/gproc/gproc_process_newprocess.go
+++ os/gproc/gproc_process_newprocess.go
@@ -4,18 +4,9 @@
 // If a copy of the MIT was not distributed with this file,
 // You can obtain one at https://github.com/gogf/gf.
 
-//go:build windows
+//go:build !windows
 
 package gproc
 
-import (
-	"syscall"
-
-	"github.com/gogf/gf/v2/text/gstr"
-)
-
-// Set the underlying parameters directly on the Windows platform
-func joinProcessArgs(p *Process) {
-	p.SysProcAttr = &syscall.SysProcAttr{}
-	p.SysProcAttr.CmdLine = gstr.Join(p.Args, " ")
-}
+// Do nothing, just set it on the Windows platform
+func joinProcessArgs(p *Process) {}
