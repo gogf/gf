27b1f0c3483b9eb74d083d809a5a2207ac5f0536
wln32 <wln32@foxmail.com>
2024-05-01 19:14:22 +0800

    test version

--- contrib/drivers/mysql/mysql_z_unit_core_test.go
+++ contrib/drivers/mysql/mysql_z_unit_core_test.go
@@ -1622,6 +1622,10 @@ func Test_Types(t *testing.T) {
 			TinyInt   bool
 		}
 		var obj *T
+		t.Log(db.GetConfig())
+		res, err := db.Query(ctx, "select version();")
+		t.AssertNil(err)
+		t.Log("version=", res)
 		err = db.Model("types").Scan(&obj)
 		t.AssertNil(err)
 		t.Assert(obj.Id, 1)
