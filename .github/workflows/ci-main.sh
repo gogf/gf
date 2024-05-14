1e3de90f1acbf3f095003f4c690f4963f6d3c0f4
oldme <tyyn1022@gmail.com>
2024-05-14 14:04:53 +0800

    exit

--- .github/workflows/ci-main.sh
+++ .github/workflows/ci-main.sh
@@ -83,7 +83,7 @@ for file in `find . -name go.mod`; do
     go build ./...
     # check coverage
     if [ "${coverage}" = "coverage" ]; then
-      go test ./... -v -race -coverprofile=coverage.out -covermode=atomic -coverpkg=./...,github.com/gogf/gf/... || exit 1
+      go test ./... -race -coverprofile=coverage.out -covermode=atomic -coverpkg=./...,github.com/gogf/gf/...
 
       if grep -q "/gogf/gf/.*/v2" go.mod; then
         sed -i "s/gogf\/gf\(\/.*\)\/v2/gogf\/gf\/v2\1/g" coverage.out
