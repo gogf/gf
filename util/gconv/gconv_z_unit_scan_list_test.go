568800d8247772f2f810fedafeca57c244890981
oldme <tyyn1022@gmail.com>
2024-05-18 16:59:58 +0800

    up

--- util/gconv/gconv_z_unit_scan_list_test.go
+++ util/gconv/gconv_z_unit_scan_list_test.go
@@ -288,6 +288,58 @@ func TestScanList(t *testing.T) {
 		var a int = 1
 		err = gconv.ScanList(userDetails, &a, "User")
 		t.AssertNE(err, nil)
+	})
+}
 
+func TestScanListErr(t *testing.T) {
+	type EntityUser struct {
+		Uid  int
+		Name string
+	}
+
+	type EntityUserDetail struct {
+		Uid     int
+		Address string
+	}
+
+	type EntityUserScores struct {
+		Id    int
+		Uid   int
+		Score int
+	}
+
+	gtest.C(t, func(t *gtest.T) {
+		type Entity struct {
+			User       EntityUser
+			UserDetail EntityUserDetail
+			UserScores []EntityUserScores
+		}
+
+		var (
+			err         error
+			entities    []Entity
+			entityUsers = []EntityUser{
+				{Uid: 1, Name: "name1"},
+				{Uid: 2, Name: "name2"},
+				{Uid: 3, Name: "name3"},
+			}
+			userDetails = []EntityUserDetail{
+				{Uid: 1, Address: "address1"},
+				{Uid: 2, Address: "address2"},
+			}
+			userScores = []EntityUserScores{
+				{Id: 10, Uid: 1, Score: 100},
+				{Id: 11, Uid: 1, Score: 60},
+				{Id: 20, Uid: 2, Score: 99},
+			}
+		)
+		err = gconv.ScanList(entityUsers, &entities, "User")
+		t.AssertNil(err)
+
+		err = gconv.ScanList(userDetails, &entities, "UserDetail", "User", "uuid")
+		t.AssertNE(err, nil)
+
+		err = gconv.ScanList(userScores, &entities, "UserScores", "User", "uid")
+		t.AssertNil(err)
 	})
 }
