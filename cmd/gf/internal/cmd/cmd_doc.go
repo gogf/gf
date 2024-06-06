f14fb4856b22440432e6e308b3007ac871ab6029
hailaz <739476267@qq.com>
2024-06-06 18:33:58 +0800

    fix: 增加本地文档参数

--- cmd/gf/internal/cmd/cmd_doc.go
+++ cmd/gf/internal/cmd/cmd_doc.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 
 	"github.com/gogf/gf/cmd/gf/v2/internal/utility/mlog"
 	"github.com/gogf/gf/v2/encoding/gcompress"
@@ -29,18 +30,44 @@ type cDoc struct {
 
 type cDocInput struct {
 	g.Meta `name:"doc"`
+	Path   string `short:"p"  name:"path"    brief:"download docs directory path, default is \"%temp%/goframe\""`
+	Port   int    `short:"o"  name:"port"    brief:"http server port, default is 8080" d:"8080"`
+	Update bool   `short:"u"  name:"update"  brief:"clean docs directory and update docs"`
+	Clean  bool   `short:"c"  name:"clean"   brief:"clean docs directory"`
 }
 
 type cDocOutput struct{}
 
 func (c cDoc) Index(ctx context.Context, in cDocInput) (out *cDocOutput, err error) {
-
-	docDir := NewDocSetting().DownloadDoc()
-	mlog.Print("文档目录:", docDir)
+	docs := NewDocSetting(in.Path)
+	mlog.Print("下载文档所在目录:", docs.DocDir)
+	if in.Clean {
+		mlog.Print("清理文档目录")
+		err = docs.Clean()
+		if err != nil {
+			mlog.Print("清理文档目录失败:", err)
+			return
+		}
+		return
+	}
+	if in.Update {
+		mlog.Print("清理旧文档目录")
+		err = docs.Clean()
+		if err != nil {
+			mlog.Print("清理旧文档目录失败:", err)
+			return
+		}
+	}
+	err = docs.DownloadDoc()
+	if err != nil {
+		mlog.Print("下载文档失败:", err)
+		return
+	}
 	s := g.Server()
-	s.SetServerRoot(docDir)
-	s.SetPort(8199)
-	mlog.Print("http://127.0.0.1:8199")
+	s.SetServerRoot(docs.DocDir)
+	s.SetPort(in.Port)
+	s.SetDumpRouterMap(false)
+	mlog.Printf("访问地址 http://127.0.0.1:%d", in.Port)
 	s.Run()
 	return
 }
@@ -56,30 +83,45 @@ type DocSetting struct {
 // NewDocSetting description
 //
 // createTime: 2024-05-14 12:19:55
-func NewDocSetting() *DocSetting {
+func NewDocSetting(tempDir string) *DocSetting {
 	fileName := "gf-doc-md.zip"
-	tempDir := gfile.Temp("goframe")
-	if _, err := os.Stat(tempDir); err != nil {
-		err = gfile.Mkdir(tempDir)
+	if tempDir == "" {
+		tempDir = gfile.Temp("goframe/docs")
+	} else {
+		tempDir = gfile.Abs(path.Join(tempDir, "docs"))
+	}
+
+	return &DocSetting{
+		TempDir:    filepath.FromSlash(tempDir),
+		DocDir:     filepath.FromSlash(path.Join(tempDir, "gf-gh-pages")),
+		DocURL:     "https://codeload.github.com/gogf/gf/zip/refs/heads/gh-pages",
+		DocZipFile: filepath.FromSlash(path.Join(tempDir, fileName)),
+	}
+}
+
+func (d *DocSetting) Clean() error {
+	if _, err := os.Stat(d.TempDir); err == nil {
+		err = gfile.Remove(d.TempDir)
+		if err != nil {
+			mlog.Print("删除临时目录失败:", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func (d *DocSetting) DownloadDoc() error {
+	if _, err := os.Stat(d.TempDir); err != nil {
+		err = gfile.Mkdir(d.TempDir)
 		if err != nil {
 			mlog.Print("创建临时目录失败:", err)
 			return nil
 		}
 	}
-	return &DocSetting{
-		TempDir:    tempDir,
-		DocDir:     path.Join(tempDir, "gf-gh-pages"),
-		DocURL:     "https://codeload.github.com/gogf/gf/zip/refs/heads/gh-pages",
-		DocZipFile: path.Join(tempDir, fileName),
-	}
-}
-
-func (d *DocSetting) DownloadDoc() (docDir string) {
-	docDir = d.DocDir
 	// 判断文件是否存在
-	if _, err := os.Stat(docDir); err == nil {
-		mlog.Print("目录已存在，无需下载解压缩")
-		return
+	if _, err := os.Stat(d.DocDir); err == nil {
+		mlog.Print("文档已存在，无需下载解压缩")
+		return nil
 	}
 
 	if _, err := os.Stat(d.DocZipFile); err == nil {
@@ -90,7 +132,7 @@ func (d *DocSetting) DownloadDoc() (docDir string) {
 		resp, err := http.Get(d.DocURL)
 		if err != nil {
 			mlog.Print("下载文件失败:", err)
-			return
+			return err
 		}
 		defer resp.Body.Close()
 
@@ -98,7 +140,7 @@ func (d *DocSetting) DownloadDoc() (docDir string) {
 		out, err := os.Create(d.DocZipFile)
 		if err != nil {
 			mlog.Print("创建文件失败:", err)
-			return
+			return err
 		}
 		defer out.Close()
 
@@ -106,7 +148,7 @@ func (d *DocSetting) DownloadDoc() (docDir string) {
 		_, err = io.Copy(out, resp.Body)
 		if err != nil {
 			mlog.Print("写入文件失败:", err)
-			return
+			return err
 		}
 	}
 
@@ -114,10 +156,11 @@ func (d *DocSetting) DownloadDoc() (docDir string) {
 	// 解压缩文件
 	err := gcompress.UnZipFile(d.DocZipFile, d.TempDir)
 	if err != nil {
-		mlog.Print("解压缩文件失败:", err)
-		return
+		mlog.Print("解压缩文件失败，请重新运行:", err)
+		gfile.Remove(d.DocZipFile)
+		return err
 	}
 
 	mlog.Print("下载并解压缩成功")
-	return
+	return nil
 }
