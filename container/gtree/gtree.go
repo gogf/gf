988062e58d63869b97256950fdb409ae9d4317e4
oldme <tyyn1022@gmail.com>
2024-06-25 22:33:12 +0800

    up

--- container/gtree/gtree.go
+++ container/gtree/gtree.go
@@ -11,8 +11,8 @@ package gtree
 
 import "github.com/gogf/gf/v2/container/gvar"
 
-// Gtree defines the interface for basic operations of a tree.
-type Gtree interface {
+// Tree defines the interface for basic operations of a tree.
+type Tree interface {
 	// Set inserts node into the tree.
 	Set(key interface{}, value interface{})
 
