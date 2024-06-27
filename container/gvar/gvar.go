c3cfc01cf6ab2fc6b252b870d51da4e5b0aecd30
John Guo <john@johng.cn>
2024-06-27 21:12:51 +0800

    refactor(container/gring): mark deprecated, package `container/gring` will not be maintained in future (#3665)

--- container/gvar/gvar.go
+++ container/gvar/gvar.go
@@ -4,7 +4,7 @@
 // If a copy of the MIT was not distributed with this file,
 // You can obtain one at https://github.com/gogf/gf.
 
-// Package gvar provides an universal variable type, like generics.
+// Package gvar provides an universal variable type, like runtime generics.
 package gvar
 
 import (
