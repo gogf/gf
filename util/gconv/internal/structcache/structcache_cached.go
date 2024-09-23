f1b39af4bd9864f270973ac1a0af184ba9d3deb9
wln32 <wln32@foxmail.com>
2024-09-24 01:15:29 +0800

    1.更新OtherSameNameFieldIndex的类型从[][]int为CachedFieldInfo 2.补充测试issue#3797，以及嵌套结构体同名不同类型字段 3.过滤掉gmeta.Meta类型的字段 4.对于匿名结构体字段不带tag的，不再缓存 5.增加一些工具函数

--- util/gconv/internal/structcache/structcache_cached.go
+++ util/gconv/internal/structcache/structcache_cached.go
@@ -115,16 +115,11 @@ func parseStruct(
 		if !utils.IsLetterUpper(fieldName[0]) {
 			continue
 		}
-
+		if fieldType.String() == "gmeta.Meta" {
+			continue
+		}
 		copyFieldIndexes := make([]int, len(fieldIndexes))
 		copy(copyFieldIndexes, fieldIndexes)
-
-		// Do not directly use append(fieldIndexes, i)
-		// When the structure is nested deeply, it may lead to bugs,
-		// which are caused by the slice expansion mechanism
-		// So it is necessary to allocate a separate index for each field
-		// See details https://github.com/gogf/gf/issues/3789
-		structInfo.AddField(structField, append(copyFieldIndexes, i), priorityTagArray)
 
 		// normal basic attributes.
 		if structField.Anonymous {
@@ -136,9 +131,17 @@ func parseStruct(
 				continue
 			}
 			if structField.Tag != "" {
-				// TODO: If it's an anonymous field with a tag, doesn't it need to be recursive?
+				// Do not add anonymous structures without tags
+				structInfo.AddField(structField, append(copyFieldIndexes, i), priorityTagArray)
 			}
 			parseStruct(fieldType, append(copyFieldIndexes, i), structInfo, priorityTagArray)
+			continue
 		}
+		// Do not directly use append(fieldIndexes, i)
+		// When the structure is nested deeply, it may lead to bugs,
+		// which are caused by the slice expansion mechanism
+		// So it is necessary to allocate a separate index for each field
+		// See details https://github.com/gogf/gf/issues/3789
+		structInfo.AddField(structField, append(copyFieldIndexes, i), priorityTagArray)
 	}
 }
