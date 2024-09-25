bdd296d1c6f78141374b59a0a7464702d5276f88
wln32 <wln32@foxmail.com>
2024-09-25 18:59:27 +0800

    忽略字段数为0的空结构体

--- util/gconv/internal/structcache/structcache_cached.go
+++ util/gconv/internal/structcache/structcache_cached.go
@@ -121,17 +121,15 @@ func parseStructToCachedStructInfo(
 
 		// normal basic attributes.
 		if structField.Anonymous {
-			isStructPtr := false
 			// handle struct attributes, it might be struct/*struct embedded..
 			if fieldType.Kind() == reflect.Ptr {
 				fieldType = fieldType.Elem()
-				isStructPtr = true
 			}
 			if fieldType.Kind() != reflect.Struct {
 				continue
 			}
-			// Skip the embedded structure of the value type of the 0 field,
-			if isStructPtr == false && fieldType.NumField() == 0 {
+			// Skip the embedded structure of the 0 field,
+			if fieldType.NumField() == 0 {
 				continue
 			}
 			if structField.Tag != "" {
