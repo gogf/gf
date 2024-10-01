210c7d058bb98cdd1d8ada2fdd0bdeddd1d392a9
oldme <tyyn1022@gmail.com>
2024-10-01 11:52:11 +0800

    fix(cmd/gf): gf gen service error when the package version number at the end

---
+++ cmd/gf/internal/cmd/testdata/issue/3835/logic/logic.go
@@ -0,0 +1,9 @@
+// ==========================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// ==========================================================================
+
+package logic
+
+import (
+	_ "github.com/gogf/gf/cmd/gf/v2/internal/cmd/testdata/issue/3835/logic/issue3835"
+)
