2f6e4b865814974ba3125de1200865d6b721815a
wangweifeng <952529160@qq.com>
2024-10-15 11:49:38 +0800

    pgsql insert or ignore support

--- contrib/drivers/pgsql/pgsql_do_insert.go
+++ contrib/drivers/pgsql/pgsql_do_insert.go
@@ -9,10 +9,11 @@ package pgsql
 import (
 	"context"
 	"database/sql"
-
+	"fmt"
 	"github.com/gogf/gf/v2/database/gdb"
 	"github.com/gogf/gf/v2/errors/gcode"
 	"github.com/gogf/gf/v2/errors/gerror"
+	"strings"
 )
 
 // DoInsert inserts or updates data for given table.
@@ -25,10 +26,7 @@ func (d *Driver) DoInsert(ctx context.Context, link gdb.Link, table string, list
 		)
 
 	case gdb.InsertOptionIgnore:
-		return nil, gerror.NewCode(
-			gcode.CodeNotSupported,
-			`Insert ignore operation is not supported by pgsql driver`,
-		)
+		return d.doInsertIgnore(ctx, link, table, list)
 
 	case gdb.InsertOptionDefault:
 		tableFields, err := d.GetCore().GetDB().TableFields(ctx, table)
@@ -43,4 +41,40 @@ func (d *Driver) DoInsert(ctx context.Context, link gdb.Link, table string, list
 		}
 	}
 	return d.Core.DoInsert(ctx, link, table, list, option)
+}
+
+// doInsertIgnore inserts a list of records into the specified table in pgsql.
+// INSERT INTO <table> (<columns>) VALUES (<values>) ON CONFLICT DO NOTHING
+func (d *Driver) doInsertIgnore(ctx context.Context, link gdb.Link, table string, list gdb.List) (result sql.Result, err error) {
+	if len(list) == 0 {
+		return nil, gerror.NewCode(gcode.CodeInvalidRequest, `Insert operation list is empty for pgsql driver`)
+	}
+
+	var (
+		one          = list[0]
+		charL, charR = d.GetChars()
+		insertKeys   = make([]string, 0, len(one))
+		insertValues = make([]string, 0, len(one))
+		queryValues  = make([]interface{}, 0, len(one))
+	)
+
+	for key, value := range one {
+		insertKeys = append(insertKeys, charL+key+charR)
+		insertValues = append(insertValues, "?")
+		queryValues = append(queryValues, value)
+	}
+
+	sqlStr := fmt.Sprintf(
+		`INSERT INTO %s (%s) VALUES (%s) ON CONFLICT DO NOTHING`,
+		table,
+		strings.Join(insertKeys, ","),
+		strings.Join(insertValues, ","),
+	)
+
+	result, err = d.DoExec(ctx, link, sqlStr, queryValues...)
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
 }
