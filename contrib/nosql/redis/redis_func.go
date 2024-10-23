cyjaysong/goframe

gredis Option 易用性增强

contrib/nosql/redis/redis_func.go
@@ -7,81 +7,16 @@
 package redis
 
 import (
-	"reflect"
-
-	"github.com/gogf/gf/v2/os/gstructs"
+	"github.com/gogf/gf/v2/frame/g"
 )
 
-func mustMergeOptionToArgs(args []interface{}, option interface{}) []interface{} {
-	if option == nil {
-		return args
-	}
-	var (
-		err        error
-		optionArgs []interface{}
-	)
-	optionArgs, err = convertOptionToArgs(option)
-	if err != nil {
-		panic(err)
-	}
-	return append(args, optionArgs...)
+type Option interface {
+	OptionToArgs() []interface{}
 }
 
-func convertOptionToArgs(option interface{}) ([]interface{}, error) {
-	if option == nil {
-		return nil, nil
+func mustMergeOptionToArgs(args []interface{}, opt Option) []interface{} {
+	if g.IsNil(opt) {
+		return args
 	}
-	var (
-		err       error
-		args      = make([]interface{}, 0)
-		fields    []gstructs.Field
-		subFields []gstructs.Field
-	)
-	fields, err = gstructs.Fields(gstructs.FieldsInput{
-		Pointer:         option,
-		RecursiveOption: gstructs.RecursiveOptionEmbeddedNoTag,
-	})
-	if err != nil {
-		return nil, err
-	}
-	for _, field := range fields {
-		switch field.OriginalKind() {
-		// See SetOption
-		case reflect.Bool:
-			if field.Value.Bool() {
-				args = append(args, field.Name())
-			}
-
-		// See ZRangeOption
-		case reflect.Struct:
-			if field.Value.IsNil() {
-				continue
-			}
-			if !field.IsEmbedded() {
-				args = append(args, field.Name())
-			}
-			subFields, err = gstructs.Fields(gstructs.FieldsInput{
-				Pointer:         option,
-				RecursiveOption: gstructs.RecursiveOptionEmbeddedNoTag,
-			})
-			if err != nil {
-				return nil, err
-			}
-			for _, subField := range subFields {
-				args = append(args, subField.Value.Interface())
-			}
-
-		// See TTLOption
-		default:
-			fieldValue := field.Value.Interface()
-			if field.Value.Kind() == reflect.Ptr {
-				if field.Value.IsNil() {
-					continue
-				}
-				fieldValue = field.Value.Elem().Interface()
-			}
-			args = append(args, field.Name(), fieldValue)
-		}
-	}
-	return args, nil
+	return append(args, opt.OptionToArgs()...)
 }

contrib/nosql/redis/redis_group_generic.go
@@ -41,7 +41,7 @@ func (r *Redis) GroupGeneric() gredis.IGroupGeneric {
 //
 // https://redis.io/commands/copy/
 func (r GroupGeneric) Copy(ctx context.Context, source, destination string, option ...gredis.CopyOption) (int64, error) {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -179,11 +179,10 @@ func (r GroupGeneric) Keys(ctx context.Context, pattern string) ([]string, error
 //
 // https://redis.io/commands/scan/
 func (r GroupGeneric) Scan(ctx context.Context, cursor uint64, option ...gredis.ScanOption) (uint64, []string, error) {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
-		usedOption = option[0].ToUsedOption()
+		usedOption = option[0]
 	}
-
 	v, err := r.Operation.Do(ctx, "Scan", mustMergeOptionToArgs(
 		[]interface{}{cursor}, usedOption,
 	)...)
@@ -231,7 +230,7 @@ func (r GroupGeneric) FlushAll(ctx context.Context, option ...gredis.FlushOp) er
 //
 // https://redis.io/commands/expire/
 func (r GroupGeneric) Expire(ctx context.Context, key string, seconds int64, option ...gredis.ExpireOption) (int64, error) {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -252,7 +251,7 @@ func (r GroupGeneric) Expire(ctx context.Context, key string, seconds int64, opt
 //
 // https://redis.io/commands/expireat/
 func (r GroupGeneric) ExpireAt(ctx context.Context, key string, time time.Time, option ...gredis.ExpireOption) (int64, error) {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -316,7 +315,7 @@ func (r GroupGeneric) Persist(ctx context.Context, key string) (int64, error) {
 //
 // https://redis.io/commands/pexpire/
 func (r GroupGeneric) PExpire(ctx context.Context, key string, milliseconds int64, option ...gredis.ExpireOption) (int64, error) {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -331,7 +330,7 @@ func (r GroupGeneric) PExpire(ctx context.Context, key string, milliseconds int6
 //
 // https://redis.io/commands/pexpireat/
 func (r GroupGeneric) PExpireAt(ctx context.Context, key string, time time.Time, option ...gredis.ExpireOption) (int64, error) {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
 		usedOption = option[0]
 	}

contrib/nosql/redis/redis_group_script.go
@@ -93,7 +93,7 @@ func (r GroupScript) ScriptExists(ctx context.Context, sha1 string, sha1s ...str
 //
 // https://redis.io/commands/script-flush/
 func (r GroupScript) ScriptFlush(ctx context.Context, option ...gredis.ScriptFlushOption) error {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
 		usedOption = option[0]
 	}

contrib/nosql/redis/redis_group_sorted_set.go
@@ -120,7 +120,7 @@ func (r GroupSortedSet) ZCount(ctx context.Context, key string, min, max string)
 //
 // https://redis.io/commands/zrange/
 func (r GroupSortedSet) ZRange(ctx context.Context, key string, start, stop int64, option ...gredis.ZRangeOption) (gvar.Vars, error) {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -140,7 +140,7 @@ func (r GroupSortedSet) ZRange(ctx context.Context, key string, start, stop int6
 //
 // https://redis.io/commands/zrevrange/
 func (r GroupSortedSet) ZRevRange(ctx context.Context, key string, start, stop int64, option ...gredis.ZRevRangeOption) (*gvar.Var, error) {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
 		usedOption = option[0]
 	}

contrib/nosql/redis/redis_group_string.go
@@ -32,7 +32,7 @@ func (r *Redis) GroupString() gredis.IGroupString {
 //
 // https://redis.io/commands/set/
 func (r GroupString) Set(ctx context.Context, key string, value interface{}, option ...gredis.SetOption) (*gvar.Var, error) {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -52,7 +52,7 @@ func (r GroupString) Set(ctx context.Context, key string, value interface{}, opt
 //
 // https://redis.io/commands/setnx/
 func (r GroupString) SetNX(ctx context.Context, key string, value interface{}) (bool, error) {
-	v, err := r.Operation.Do(ctx, "SetNX", key, value)
+	v, err := r.Operation.Do(ctx, "Set", key, value, "NX")
 	return v.Bool(), err
 }
 
@@ -70,7 +70,7 @@ func (r GroupString) SetNX(ctx context.Context, key string, value interface{}) (
 //
 // https://redis.io/commands/setex/
 func (r GroupString) SetEX(ctx context.Context, key string, value interface{}, ttlInSeconds int64) error {
-	_, err := r.Operation.Do(ctx, "SetEX", key, ttlInSeconds, value)
+	_, err := r.Operation.Do(ctx, "Set", key, value, "EX", ttlInSeconds)
 	return err
 }
 
@@ -95,7 +95,7 @@ func (r GroupString) GetDel(ctx context.Context, key string) (*gvar.Var, error) 
 //
 // https://redis.io/commands/getex/
 func (r GroupString) GetEX(ctx context.Context, key string, option ...gredis.GetEXOption) (*gvar.Var, error) {
-	var usedOption interface{}
+	var usedOption Option
 	if len(option) > 0 {
 		usedOption = option[0]
 	}

gogf/gf

contrib/nosql/redis/redis_func.go
@@ -7,81 +7,16 @@
 package redis
 
 import (
-	"reflect"
-
-	"github.com/gogf/gf/v2/os/gstructs"
+	"github.com/gogf/gf/v2/frame/g"
 )
 
-func mustMergeOptionToArgs(args []interface{}, option interface{}) []interface{} {
-	if option == nil {
-		return args
-	}
-	var (
-		err        error
-		optionArgs []interface{}
-	)
-	optionArgs, err = convertOptionToArgs(option)
-	if err != nil {
-		panic(err)
-	}
-	return append(args, optionArgs...)
+type redisOption interface {
+	OptionToArgs() []interface{}
 }
 
-func convertOptionToArgs(option interface{}) ([]interface{}, error) {
-	if option == nil {
-		return nil, nil
+func mustMergeOptionToArgs(args []interface{}, opt redisOption) []interface{} {
+	if g.IsNil(opt) {
+		return args
 	}
-	var (
-		err       error
-		args      = make([]interface{}, 0)
-		fields    []gstructs.Field
-		subFields []gstructs.Field
-	)
-	fields, err = gstructs.Fields(gstructs.FieldsInput{
-		Pointer:         option,
-		RecursiveOption: gstructs.RecursiveOptionEmbeddedNoTag,
-	})
-	if err != nil {
-		return nil, err
-	}
-	for _, field := range fields {
-		switch field.OriginalKind() {
-		// See SetOption
-		case reflect.Bool:
-			if field.Value.Bool() {
-				args = append(args, field.Name())
-			}
-
-		// See ZRangeOption
-		case reflect.Struct:
-			if field.Value.IsNil() {
-				continue
-			}
-			if !field.IsEmbedded() {
-				args = append(args, field.Name())
-			}
-			subFields, err = gstructs.Fields(gstructs.FieldsInput{
-				Pointer:         option,
-				RecursiveOption: gstructs.RecursiveOptionEmbeddedNoTag,
-			})
-			if err != nil {
-				return nil, err
-			}
-			for _, subField := range subFields {
-				args = append(args, subField.Value.Interface())
-			}
-
-		// See TTLOption
-		default:
-			fieldValue := field.Value.Interface()
-			if field.Value.Kind() == reflect.Ptr {
-				if field.Value.IsNil() {
-					continue
-				}
-				fieldValue = field.Value.Elem().Interface()
-			}
-			args = append(args, field.Name(), fieldValue)
-		}
-	}
-	return args, nil
+	return append(args, opt.OptionToArgs()...)
 }

contrib/nosql/redis/redis_group_generic.go
@@ -41,7 +41,7 @@ func (r *Redis) GroupGeneric() gredis.IGroupGeneric {
 //
 // https://redis.io/commands/copy/
 func (r GroupGeneric) Copy(ctx context.Context, source, destination string, option ...gredis.CopyOption) (int64, error) {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -179,11 +179,10 @@ func (r GroupGeneric) Keys(ctx context.Context, pattern string) ([]string, error
 //
 // https://redis.io/commands/scan/
 func (r GroupGeneric) Scan(ctx context.Context, cursor uint64, option ...gredis.ScanOption) (uint64, []string, error) {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
-		usedOption = option[0].ToUsedOption()
+		usedOption = option[0]
 	}
-
 	v, err := r.Operation.Do(ctx, "Scan", mustMergeOptionToArgs(
 		[]interface{}{cursor}, usedOption,
 	)...)
@@ -231,7 +230,7 @@ func (r GroupGeneric) FlushAll(ctx context.Context, option ...gredis.FlushOp) er
 //
 // https://redis.io/commands/expire/
 func (r GroupGeneric) Expire(ctx context.Context, key string, seconds int64, option ...gredis.ExpireOption) (int64, error) {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -252,7 +251,7 @@ func (r GroupGeneric) Expire(ctx context.Context, key string, seconds int64, opt
 //
 // https://redis.io/commands/expireat/
 func (r GroupGeneric) ExpireAt(ctx context.Context, key string, time time.Time, option ...gredis.ExpireOption) (int64, error) {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -316,7 +315,7 @@ func (r GroupGeneric) Persist(ctx context.Context, key string) (int64, error) {
 //
 // https://redis.io/commands/pexpire/
 func (r GroupGeneric) PExpire(ctx context.Context, key string, milliseconds int64, option ...gredis.ExpireOption) (int64, error) {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -331,7 +330,7 @@ func (r GroupGeneric) PExpire(ctx context.Context, key string, milliseconds int6
 //
 // https://redis.io/commands/pexpireat/
 func (r GroupGeneric) PExpireAt(ctx context.Context, key string, time time.Time, option ...gredis.ExpireOption) (int64, error) {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
 		usedOption = option[0]
 	}

contrib/nosql/redis/redis_group_script.go
@@ -93,7 +93,7 @@ func (r GroupScript) ScriptExists(ctx context.Context, sha1 string, sha1s ...str
 //
 // https://redis.io/commands/script-flush/
 func (r GroupScript) ScriptFlush(ctx context.Context, option ...gredis.ScriptFlushOption) error {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
 		usedOption = option[0]
 	}

contrib/nosql/redis/redis_group_sorted_set.go
@@ -120,7 +120,7 @@ func (r GroupSortedSet) ZCount(ctx context.Context, key string, min, max string)
 //
 // https://redis.io/commands/zrange/
 func (r GroupSortedSet) ZRange(ctx context.Context, key string, start, stop int64, option ...gredis.ZRangeOption) (gvar.Vars, error) {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -140,7 +140,7 @@ func (r GroupSortedSet) ZRange(ctx context.Context, key string, start, stop int6
 //
 // https://redis.io/commands/zrevrange/
 func (r GroupSortedSet) ZRevRange(ctx context.Context, key string, start, stop int64, option ...gredis.ZRevRangeOption) (*gvar.Var, error) {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
 		usedOption = option[0]
 	}

contrib/nosql/redis/redis_group_string.go
@@ -32,7 +32,7 @@ func (r *Redis) GroupString() gredis.IGroupString {
 //
 // https://redis.io/commands/set/
 func (r GroupString) Set(ctx context.Context, key string, value interface{}, option ...gredis.SetOption) (*gvar.Var, error) {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
 		usedOption = option[0]
 	}
@@ -52,7 +52,7 @@ func (r GroupString) Set(ctx context.Context, key string, value interface{}, opt
 //
 // https://redis.io/commands/setnx/
 func (r GroupString) SetNX(ctx context.Context, key string, value interface{}) (bool, error) {
-	v, err := r.Operation.Do(ctx, "SetNX", key, value)
+	v, err := r.Operation.Do(ctx, "Set", key, value, "NX")
 	return v.Bool(), err
 }
 
@@ -70,7 +70,7 @@ func (r GroupString) SetNX(ctx context.Context, key string, value interface{}) (
 //
 // https://redis.io/commands/setex/
 func (r GroupString) SetEX(ctx context.Context, key string, value interface{}, ttlInSeconds int64) error {
-	_, err := r.Operation.Do(ctx, "SetEX", key, ttlInSeconds, value)
+	_, err := r.Operation.Do(ctx, "Set", key, value, "EX", ttlInSeconds)
 	return err
 }
 
@@ -95,7 +95,7 @@ func (r GroupString) GetDel(ctx context.Context, key string) (*gvar.Var, error) 
 //
 // https://redis.io/commands/getex/
 func (r GroupString) GetEX(ctx context.Context, key string, option ...gredis.GetEXOption) (*gvar.Var, error) {
-	var usedOption interface{}
+	var usedOption redisOption
 	if len(option) > 0 {
 		usedOption = option[0]
 	}