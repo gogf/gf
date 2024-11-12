28cfb55681c065cd2ca2bd52091cba1528f21f3d
ShiZili <okyer@aliyun.com>
2024-11-12 09:45:29 +0800

    feat: add gaussdb driver

---
+++ contrib/drivers/gaussdb/gaussdb_result.go
@@ -0,0 +1,24 @@
+// Copyright GoFrame Author(https://goframe.org). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
+package gaussdb
+
+import "database/sql"
+
+type Result struct {
+	sql.Result
+	affected          int64
+	lastInsertId      int64
+	lastInsertIdError error
+}
+
+func (pgr Result) RowsAffected() (int64, error) {
+	return pgr.affected, nil
+}
+
+func (pgr Result) LastInsertId() (int64, error) {
+	return pgr.lastInsertId, pgr.lastInsertIdError
+}
