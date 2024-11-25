9755bcc6d8c691c5bf94e06d3d5d91ad5f68c16e
longxiangqiao <longxiangqiao@qq.com>
2024-11-25 23:55:37 +0800

    chore: fix some function names in comment
    Signed-off-by: longxiangqiao <longxiangqiao@qq.com>

--- util/grand/grand_buffer.go
+++ util/grand/grand_buffer.go
@@ -28,7 +28,7 @@ func init() {
 	go asyncProducingRandomBufferBytesLoop()
 }
 
-// asyncProducingRandomBufferBytes is a named goroutine, which uses an asynchronous goroutine
+// asyncProducingRandomBufferBytesLoop is a named goroutine, which uses an asynchronous goroutine
 // to produce the random bytes, and a buffer chan to store the random bytes.
 // So it has high performance to generate random numbers.
 func asyncProducingRandomBufferBytesLoop() {
