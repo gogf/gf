93a3169f239f8a3801e4f9ea9eeb0254bbe53d79
wanna <wanna.w@binarywalk.com>
2024-12-02 11:47:09 +0800

    add test for issue 3953

--- cmd/gf/internal/cmd/cmd_z_unit_gen_pb_test.go
+++ cmd/gf/internal/cmd/cmd_z_unit_gen_pb_test.go
@@ -48,3 +48,42 @@ func TestGenPbIssue3882(t *testing.T) {
 		t.Assert(gstr.Contains(genContent, exceptText), true)
 	})
 }
+
+// This issue only occurs when executing multiple times
+// and the subsequent OutputApi is the parent directory of the previous execution
+func TestGenPbIssue3953(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		var (
+			outputPath     = gfile.Temp(guid.S())
+			outputApiPath  = filepath.Join(outputPath, "api")
+			outputCtrlPath = filepath.Join(outputPath, "controller")
+
+			protobufFolder = gtest.DataPath("issue", "3953")
+			in             = genpb.CGenPbInput{
+				Path:       protobufFolder,
+				OutputApi:  outputApiPath,
+				OutputCtrl: outputCtrlPath,
+			}
+			err error
+		)
+		err = gfile.Mkdir(outputApiPath)
+		t.AssertNil(err)
+		err = gfile.Mkdir(outputCtrlPath)
+		t.AssertNil(err)
+		defer gfile.Remove(outputPath)
+
+		_, err = genpb.CGenPb{}.Pb(ctx, in)
+		// do twice,and set outputApi to outputPath
+		in.OutputApi = outputPath
+		_, err = genpb.CGenPb{}.Pb(ctx, in)
+		t.AssertNil(err)
+
+		var (
+			genContent = gfile.GetContents(filepath.Join(outputApiPath, "issue3953.pb.go"))
+			// The old version would have appeared `v:"required" v:"required"`
+			// but the new version of the code will appear `v:"required"` only once
+			notExceptText = `v:"required" v:"required"`
+		)
+		t.Assert(gstr.Contains(genContent, notExceptText), false)
+	})
+}
