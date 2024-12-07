02227163d2dfda9e45ec902e3a2ddb184e89503e
houseme <housemecn@gmail.com>
2024-12-08 01:24:04 +0800

    test

--- contrib/registry/consul/consul.go
+++ contrib/registry/consul/consul.go
@@ -13,10 +13,9 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/hashicorp/consul/api"
-
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/net/gsvc"
+	"github.com/hashicorp/consul/api"
 )
 
 const (
