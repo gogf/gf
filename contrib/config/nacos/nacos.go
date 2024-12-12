db16507f97a2567bc0e06d77ce503bd7e55d4dec
shanyujie <1196661499@qq.com>
2024-12-12 11:46:28 +0800

    [lanceadd] 1.Allows custom actions after a remote profile update 2.The two PRs #4025 and #4029 made 'v.17.0'->'v.18.0'->'v1.17.0' to the version number of 'github.com/fatih/color', it seems that 'v.18.0' should be used

--- contrib/config/nacos/nacos.go
+++ contrib/config/nacos/nacos.go
@@ -9,24 +9,23 @@ package nacos
 
 import (
 	"context"
-
-	"github.com/nacos-group/nacos-sdk-go/v2/clients"
-	"github.com/nacos-group/nacos-sdk-go/v2/clients/config_client"
-	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
-	"github.com/nacos-group/nacos-sdk-go/v2/vo"
-
 	"github.com/gogf/gf/v2/encoding/gjson"
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gcfg"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/config_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 )
 
 // Config is the configuration object for nacos client.
 type Config struct {
-	ServerConfigs []constant.ServerConfig `v:"required"` // See constant.ServerConfig
-	ClientConfig  constant.ClientConfig   `v:"required"` // See constant.ClientConfig
-	ConfigParam   vo.ConfigParam          `v:"required"` // See vo.ConfigParam
-	Watch         bool                    // Watch watches remote configuration updates, which updates local configuration in memory immediately when remote configuration changes.
+	ServerConfigs  []constant.ServerConfig                     `v:"required"` // See constant.ServerConfig
+	ClientConfig   constant.ClientConfig                       `v:"required"` // See constant.ClientConfig
+	ConfigParam    vo.ConfigParam                              `v:"required"` // See vo.ConfigParam
+	Watch          bool                                        // Watch watches remote configuration updates, which updates local configuration in memory immediately when remote configuration changes.
+	OnConfigChange func(namespace, group, dataId, data string) // Configure change callback function
 }
 
 // Client implements gcfg.Adapter implementing using nacos service.
@@ -125,9 +124,11 @@ func (c *Client) addWatcher() error {
 	if !c.config.Watch {
 		return nil
 	}
-
 	c.config.ConfigParam.OnChange = func(namespace, group, dataId, data string) {
 		c.doUpdate(data)
+		if c.config.OnConfigChange != nil {
+			go c.config.OnConfigChange(namespace, group, dataId, data)
+		}
 	}
 
 	if err := c.client.ListenConfig(c.config.ConfigParam); err != nil {
