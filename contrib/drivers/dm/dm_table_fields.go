03179f279c9470f856fdff987d18dcc17b56d0c4
yxh <yxh1103@qq.com>
2025-02-14 09:27:01 +0800

    fix The Dameng(DM) database supports the WherePri method.

--- contrib/drivers/dm/dm_table_fields.go
+++ contrib/drivers/dm/dm_table_fields.go
@@ -9,6 +9,7 @@ package dm
 import (
 	"context"
 	"fmt"
+	"github.com/gogf/gf/v2/container/gmap"
 	"strings"
 
 	"github.com/gogf/gf/v2/database/gdb"
@@ -16,7 +17,13 @@ import (
 )
 
 const (
-	tableFieldsSqlTmp = `SELECT * FROM ALL_TAB_COLUMNS WHERE Table_Name= '%s' AND OWNER = '%s'`
+	tableFieldsSqlTmp         = `SELECT * FROM ALL_TAB_COLUMNS WHERE Table_Name= '%s' AND OWNER = '%s'`
+	tableFieldsPkSqlSchemaTmp = `SELECT COLS.COLUMN_NAME AS PRIMARY_KEY_COLUMN FROM USER_CONSTRAINTS CONS 
+									JOIN USER_CONS_COLUMNS COLS ON CONS.CONSTRAINT_NAME = COLS.CONSTRAINT_NAME WHERE 
+							   		CONS.TABLE_NAME = '%s' AND CONS.CONSTRAINT_TYPE = 'P'`
+	tableFieldsPkSqlDBATmp = `SELECT COLS.COLUMN_NAME AS PRIMARY_KEY_COLUMN FROM DBA_CONSTRAINTS CONS 
+    								JOIN DBA_CONS_COLUMNS COLS ON CONS.CONSTRAINT_NAME = COLS.CONSTRAINT_NAME WHERE 
+								  	CONS.TABLE_NAME = '%s' AND CONS.OWNER = '%s' AND CONS.CONSTRAINT_TYPE = 'P'`
 )
 
 // TableFields retrieves and returns the fields' information of specified table of current schema.
@@ -24,8 +31,9 @@ func (d *Driver) TableFields(
 	ctx context.Context, table string, schema ...string,
 ) (fields map[string]*gdb.TableField, err error) {
 	var (
-		result gdb.Result
-		link   gdb.Link
+		result   gdb.Result
+		pkResult gdb.Result
+		link     gdb.Link
 		// When no schema is specified, the configuration item is returned by default
 		usedSchema = gutil.GetOrDefaultStr(d.GetSchema(), schema...)
 	)
@@ -45,7 +53,28 @@ func (d *Driver) TableFields(
 	if err != nil {
 		return nil, err
 	}
+	// Query the primary key field
+	pkResult, err = d.DoSelect(
+		ctx, link,
+		fmt.Sprintf(tableFieldsPkSqlSchemaTmp, strings.ToUpper(table)),
+	)
+	if err != nil {
+		return nil, err
+	}
+	if pkResult.IsEmpty() {
+		pkResult, err = d.DoSelect(
+			ctx, link,
+			fmt.Sprintf(tableFieldsPkSqlDBATmp, strings.ToUpper(table), strings.ToUpper(d.GetSchema())),
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
 	fields = make(map[string]*gdb.TableField)
+	pkFields := gmap.NewStrStrMap()
+	for _, pk := range pkResult {
+		pkFields.Set(pk["PRIMARY_KEY_COLUMN"].String(), "PRI")
+	}
 	for i, m := range result {
 		// m[NULLABLE] returns "N" "Y"
 		// "N" means not null
@@ -60,7 +89,7 @@ func (d *Driver) TableFields(
 			Type:    m["DATA_TYPE"].String(),
 			Null:    nullable,
 			Default: m["DATA_DEFAULT"].Val(),
-			// Key:     m["Key"].String(),
+			Key:     pkFields.Get(m["COLUMN_NAME"].String()),
 			// Extra:   m["Extra"].String(),
 			// Comment: m["Comment"].String(),
 		}
