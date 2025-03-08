018650583ab526aff552cd15164d23fc60312d18
panda <915615059@qq.com>
2025-03-08 14:17:21 +0800

    feat(ghttp): add Request.GetMetaTag to get specific meta tag value

--- util/gmeta/gmeta.go
+++ util/gmeta/gmeta.go
@@ -10,15 +10,17 @@ package gmeta
 import (
 	"github.com/gogf/gf/v2/container/gvar"
 	"github.com/gogf/gf/v2/os/gstructs"
+	"reflect"
 )
 
 // Meta is used as an embedded attribute for struct to enabled metadata feature.
 type Meta struct{}
 
-const (
-	metaAttributeName = "Meta"       // metaAttributeName is the attribute name of metadata in struct.
-	metaTypeName      = "gmeta.Meta" // metaTypeName is for type string comparison.
-)
+// metaAttributeName is the attribute name of metadata in struct.
+const metaAttributeName = "Meta"
+
+// metaType holds the reflection. Type of Meta, used for efficient type comparison.
+var metaType = reflect.TypeOf(Meta{})
 
 // Data retrieves and returns all metadata from `object`.
 func Data(object interface{}) map[string]string {
@@ -27,7 +29,7 @@ func Data(object interface{}) map[string]string {
 		return nil
 	}
 	if field, ok := reflectType.FieldByName(metaAttributeName); ok {
-		if field.Type.String() == metaTypeName {
+		if field.Type == metaType {
 			return gstructs.ParseTag(string(field.Tag))
 		}
 	}
