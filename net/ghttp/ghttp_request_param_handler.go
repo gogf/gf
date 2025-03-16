8e2e4e908039c749ece060d70c1cbf838055ea46
John Guo <john@johng.cn>
2025-03-16 09:38:42 +0800

    feat(net/ghttp): add GetMetaTag function for HandlerItem

--- net/ghttp/ghttp_request_param_handler.go
+++ net/ghttp/ghttp_request_param_handler.go
@@ -6,8 +6,6 @@
 
 package ghttp
 
-import "github.com/gogf/gf/v2/util/gmeta"
-
 // GetHandlerResponse retrieves and returns the handler response object and its error.
 func (r *Request) GetHandlerResponse() interface{} {
 	return r.handlerResponse
@@ -16,26 +14,4 @@ func (r *Request) GetHandlerResponse() interface{} {
 // GetServeHandler retrieves and returns the user defined handler used to serve this request.
 func (r *Request) GetServeHandler() *HandlerItemParsed {
 	return r.serveHandler
-}
-
-// GetMetaTag retrieves and returns the metadata value associated with the given key from the request struct.
-// The meta value is from struct tags from g.Meta/gmeta.Meta type.
-// For example:
-//
-//	type GetMetaTagReq struct {
-//	    g.Meta `path:"/test" method:"post" summary:"meta_tag" tags:"meta"`
-//	    // ...
-//	}
-//
-// r.GetServeHandler().GetMetaTag("summary") // returns "meta_tag"
-// r.GetServeHandler().GetMetaTag("method")  // returns "post"
-func (h *HandlerItemParsed) GetMetaTag(key string) string {
-	if h == nil || h.Handler == nil {
-		return ""
-	}
-	metaValue := gmeta.Get(h.Handler.Info.Type.In(1), key)
-	if metaValue != nil {
-		return metaValue.String()
-	}
-	return ""
 }
