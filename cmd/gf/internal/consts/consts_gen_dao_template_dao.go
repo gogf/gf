0bd387427c2899f40e4594f50ed4158ea8149289
CyJaySong <cyjaysong@gmail.com>
2025-04-24 13:54:40 +0800

    生成的dao模版默认非Safe模式

--- cmd/gf/internal/consts/consts_gen_dao_template_dao.go
+++ cmd/gf/internal/consts/consts_gen_dao_template_dao.go
@@ -124,7 +124,7 @@ func (dao *{{.TplTableNameCamelCase}}Dao) Ctx(ctx context.Context) *gdb.Model {
 	for _, handler := range dao.handlers {
 		model = handler(model)
 	}
-	return model.Safe().Ctx(ctx)
+	return model.Ctx(ctx)
 }
 
 // Transaction wraps the transaction logic using function f.
