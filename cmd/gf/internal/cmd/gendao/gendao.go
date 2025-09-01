f08897a1142caeb1c309e9a4eaebb7298e842a92
Luoyy <10894778+zishang520@users.noreply.github.com>
2025-09-01 12:20:22 +0800

    chore(tablewriter): upgrade to v1.0.9 and refactor table rendering logic (#4352)
    ### What’s Changed
    
    * Upgraded `github.com/olekukonko/tablewriter` from previous version to
    **v1.0.9**.
    
    ---------
    
    Co-authored-by: hailaz <739476267@qq.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- cmd/gf/internal/cmd/gendao/gendao.go
+++ cmd/gf/internal/cmd/gendao/gendao.go
@@ -11,6 +11,9 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/olekukonko/tablewriter"
+	"github.com/olekukonko/tablewriter/renderer"
+	"github.com/olekukonko/tablewriter/tw"
 	"golang.org/x/mod/modfile"
 
 	"github.com/gogf/gf/v2/container/garray"
@@ -99,6 +102,20 @@ var (
 			Type: "float64",
 		},
 	}
+
+	// tablewriter Options
+	twRenderer = tablewriter.WithRenderer(renderer.NewBlueprint(tw.Rendition{
+		Borders: tw.Border{Top: tw.Off, Bottom: tw.Off, Left: tw.Off, Right: tw.Off},
+		Settings: tw.Settings{
+			Separators: tw.Separators{BetweenRows: tw.Off, BetweenColumns: tw.Off},
+		},
+		Symbols: tw.NewSymbols(tw.StyleASCII),
+	}))
+	twConfig = tablewriter.WithConfig(tablewriter.Config{
+		Row: tw.CellConfig{
+			Formatting: tw.CellFormatting{AutoWrap: tw.WrapNone},
+		},
+	})
 )
 
 func (c CGenDao) Dao(ctx context.Context, in CGenDaoInput) (out *CGenDaoOutput, err error) {
