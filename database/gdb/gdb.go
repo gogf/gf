cf2234d0ec5d230d2d46f01f10b3ebb86175ba8e
shanyujie <1196661499@qq.com>
2025-10-22 17:51:54 +0800

    refactor(database): 移除未使用的缓存前缀常量

--- database/gdb/gdb.go
+++ database/gdb/gdb.go
@@ -708,7 +708,6 @@ const (
 	defaultMaxConnLifeTime                = 30 * time.Second // Max lifetime for per connection in pool in seconds.
 	cachePrefixTableFields                = `TableFields:`
 	cachePrefixSelectCache                = `SelectCache:`
-	cachePrefixCountCache                 = `CountCache:`
 	commandEnvKeyForDryRun                = "gf.gdb.dryrun"
 	modelForDaoSuffix                     = `ForDao`
 	dbRoleSlave                           = `slave`
