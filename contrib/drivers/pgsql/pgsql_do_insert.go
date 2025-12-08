01cd4a33842ba5b775815a8fad8b510044f1871e
John Guo <john@johng.cn>
2025-12-08 14:55:19 +0800

    up

--- contrib/drivers/pgsql/pgsql_do_insert.go
+++ contrib/drivers/pgsql/pgsql_do_insert.go
@@ -34,7 +34,7 @@ func (d *Driver) DoInsert(
 				return nil, gerror.WrapCode(
 					gcode.CodeInternalError,
 					err,
-					`failed to get primary keys for Replace operation`,
+					`failed to get primary keys for SaReplace operation`,
 				)
 			}
 			foundPrimaryKey := false
