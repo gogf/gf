bc2346ff5b24da76ddf85992111e633e8ba0fd80
John Guo <john@johng.cn>
2026-09-16 10:54:06 +0800

    fix(contrib/drivers/pgsql): mark all composite primary-key columns

--- contrib/drivers/pgsql/pgsql_z_unit_db_test.go
+++ contrib/drivers/pgsql/pgsql_z_unit_db_test.go
@@ -109,6 +109,16 @@ func Test_DB_Save_CompositePrimaryKey(t *testing.T) {
 		`, table))
 		t.AssertNil(err)
 
+		fields, err := db.TableFields(ctx, table)
+		t.AssertNil(err)
+		t.Assert(fields["a"].Key, "pri")
+		t.Assert(fields["b"].Key, "pri")
+		keys, err := db.GetCore().GetPrimaryKeys(ctx, table)
+		t.AssertNil(err)
+		t.Assert(len(keys), 2)
+		t.AssertIN("a", keys)
+		t.AssertIN("b", keys)
+
 		_, err = db.Save(ctx, table, g.Map{"a": 1, "b": 2, "name": "n1"})
 		t.AssertNil(err)
 
